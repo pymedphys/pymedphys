85b6afd7a40461aec8eb252a240b9f93433cd767
Simon Biggs <mail@simonbiggs.net>
2018-08-30 11:14:53 +1000

    update license

--- pymedphys/level1/msqconnect.py
+++ pymedphys/level1/msqconnect.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2018 CCA Health Care
+# Copyright (C) 2018 Cancer Care Associates
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published
