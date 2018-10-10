6ce2207975672533ddb6dcfbd796fac9089c8d96
Simon Biggs <mail@simonbiggs.net>
2018-10-10 15:00:01 +1100

    CI Travis setup (#2)
    Add Travis CI
    
    Former-commit-id: c2cdd6d1602b80dcc2b1d0a85f7667a90c204d48

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
+import io
+from glob import glob
+from os.path import basename
+from os.path import dirname
+from os.path import join
+from os.path import splitext
+
 from setuptools import setup, find_packages
 
-with open('README.md', 'r') as readme_file:
-    long_description = readme_file.read()
+
+def read(*names, **kwargs):
+    return io.open(
+        join(dirname(__file__), *names),
+        encoding=kwargs.get('encoding', 'utf8')
+    ).read()
+
 
 setup(
     name="pymedphys",
@@ -9,18 +21,21 @@ setup(
     author="Simon Biggs",
     author_email="me@simonbiggs.net",
     description='Medical Physics python modules',
-    long_description=long_description,
+    long_description=read('README.md'),
     long_description_content_type='text/markdown',
     classifiers=[
     'Development Status :: 3 - Alpha',
     'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
-    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.5',
     'Topic :: Scientific/Engineering :: Medical Science Apps.',
     'Topic :: Scientific/Engineering :: Physics',
     'Intended Audience :: Science/Research',
     'Intended Audience :: Healthcare Industry'
     ],
-    packages=find_packages(),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
     license='AGPLv3+',
     install_requires=[
         'numpy',
