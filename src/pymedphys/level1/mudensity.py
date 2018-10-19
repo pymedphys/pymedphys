785fa707b5282f351402cad34fdd876517fd1ec2
Simon Biggs <me@simonbiggs.net>
2018-10-20 10:15:03 +1100

    added outputs tests to mu_density doctests
    Former-commit-id: 66a8301207af5849fdc441cbc0139e3da83b9608

--- src/pymedphys/level1/mudensity.py
+++ src/pymedphys/level1/mudensity.py
@@ -125,6 +125,25 @@ def calc_mu_density(mu, mlc, jaw, grid_resolution=1, max_leaf_gap=400,
         ...    leaf_pair_widths=leaf_pair_widths)
         >>> display_mu_density(grid, mu_density)
 
+        >>> np.round(mu_density, 1)
+        array([[0. , 0. , 0. , 0. , 0. , 0. , 0. , 0. , 0. , 0. , 0. ],
+               [0. , 0. , 0. , 0.3, 1.9, 2.2, 1.9, 0.4, 0. , 0. , 0. ],
+               [0. , 0. , 0. , 0.4, 2.2, 2.5, 2.2, 0.5, 0. , 0. , 0. ],
+               [0. , 0. , 0. , 0.4, 2.4, 2.8, 2.5, 0.8, 0. , 0. , 0. ],
+               [0. , 0. , 0. , 0.4, 2.5, 3.1, 2.9, 1. , 0. , 0. , 0. ],
+               [0. , 0. , 0. , 0.4, 2.5, 3.4, 3.2, 1.3, 0. , 0. , 0. ],
+               [0. , 0. , 0.3, 2.4, 3.2, 3.7, 3.7, 3.5, 1.6, 0. , 0. ],
+               [0. , 0. , 0.3, 2.4, 3.2, 3.8, 4. , 3.8, 1.9, 0.1, 0. ],
+               [0. , 0. , 0.3, 2.4, 3.2, 3.8, 4.3, 4.1, 2.2, 0.1, 0. ],
+               [0. , 0. , 0.3, 2.4, 3.2, 3.9, 5.2, 4.7, 2.5, 0.2, 0. ],
+               [0. , 0. , 0.3, 2.4, 3.2, 3.8, 5.4, 6.6, 3.8, 0.5, 0. ],
+               [0. , 0.3, 2.3, 3. , 3.5, 4. , 5.1, 7.4, 6.8, 3.9, 0.5],
+               [0. , 0.3, 2.3, 3. , 3.5, 4. , 4.7, 6.9, 6.7, 3.9, 0.5],
+               [0. , 0.3, 2.3, 3. , 3.5, 4. , 4.5, 6.3, 6.4, 3.9, 0.5],
+               [0. , 0.3, 2.3, 3. , 3.5, 4. , 4.5, 5.6, 5.8, 3.8, 0.5],
+               [0. , 0.3, 2.3, 3. , 3.5, 4. , 4.5, 5.1, 5.1, 3.2, 0.5],
+               [0. , 0. , 0. , 0. , 0. , 0. , 0. , 0. , 0. , 0. , 0. ]])
+
 
         MU, MLC, and Jaw arguments can be retrieved from the `DeliveryData`
         object as defined within `pymedphys.level1.deliverydata`. Delivery
@@ -242,6 +261,12 @@ def calc_single_control_point(mlc, jaw, delivered_mu=1,
         >>> grid, mu_density = calc_single_control_point(
         ...     mlc, jaw, leaf_pair_widths=leaf_pair_widths)
         >>> display_mu_density(grid, mu_density)
+
+        >>> np.round(mu_density, 1)
+        array([[0. , 0.1, 0.4, 0.5, 0.4, 0.1, 0. ],
+               [0. , 0.1, 0.9, 1. , 0.9, 0.1, 0. ],
+               [0.1, 0.9, 1. , 1. , 1. , 0.9, 0.1],
+               [0. , 0.2, 0.2, 0.2, 0.2, 0.2, 0. ]])
     """
     leaf_pair_widths = np.array(leaf_pair_widths)
     leaf_division = leaf_pair_widths / grid_resolution
@@ -286,11 +311,17 @@ def single_mlc_pair(left_mlc, right_mlc, grid_resolution=1, time_steps=50):
     Example:
         >>> import matplotlib.pyplot as plt
 
+        >>> from pymedphys.level1.mudensity import (
+        ...     single_mlc_pair)
+
         >>> mlc_left = (-2.3, 3.1)  # (start position, end position)
         >>> mlc_right = (0, 7.7)
 
         >>> x, mu_density = single_mlc_pair(mlc_left, mlc_right)
         >>> fig = plt.plot(x, mu_density, '-o')
+
+        >>> np.round(mu_density, 1)
+        array([0.1, 0.2, 0.4, 0.5, 0.5, 0.6, 0.5, 0.4, 0.2, 0.1, 0. ])
     """
     leaf_pair_widths = [grid_resolution]
     jaw = np.array([
