f74304c49c6408c3f458580a818c0293db59f4f1
Simon Biggs <me@simonbiggs.net>
2018-10-23 08:02:45 +1100

    fix gitlab server address
    Former-commit-id: df222d87162a76090d85feab977f4e77fc4c1803 [formerly b447e3eda52bc3873474df1b4eaf12bcf8d3d483]
    Former-commit-id: 5ac493129f5b69c7d762b5c1335539e71d70be22

--- scripts/gitlab-ci/01-install-private.sh
+++ scripts/gitlab-ci/01-install-private.sh
@@ -6,7 +6,7 @@ source activate test
 mkdir $EXTERNAL_GIT_DIR
 cd $EXTERNAL_GIT_DIR
 
-git clone https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.instance/CCA-Physics/monorepo.git
+git clone https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.com/CCA-Physics/monorepo.git
 cd monorepo/Libraries/level-2/decode_trf
 
 pip install -e
