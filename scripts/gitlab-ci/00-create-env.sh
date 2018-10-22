68cb1320c9bdada2e36b469229e58c78caff2cef
Simon Biggs <me@simonbiggs.net>
2018-10-23 08:53:21 +1100

    swap to apt-get and make it quiet
    Former-commit-id: 4d76d336c6f4668576c1d3580e7ba47a28a6bace [formerly 16805883ba336a3ba284bef466ab315b795c4937]
    Former-commit-id: d2bea371bcabf60392a8f6068e2ff0777f7926bb

--- scripts/gitlab-ci/00-create-env.sh
+++ scripts/gitlab-ci/00-create-env.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-apt update
-apt install libgl1-mesa-glx -y
+apt-get update
+apt-get install libgl1-mesa-glx -y -q
 
 conda config --set always_yes yes --set changeps1 no
 conda config --add channels conda-forge
