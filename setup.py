93c2bae0643fca71fbff56f3ed3b0105482f1e3e
Simon Biggs <me@simonbiggs.net>
2018-11-02 01:17:05 +1100

    requirements
    Former-commit-id: de44cecdd90e6cdb2acabd912b9135ee9df2ebee [formerly e2ecbc4f96d5ae34f20aa0c61e8b829f5ebccf2f]
    Former-commit-id: cb0e8041c2edee482d79313702b8a87f0092c2f1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def read(*names, **kwargs):
 
 setup(
     name="pymedphys",
-    version="0.3.1",
+    version="0.3.2",
     author="Simon Biggs",
     author_email="me@simonbiggs.net",
     description='Medical Physics python modules',
