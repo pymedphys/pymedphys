ca155bc4a2b24d6d26a03e8551c64dca8faa54bf
Simon Biggs <me@simonbiggs.net>
2018-11-17 05:44:31 +1100

    Added Matthew Jennings to copyright for gitlab-ci, updated tests to not test trf if access to monorepo is unavailable.
    Former-commit-id: 704e0392bd0de47bcc5615a075b8e3736bcd0b8b

--- scripts/gitlab-ci/02-install-private.sh
+++ scripts/gitlab-ci/02-install-private.sh
@@ -3,10 +3,8 @@
 set -ex
 source activate test
 
-mkdir $EXTERNAL_GIT_DIR
 cd $EXTERNAL_GIT_DIR
 
-git clone https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.com/CCA-Physics/monorepo.git
-cd monorepo/Libraries/level-2/decode_trf
-
+cd monorepo/Libraries/level-2/decode_trf;
 pip install .
+
