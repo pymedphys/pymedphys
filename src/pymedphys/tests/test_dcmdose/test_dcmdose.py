62ea7f49441397af2b4432068c1d15a8f3a34e95
Matthew Jennings <matthew.jennings2@sa.gov.au>
2018-11-24 14:05:46 +1030

    dcmdose.py: Tweak comments test_dcmdose.py: Add eight DICOM RT Dose files to test data, one for each of the possible scan orientations. Read in DICOMs to test function.

--- src/pymedphys/tests/test_dcmdose/test_dcmdose.py
+++ src/pymedphys/tests/test_dcmdose/test_dcmdose.py
@@ -4,12 +4,32 @@ from pymedphys.dcm import load_xyz_from_dicom
 
 class TestDcmDose():
 
-    def test_load_xyz_from_dicom_coordinates(self):
-        # TODO: read in HFS, HFP, FFS and FFP skeleton dcm dose files using pydicom
+    def test_load_xyz_from_dicom(self):
+        expected_coords = { 'FFDL' : 'he' }
+        
+        test_dcms = {}
+        test_dcms['FFDL'] = dcm.dcmread("../data/dcmdose/RD.DICOMORIENT.Dose_FFDL.dcm")
+        test_dcms['FFDR'] = dcm.dcmread("../data/dcmdose/RD.DICOMORIENT.Dose_FFDR.dcm")
+        test_dcms['FFP']  = dcm.dcmread("../data/dcmdose/RD.DICOMORIENT.Dose_FFP.dcm")
+        test_dcms['FFS']  = dcm.dcmread("../data/dcmdose/RD.DICOMORIENT.Dose_FFS.dcm")
+        test_dcms['HFDL'] = dcm.dcmread("../data/dcmdose/RD.DICOMORIENT.Dose_HFDL.dcm")
+        test_dcms['HFDR'] = dcm.dcmread("../data/dcmdose/RD.DICOMORIENT.Dose_HFDR.dcm")
+        test_dcms['HFP']  = dcm.dcmread("../data/dcmdose/RD.DICOMORIENT.Dose_HFP.dcm")
+        test_dcms['HFS']  = dcm.dcmread("../data/dcmdose/RD.DICOMORIENT.Dose_HFS.dcm")
+        
+        for orient, d in test_dcms.items():
+            x, y, z = load_xyz_from_dicom(d)
+            print(x)
+            print(y)
+            print(z)
+            break
 
         # TODO: run load_xyz_from_dicom() on each file and compare x, y and z coordinate arrays to expected values
             # What values to use as expected?
                 # Use a commercial DICOM RT Dose display tool?
-                # What if coordinate system
+                # What if coordinate system is incorrectly configured in software?
 
         return
+if __name__ == "__main__":
+    tdd = TestDcmDose()
+    tdd.test_load_xyz_from_dicom()
