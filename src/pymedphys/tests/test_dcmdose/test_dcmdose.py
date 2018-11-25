de2345a1d746b670bab676b17da204694e611d99
Simon Biggs <me@simonbiggs.net>
2018-11-25 16:36:57 +1100

    run autopep8
    Former-commit-id: 8031282af88111aa17c9fa85f1c99d09f1a6e504 [formerly 121e15d5449094e58148df3b209ef95f1d411f7b]
    Former-commit-id: 6a950e1cf734182c1dc6dab0f88dccecbc72bea5

--- src/pymedphys/tests/test_dcmdose/test_dcmdose.py
+++ src/pymedphys/tests/test_dcmdose/test_dcmdose.py
@@ -2,21 +2,30 @@ import pydicom as dcm
 import numpy as np
 from pymedphys.dcm import load_xyz_from_dicom
 
+
 class TestDcmDose():
 
     def test_load_xyz_from_dicom(self):
-        expected_coords = { 'FFDL' : 'he' }
-        
+        expected_coords = {'FFDL': 'he'}
+
         test_dcms = {}
-        test_dcms['FFDL'] = dcm.dcmread("../data/dcmdose/RD.DICOMORIENT.Dose_FFDL.dcm")
-        test_dcms['FFDR'] = dcm.dcmread("../data/dcmdose/RD.DICOMORIENT.Dose_FFDR.dcm")
-        test_dcms['FFP']  = dcm.dcmread("../data/dcmdose/RD.DICOMORIENT.Dose_FFP.dcm")
-        test_dcms['FFS']  = dcm.dcmread("../data/dcmdose/RD.DICOMORIENT.Dose_FFS.dcm")
-        test_dcms['HFDL'] = dcm.dcmread("../data/dcmdose/RD.DICOMORIENT.Dose_HFDL.dcm")
-        test_dcms['HFDR'] = dcm.dcmread("../data/dcmdose/RD.DICOMORIENT.Dose_HFDR.dcm")
-        test_dcms['HFP']  = dcm.dcmread("../data/dcmdose/RD.DICOMORIENT.Dose_HFP.dcm")
-        test_dcms['HFS']  = dcm.dcmread("../data/dcmdose/RD.DICOMORIENT.Dose_HFS.dcm")
-        
+        test_dcms['FFDL'] = dcm.dcmread(
+            "../data/dcmdose/RD.DICOMORIENT.Dose_FFDL.dcm")
+        test_dcms['FFDR'] = dcm.dcmread(
+            "../data/dcmdose/RD.DICOMORIENT.Dose_FFDR.dcm")
+        test_dcms['FFP'] = dcm.dcmread(
+            "../data/dcmdose/RD.DICOMORIENT.Dose_FFP.dcm")
+        test_dcms['FFS'] = dcm.dcmread(
+            "../data/dcmdose/RD.DICOMORIENT.Dose_FFS.dcm")
+        test_dcms['HFDL'] = dcm.dcmread(
+            "../data/dcmdose/RD.DICOMORIENT.Dose_HFDL.dcm")
+        test_dcms['HFDR'] = dcm.dcmread(
+            "../data/dcmdose/RD.DICOMORIENT.Dose_HFDR.dcm")
+        test_dcms['HFP'] = dcm.dcmread(
+            "../data/dcmdose/RD.DICOMORIENT.Dose_HFP.dcm")
+        test_dcms['HFS'] = dcm.dcmread(
+            "../data/dcmdose/RD.DICOMORIENT.Dose_HFS.dcm")
+
         for orient, d in test_dcms.items():
             x, y, z = load_xyz_from_dicom(d)
             print(x)
@@ -26,10 +35,12 @@ class TestDcmDose():
 
         # TODO: run load_xyz_from_dicom() on each file and compare x, y and z coordinate arrays to expected values
             # What values to use as expected?
-                # Use a commercial DICOM RT Dose display tool?
-                # What if coordinate system is incorrectly configured in software?
+            # Use a commercial DICOM RT Dose display tool?
+            # What if coordinate system is incorrectly configured in software?
 
         return
+
+
 if __name__ == "__main__":
     tdd = TestDcmDose()
     tdd.test_load_xyz_from_dicom()
