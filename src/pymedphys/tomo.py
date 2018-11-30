d620d2818384eb9893bed00789c26b2e8fc81c66
SimonBiggs <me@simonbiggs.net>
2018-11-30 11:28:31 +1100

    fix typo

--- src/pymedphys/tomo.py
+++ src/pymedphys/tomo.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2018 PyMedPhys Contibutors
+# Copyright (C) 2018 PyMedPhys Contributors
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published
