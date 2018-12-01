09accbb538dd2923e36bef952f4cca92bca752f1
Simon Biggs <me@simonbiggs.net>
2018-12-01 14:08:25 +1100

    begin creating unshuffle test
    Former-commit-id: 86b5b2f65922f3e03a8457e9a77263e68f0683c6 [formerly 01ac096b8ffe3eb4c3b78ee1c3f394c38870c9eb]
    Former-commit-id: b96803f4458dd67aa94da67d6a35064708c88eac

--- src/pymedphys/_level1/tomounshuffle.py
+++ src/pymedphys/_level1/tomounshuffle.py
@@ -23,9 +23,13 @@
 # You should have received a copy of the Apache-2.0 along with this
 # program. If not, see <http://www.apache.org/licenses/LICENSE-2.0>.
 
+
+"""@author: king.r.paul@gmail.com
 """
-@author: king.r.paul@gmail.com
-"""
+
+from string import digits as DIGITS
+from string import ascii_letters as LETTERS
+import csv
 
 import numpy as np
 
@@ -79,3 +83,44 @@ def unshuffle_sinogram(array):
     result = [[p[31 - gap:32 + gap] for p in result[i]] for i in range(51)]
 
     return result
+
+
+def unshuffle_sinogram_csv(file_name):
+    """
+    Convert a CSV sinogram file into a PDF fluence map collection, by
+    unshuffling the sinogram, i.e. separating leaf pattern into
+    the 51 tomtherapy discretization angles; display & save result.
+
+    Keyword Args:
+        file_name:
+            path to csv sinogram file, file formatted as follows
+                "Patient name: ANONYMOUS^PATIENT, ID: 00000",,,,,,,,,
+                ,0,0,0,0,0,0,0,0,0,0,0,0,,0.39123373,0.366435635 ...
+            Demographics on row1, with each following row corresponding
+            to a single couch step increment and comprised of 64 cells.
+            Each cell in a row corresponding to an mlc leaf, and
+            containing its leaf-open fraction.
+            This format is produced by ExportTomoSinogram.py, shared by
+            Brandon Merz on on the RaySearch customer forum, 1/18/2018.
+    """
+
+    with open(file_name, 'r') as csvfile:
+
+        # PATIENT NAME & ID
+        pat_name, pat_num = csvfile.readline().split('ID:')
+        pat_name = pat_name.replace('Patient name:', '')
+        pat_name_last, pat_name_first = pat_name.split('^')
+
+        pat_name_last = ''.join([c for c in pat_name_last if c in LETTERS])
+        pat_name_first = ''.join([c for c in pat_name_first if c in LETTERS])
+        pat_num = ''.join([c for c in pat_num if c in DIGITS])
+
+        document_id = pat_num + ' - ' + pat_name_last + ', ' + pat_name_first
+
+        # SINOGRAM
+        reader = csv.reader(csvfile, delimiter=',')
+        array = np.asarray([line[1:] for line in reader]).astype(float)
+
+    result = unshuffle_sinogram(array)
+
+    return document_id, result
