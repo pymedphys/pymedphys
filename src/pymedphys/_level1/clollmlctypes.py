cef370247100a14f72d92b2f06140d66852c8692
Simon Biggs <me@simonbiggs.net>
2018-12-02 04:52:42 +1100

    pull mudensity under coll module
    Former-commit-id: 2d11b061a5462712fd139b04ce3345c78bfea4ff

--- src/pymedphys/_level1/clollmlctypes.py
+++ src/pymedphys/_level1/clollmlctypes.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2018 Paul King
+# Copyright (C) 2018 PyMedPhys Contributors
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published
@@ -23,20 +23,6 @@
 # You should have received a copy of the Apache-2.0 along with this
 # program. If not, see <http://www.apache.org/licenses/LICENSE-2.0>.
 
-
-from pymedphys.aperture import mlc_equivalent_square_fs
-
-
-def test_equivalent_fs():
-    """ Compare effective field size for known pattern against benchmark. """
-    shape = [(0.0, 0.0) for i in range(14)]
-    shape += [(3.03, 2.47), (2.88, 2.46), (3.08, 2.51), (2.86, 2.46),
-              (2.88, 2.46), (2.91, 5.04), (2.5, 5.04), (2.55, 4.87),
-              (2.38, 4.61), (2.38, 7.04), (2.61, 7.46), (2.48, 6.55),
-              (3.02, 6.52), (3.9, 7.2), (4.5, 7.5), (4.5, 7.5), (4.5, 7.5),
-              (4.5, 7.5), (4.45, 7.5), (4.0, 7.5), (3.5, 7.5), (3.49, 7.5),
-              (3.0, 7.5), (3.0, 7.5), (3.0, 7.5), (2.5, 7.5), (2.5, 7.5),
-              (2.49, 6.52)]
-    shape += [(0.0, 0.0) for i in range(18)]
-
-    assert abs(mlc_equivalent_square_fs(shape) - 10.725) < 0.05
+MILLENIUM = (10,)*10 + (5,)*40 + (10,)*10
+BRAINLAB = (5.5,)*3 + (4.5,)*3 + (3,)*14 + (4.5,)*3 + (5.5,)*3
+AGILITY = (5,)*80
--- src/pymedphys/_level2/collmudensity.py
+++ src/pymedphys/_level2/collmudensity.py
@@ -124,7 +124,7 @@ def calc_mu_density(mu, mlc, jaw, grid_resolution=__DEFAULT_GRID_RESOLUTION,
     --------
     >>> import numpy as np
     >>>
-    >>> from pymedphys.mudensity import (
+    >>> from pymedphys.coll import (
     ...     calc_mu_density, get_grid, display_mu_density)
     >>>
     >>> leaf_pair_widths = (5, 5, 5)
@@ -195,7 +195,7 @@ def calc_mu_density(mu, mlc, jaw, grid_resolution=__DEFAULT_GRID_RESOLUTION,
 
     MU Density from a Mosaiq record
 
-    >>> from pymedphys.mudensity import (
+    >>> from pymedphys.coll import (
     ...     calc_mu_density, get_grid, display_mu_density)
     >>>
     >>> from pymedphys.msq import (
@@ -220,7 +220,7 @@ def calc_mu_density(mu, mlc, jaw, grid_resolution=__DEFAULT_GRID_RESOLUTION,
 
     MU Density from a logfile at a given filepath
 
-    >>> from pymedphys.mudensity import (
+    >>> from pymedphys.coll import (
     ...     calc_mu_density, get_grid, display_mu_density)
     >>>
     >>> from pymedphys.trf import delivery_data_from_logfile
@@ -280,7 +280,7 @@ def calc_single_control_point(mlc, jaw, delivered_mu=1,
     Examples
     --------
     >>> import numpy as np
-    >>> from pymedphys.mudensity import (
+    >>> from pymedphys.coll import (
     ...     calc_single_control_point, display_mu_density)
     >>>
     >>> leaf_pair_widths = (2, 2)
@@ -363,7 +363,7 @@ def single_mlc_pair(left_mlc, right_mlc,
     >>> import numpy as np
     >>> import matplotlib.pyplot as plt
     >>>
-    >>> from pymedphys.mudensity import single_mlc_pair
+    >>> from pymedphys.coll import single_mlc_pair
     >>>
     >>> mlc_left = (-2.3, 3.1)  # (start position, end position)
     >>> mlc_right = (0, 7.7)
@@ -428,7 +428,7 @@ def get_grid(max_leaf_gap=__DEFAULT_MAX_LEAF_GAP, grid_resolution=__DEFAULT_GRID
 
     Examples
     --------
-    See `pymedphys.mudensity.calc_mu_density`_.
+    See `pymedphys.coll.calc_mu_density`_.
     """
     leaf_pair_widths = np.array(leaf_pair_widths)
 
@@ -486,7 +486,7 @@ def display_mu_density(grid, mu_density, grid_resolution=None):
 
     Examples
     --------
-    See `pymedphys.mudensity.calc_mu_density`_.
+    See `pymedphys.coll.calc_mu_density`_.
     """
     if grid_resolution is None:
         grid_resolution = grid['mlc'][1] - grid['mlc'][0]
