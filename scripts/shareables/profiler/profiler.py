a65ba08a7ddc9c76342edeeeac9f2774961f244b
Paul King <king.r.paul@gmail.com>
2018-12-04 15:41:06 -0600

    add read profiler
    Former-commit-id: d0e98671c866cc338a57ec0bb5a3c3a4aec84a7d [formerly 95f03976a3160b12aa8d7531b92e851553e54d0c]
    Former-commit-id: d3d7823ee61c4f750b2c0e72405c24bb40664d0e

---
+++ scripts/shareables/profiler/profiler.py
@@ -0,0 +1,101 @@
+# Copyright (C) 2018 Paul King
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version (the "AGPL-3.0+").
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License and the additional terms for more
+# details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+# ADDITIONAL TERMS are also included as allowed by Section 7 of the GNU
+# Affrero General Public License. These aditional terms are Sections 1, 5,
+# 6, 7, 8, and 9 from the Apache License, Version 2.0 (the "Apache-2.0")
+# where all references to the definition "License" are instead defined to
+# mean the AGPL-3.0+.
+
+# You should have received a copy of the Apache-2.0 along with this
+# program. If not, see <http://www.apache.org/licenses/LICENSE-2.0>.
+
+
+import os
+import numpy as np
+
+
+def read_file(file_name):
+    """ 
+    Read and return dose profiles and CAX dose from native Profiler data file.
+    
+    Arguments:
+        file_name -- long file name of profiler file
+        
+    Returns:
+        object of type Profiler:
+            Profiler.cax = float(dose) at central axis
+            Profiler.x = x profile, i.e. [(distance, dose), ...]
+            Profiler.x = x profile, i.e. [(distance, dose), ...]
+                distance = float(+/- distance from cax
+                dose = float(absolute dose at detector)
+    """
+
+    class Profiler():
+        def __init__(self, cax, x, y):
+            assert float(cax)
+            assert type(x) == list
+            assert type(y) == list
+
+            self.cax = float(cax)
+            self.x = x
+            self.y = y
+
+    with open(file_name) as profiler_file:
+        for row in profiler_file.readlines():
+            contents = row
+            if contents[:11] == "Calibration" and "File" not in contents:
+                calibs = np.array(contents.split())[1:].astype(float)
+            elif contents[:5] == "Data:":
+                counts = np.array(contents.split()[5:145]).astype(float)
+            elif contents[:15] == "Dose Per Count:":
+                dose_per_count = (float(contents.split()[-1]))
+        assert (len(calibs)) == (len(counts)) == 140
+        assert dose_per_count > 0.0
+    dose = counts * dose_per_count * calibs
+
+    y_vals = [-16.4 + 0.4*i for i in range(83)]
+    x_vals = [-11.2 + 0.4*i for i in range(57)]
+
+    x_prof = list(zip(y_vals, dose[:57]))
+    y_prof = list(zip(x_vals, dose[57:]))
+
+    assert np.allclose(y_prof[41][1], x_prof[28][1])
+    cax_dose = y_prof[41][1]
+
+    return Profiler(cax_dose, x_prof, y_prof)
+
+
+def test():
+    file_name = os.path.join(os.getcwd(), 'test_varian_open.prs')
+    assert np.allclose(read_file(file_name).cax, 45.50562901780488)
+    assert np.allclose(read_file(file_name).x[0][1], 0.579460838649598)
+    assert np.allclose(read_file(file_name).y[0][1], 0.2910764234184594)
+
+    file_name = os.path.join(os.getcwd(), 'test_varian_wedge.prs')
+    assert np.allclose(read_file(file_name).cax, 21.863167869662274)
+    assert np.allclose(read_file(file_name).x[0][1], 0.5626051581458927)
+    assert np.allclose(read_file(file_name).y[0][1], 0.260042064635505)
+
+    file_name = os.path.join(os.getcwd(), 'test_tomo_50mm.prs')
+    assert np.allclose(read_file(file_name).cax, 784.320114110518)
+    assert np.allclose(read_file(file_name).x[0][1], 563.4064789252321)
+    assert np.allclose(read_file(file_name).y[0][1], 1.8690221773721463)
+
+    print('profiler.read_file() -> OK, props: .cax, .x, .y')
+
+
+if __name__ == "__main__":
+    test()
