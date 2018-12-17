2bf285608d0890f76e0afe210764a99fd79b61c9
Simon Biggs <me@simonbiggs.net>
2018-12-17 20:28:42 +1100

    move git hash to after actually within git repo
    Former-commit-id: 53d5896de0d8b6d2547d5a734cf63437118c9092 [formerly c7e2091eebd7a2d481e42a5d245a0144e8660f42]
    Former-commit-id: d9393a3d6a37a2b362111e2867352e2a53403846

--- scripts/gitlab-ci/02-install-private.sh
+++ scripts/gitlab-ci/02-install-private.sh
@@ -5,8 +5,7 @@ source activate test
 
 cd $EXTERNAL_GIT_DIR
 
+cd monorepo/Libraries/level-2/decode_trf;
 git rev-parse HEAD | GREP_COLORS='ms=34;1' grep $(git rev-parse --short=0 HEAD)
 
-cd monorepo/Libraries/level-2/decode_trf;
 pip install -e .
-
