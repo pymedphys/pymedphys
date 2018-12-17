e77692e26a220f1b3e7f6baec920706d9fe7f5d7
Simon Biggs <me@simonbiggs.net>
2018-12-17 20:16:16 +1100

    add branch commit hash printout for monorepo

--- scripts/gitlab-ci/02-install-private.sh
+++ scripts/gitlab-ci/02-install-private.sh
@@ -5,6 +5,8 @@ source activate test
 
 cd $EXTERNAL_GIT_DIR
 
+git rev-parse HEAD | GREP_COLORS='ms=34;1' grep $(git rev-parse --short=0 HEAD)
+
 cd monorepo/Libraries/level-2/decode_trf;
 pip install -e .
 
