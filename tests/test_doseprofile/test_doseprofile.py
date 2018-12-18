04a99e6138172682a14f245a045380dedb10f82d
Paul King <king.r.paul@gmail.com>
2018-12-18 14:52:32 -0600

    difficulty importing from test file
    Former-commit-id: 206741470bc382cc4f006c1d41a29b0fa9ac2341 [formerly f12d2a3423b6d6b8389278940c1face7c6b5b07b]
    Former-commit-id: 24d33620b7ec34dd7382cec7acc0774e10ddad07

--- tests/test_doseprofile/test_doseprofile.py
+++ tests/test_doseprofile/test_doseprofile.py
@@ -23,10 +23,15 @@
 # You should have received a copy of the Apache-2.0 along with this
 # program. If not, see <http://www.apache.org/licenses/LICENSE-2.0>.
 
-import os
-import numpy as np
 
-from pymedphys.devices import read_mapcheck_txt
+import sys
+import numpy as np
+import os
+# from pymedphys.doseprofile import *
+import pymedphys
+print(pymedphys.doseprofile)
+sys.exit()
+
 
 DATA_DIRECTORY = os.path.abspath(
     os.path.join(os.path.dirname(__file__),
@@ -35,33 +40,71 @@ DATA_DIRECTORY = os.path.abspath(
 
 def test_crossings():
     """ """
-    simple_cross = read.Scan()
-    simple_cross.x, simple_cross.y, t = [0, 1], [0, 1], 0.5
+
+    # simple_cross.x, simple_cross.y, t = [0, 1], [0, 1], 0.5
+    simple_cross = zip([0, 1], [0, 1])
+    print(simple_cross)
     assert crossings(simple_cross, t) == [0.5]
 
-    flat_section = read.Scan()
-    flat_section.x, flat_section.y, t = [0, 1, 2], [0, 0, 1], 0.5
-    assert crossings(flat_section, t) == [1.5]
+    # flat_section = read.Scan()
+    # flat_section.x, flat_section.y, t = [0, 1, 2], [0, 0, 1], 0.5
+    # assert crossings(flat_section, t) == [1.5]
 
-    double_cross = read.Scan()
-    double_cross.x, double_cross.y, t = [0, 1, 2], [0, 0, 1], 0.5
-    assert crossings(double_cross, t) == [1.5]
+    # double_cross = read.Scan()
+    # double_cross.x, double_cross.y, t = [0, 1, 2], [0, 0, 1], 0.5
+    # assert crossings(double_cross, t) == [1.5]
 
-    above_bounds = read.Scan()
-    above_bounds.x, above_bounds.y, t = [0, 1, 2], [0, 0, 1], 0.5
-    assert crossings(above_bounds, t) == [1.5]
+    # above_bounds = read.Scan()
+    # above_bounds.x, above_bounds.y, t = [0, 1, 2], [0, 0, 1], 0.5
+    # assert crossings(above_bounds, t) == [1.5]
 
-    below_bounds = read.Scan()
-    below_bounds.x, below_bounds.y, t = [0, 1, 2], [0, 0, 1], 0.5
-    assert crossings(below_bounds, t) == [1.5]
+    # below_bounds = read.Scan()
+    # below_bounds.x, below_bounds.y, t = [0, 1, 2], [0, 0, 1], 0.5
+    # assert crossings(below_bounds, t) == [1.5]
 
-    simple_pulse = read.Scan()
-    simple_pulse.x = [-10.1, -9.9, 9.9, 10.1]
-    simple_pulse.y = [0.0, 1.0, 1.0, 0.0]
-    assert crossings(simple_pulse, 0.5) == [-10.0, 10.0]
+    # simple_pulse = read.Scan()
+    # simple_pulse.x = [-10.1, -9.9, 9.9, 10.1]
+    # simple_pulse.y = [0.0, 1.0, 1.0, 0.0]
+    # assert crossings(simple_pulse, 0.5) == [-10.0, 10.0]
 
-    print 'crossings passed'
+    print('crossings passed')
+
+# def test_edges():
+#     """ """
+#     # simple pulse
+#     simple_pulse = read.Scan()
+#     simple_pulse.x = [-20, -10.1, -9.9, 9.9, 10.1, 20]
+#     simple_pulse.y = [0.0, 0.0,1.0,1.0,0.0, 0.0]
+#     assert np.allclose(edges(simple_pulse), (-10.0, 0.0, 10.0))
+#     # profiler
+#     a = read.profiler(TEST[0])
+#     for k in a.keys():
+#         assert abs(sum(edges(a[k]))) <= 0.3
+
+#     print 'edges passed'
+
+# def test_move_to_match():
+#     """ """
+#     # DYNAMIC WEDGE, PROFILER -> TOMO_FILM PROFILE
+#     p = read.profiler(TEST[1]);  to_move = p[p.keys()[0]]
+#     f = read.tomo_film(TEST[2]); to_match= f[f.keys()[0]]
+#     r = move_to_match(to_move, to_match, 0.1)
+#     assert abs(r['shft'] - 9.3) < 0.1
+#     assert np.allclose(r['flp'], True)
+#     assert len(r['x1']) -len(r['y1']) + len(r['x2']) -len(r['y2']) == 0
+#     if True: # POPUP PLOT
+#         import pylab as plt
+#         kwargs = {'markersize': 1,  'markeredgewidth': 0.2, 'marker': 'o',
+#           'linestyle': '-', 'linewidth': .010, 'markeredgecolor': 'r',
+#           'markerfacecolor': 'r'}
+#         plt.plot(r['x1'],  r['y1'], **kwargs)
+#         kwargs['markeredgecolor'] = 'b'; kwargs['markerfacecolor'] = 'b'
+#         plt.plot(r['x2'],  r['y2'], **kwargs)
+#         plt.show()
+#     print 'move_to_match passed'
 
 
 if __name__ == "__main__":
-    test_read_mapcheck_txt()
+    test_crossings()
+    # test_edges()
+    # test_move_to_match()
