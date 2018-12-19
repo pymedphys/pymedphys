9432f0c3695de3507f047dba95831499852a279d
Paul King <king.r.paul@gmail.com>
2018-12-19 09:34:57 -0600

    user doc for read_mapcheck & read_profiler

--- tests/test_devices/test_read_profiler_prs.py
+++ tests/test_devices/test_read_profiler_prs.py
@@ -33,19 +33,22 @@ DATA_DIRECTORY = os.path.join(
     os.path.dirname(__file__), "../data/devices/profiler")
 
 
-def test_read_prs():
+def test_read_profiler_prs():
 
     file_name = os.path.join(DATA_DIRECTORY, 'test_varian_open.prs')
-    assert np.allclose(read_prs(file_name).cax, 45.50562901780488)
-    assert np.allclose(read_prs(file_name).x[0][1], 0.579460838649598)
-    assert np.allclose(read_prs(file_name).y[0][1], 0.2910764234184594)
+    assert np.allclose(read_profiler_prs(file_name).cax, 45.50562901780488)
+    assert np.allclose(read_profiler_prs(file_name).x[0][1], 0.579460838649598)
+    assert np.allclose(read_profiler_prs(
+        file_name).y[0][1], 0.2910764234184594)
 
     file_name = os.path.join(DATA_DIRECTORY, 'test_varian_wedge.prs')
-    assert np.allclose(read_prs(file_name).cax, 21.863167869662274)
-    assert np.allclose(read_prs(file_name).x[0][1], 0.5626051581458927)
-    assert np.allclose(read_prs(file_name).y[0][1], 0.260042064635505)
+    assert np.allclose(read_profiler_prs(file_name).cax, 21.863167869662274)
+    assert np.allclose(read_profiler_prs(
+        file_name).x[0][1], 0.5626051581458927)
+    assert np.allclose(read_profiler_prs(file_name).y[0][1], 0.260042064635505)
 
     file_name = os.path.join(DATA_DIRECTORY, 'test_tomo_50mm.prs')
-    assert np.allclose(read_prs(file_name).cax, 784.320114110518)
-    assert np.allclose(read_prs(file_name).x[0][1], 563.4064789252321)
-    assert np.allclose(read_prs(file_name).y[0][1], 1.8690221773721463)
+    assert np.allclose(read_profiler_prs(file_name).cax, 784.320114110518)
+    assert np.allclose(read_profiler_prs(file_name).x[0][1], 563.4064789252321)
+    assert np.allclose(read_profiler_prs(
+        file_name).y[0][1], 1.8690221773721463)
