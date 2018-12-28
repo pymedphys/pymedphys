85dbd74b739b4e100670a66a76447a29c65d3b0f
Simon Biggs <me@simonbiggs.net>
2018-12-28 23:05:31 +1100

    don't use bash

--- scripts/gitlab-ci/update-docker-file.sh
+++
@@ -1,9 +0,0 @@
-# Need to run the following before this script
-# docker login registry.gitlab.com
-
-# INFO at https://gitlab.com/help/user/project/container_registry#build-and-push-images
-
-DIR=`dirname "$0"`
-
-docker build -t registry.gitlab.com/pymedphys/pymedphys/ci $DIR
-docker push registry.gitlab.com/pymedphys/pymedphys/ci
