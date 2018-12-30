126c322a74154f7cbe881c34fb959dfe961b127c
Simon Biggs <me@simonbiggs.net>
2018-12-31 02:10:26 +1100

    make a commit for example purposes
    Former-commit-id: d1790be2cd7f1361564a8d58622ce7b6559550a1 [formerly 1b7a1fe7ba10b4484ce8c71358316923d6189908]
    Former-commit-id: 8d716b7beb2c4aa7a46e5d77a737fdc22924e26b

--- application/src/renderer/index.tsx
+++ application/src/renderer/index.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+import * as electron from 'electron';
+
 import "./style.scss"
+import { matplotlib } from './matplotlib';
 
 import { Cell, Column, Table } from "@blueprintjs/table";
 
@@ -9,8 +12,10 @@ const cellRenderer = (rowIndex: number) => {
   return <Cell>{`$${(rowIndex * 10).toFixed(2)}`}</Cell>
 };
 
+const app = document.getElementById('app') as HTMLDivElement
+
 const root = document.createElement('div')
-document.body.append(root)
+app.append(root)
 
 ReactDOM.render(
   <Table numRows={10}>
@@ -18,3 +23,12 @@ ReactDOM.render(
   </Table>,
   root
 );
+
+electron.ipcRenderer.on('jupyter', (event: any, store: { port: number; token: string }) => {
+  let jupyterConfig = document.createElement('script')
+  jupyterConfig.id = 'jupyter-config-data'
+  jupyterConfig.type = 'application/json'
+  jupyterConfig.textContent = `{ "baseUrl": "http://localhost:${store.port}", "token": "${store.token}" }`
+  document.head.append(jupyterConfig)
+  matplotlib(app)
+})
