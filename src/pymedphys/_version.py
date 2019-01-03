069ad7d7a2605c8a3622b1978d8d67f47a85f253
Simon Biggs <me@simonbiggs.net>
2019-01-03 19:28:46 +1100

    get version nubmer from package.json

--- src/pymedphys/_version.py
+++ src/pymedphys/_version.py
@@ -1,2 +1,14 @@
-version_info = (0, 5, 0, 'dev0')
+"""Extracts version number information from gui/package.json
+"""
+
+
+import os
+import json
+
+HERE = os.path.dirname(__file__)
+
+with open(os.path.join(HERE, 'gui', 'package.json')) as file:
+    data = json.load(file)
+
+version_info = data['version'].replace('.', ' ').replace('-', ' ').split(' ')
 __version__ = '.'.join(map(str, version_info[:3])) + ''.join(version_info[3:])
