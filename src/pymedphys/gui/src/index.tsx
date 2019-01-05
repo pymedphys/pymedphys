66adcbcc1b7ac6a40b4392dcb1bca39950a99dfc
Simon Biggs <me@simonbiggs.net>
2019-01-05 12:27:20 +1100

    made console, output, and table within phosphor dock panel
    Former-commit-id: 988423c66441fadc3b6855dc1a42ecfaddbd1a72

--- src/pymedphys/gui/src/index.tsx
+++ src/pymedphys/gui/src/index.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style.scss';
-import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './style.css';
+import DollarsTable from './table';
+
+import { dockpanel } from './dockpanel';
+import { matplotlib, createConsole } from './jupyter';
+
+function main() {
+  dockpanel()
+  ReactDOM.render(<DollarsTable />, document.getElementById('table'));
+  document.getElementById('console')
+
+  let outputDiv = document.getElementById('output') as HTMLDivElement
+  matplotlib(outputDiv);
+
+  let consoleDiv = document.getElementById('console') as HTMLDivElement
+  createConsole(consoleDiv);
+}
+
+window.onload = main;
