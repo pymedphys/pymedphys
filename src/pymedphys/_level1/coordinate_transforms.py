2208e243c8bc98e343d7f0af5d9ccb6da7582215
Matthew Jennings <matthew.jennings2@sa.gov.au>
2019-01-29 15:36:04 +1030

    Add explicit active vs. passive transformation. Add general rotation. Test accordingly.
    Former-commit-id: aac0d8a08ea7cdfdb3689fc5e64622ebd0214742 [formerly 70f23f5a4b7db6b1171dd56ac442b6251f851e85]
    Former-commit-id: adfc3180358ddd57aa605bcfe6f26be18e99ec67

--- src/pymedphys/_level1/coordinate_transforms.py
+++ src/pymedphys/_level1/coordinate_transforms.py
@@ -25,63 +25,98 @@
 
 import numpy as np
 from numpy import sin, cos, pi, radians
+from numpy import linalg as LA
 
 from .._level0.libutils import get_imports
 IMPORTS = get_imports(globals())
 
 
-def rotate_about_x(coords_to_rotate, psi):
-    r"""Rotates a 3 x n vector of the form np.array((x, y, z)) about the x-axis
+def rotate_about_vector(coords_to_rotate, vector, theta, active=False):
+    r"""Rotates a 3 x n vector of the form np.array((x, y, z)) about the axis specified by
+    `vector`. Transforms can be active (alibi) or passive (alias). Default is passive.
+    """
+    unit_vector = vector / LA.norm(vector)
+
+    u_x = unit_vector[0]
+    u_y = unit_vector[1]
+    u_z = unit_vector[2]
+
+    s = sin(radians(theta))
+    c = cos(radians(theta))
+
+    rotation_matrix = np.array([[    c + u_x*u_x*(1-c), u_x*u_y*(1-c) - u_z*s, u_x*u_z*(1-c) + u_y*s],
+                                [u_y*u_x*(1-c) + u_z*s,     c + u_y*u_y*(1-c), u_y*u_z*(1-c) - u_x*s],
+                                [u_z*u_x*(1-c) - u_y*s, u_z*u_y*(1-c) + u_x*s,     c + u_z*u_z*(1-c)]])
+
+    # Rotation matrix above is active (unlike in other functions). Will manually transpose to avoid
+    # confusion later...
+    if not active:
+        rotation_matrix = rotation_matrix.transpose()
+
+    return rotation_matrix @ coords_to_rotate
+
+
+def rotate_about_x(coords_to_rotate, psi, active=False):
+    r"""Rotates a 3 x n vector of the form np.array((x, y, z)) about the x-axis.
+    Transforms can be active (alibi) or passive (alias), but are passive by default.
     """
     s = sin(radians(psi))
     c = cos(radians(psi))
 
-    x_rotation_matrix = np.array([[1,  0, 0],
-                                  [0,  c, s],
+    x_rotation_matrix = np.array([[1, 0, 0],
+                                  [0, c, s],
                                   [0, -s, c]])
+
+    if active:
+        x_rotation_matrix = x_rotation_matrix.transpose()
 
     return x_rotation_matrix @ coords_to_rotate
 
 
-def rotate_about_y(coords_to_rotate, phi):
+def rotate_about_y(coords_to_rotate, phi, active=False):
     r"""Rotates a 3 x n vector of the form np.array((x, y, z)) about the y-axis
+    Transforms can be active (alibi) or passive (alias), but are passive by default.
     """
     s = sin(radians(phi))
     c = cos(radians(phi))
 
     y_rotation_matrix = np.array([[c, 0, -s],
-                                  [0, 1,  0],
-                                  [s, 0,  c]])
+                                  [0, 1, 0],
+                                  [s, 0, c]])
+    if active:
+        y_rotation_matrix = y_rotation_matrix.transpose()
 
     return y_rotation_matrix @ coords_to_rotate
 
 
-def rotate_about_z(coords_to_rotate, theta):
+def rotate_about_z(coords_to_rotate, theta, active=False):
     r"""Rotates a 3 x n vector of the form np.array((x, y, z)) about the z-axis
+    Transforms can be active (alibi) or passive (alias), but are passive by default.
     """
     s = sin(radians(theta))
     c = cos(radians(theta))
 
-    z_rotation_matrix = np.array([[ c,  s, 0],
-                                  [-s,  c, 0],
-                                  [ 0,  0, 1]])
+    z_rotation_matrix = np.array([[c, s, 0],
+                                  [-s, c, 0],
+                                  [0, 0, 1]])
+
+    if active:
+        z_rotation_matrix = z_rotation_matrix.transpose()
 
     return z_rotation_matrix @ coords_to_rotate
 
 
-def translate(coords_to_translate, translation_vector):
+def translate(coords_to_translate, translation_vector, active=False):
     r"""Translates a 3 x Y array of the form np.array((x, y, z)) by a given
-    displacement vector of the same form
+    displacement vector of the same form. Transforms can be active (alibi)
+    or passive (alias), but are passive by default.
     """
+    translation_dims = np.shape(coords_to_translate)
 
-    if len(coords_to_translate.shape) == 1:
-        coords_to_translate_4d = np.append(coords_to_translate, 1)
-    else:
-        coords_to_translate_4d = np.vstack((coords_to_translate, np.ones(coords_to_translate.shape[1])))
+    for _ in translation_dims[1::]:
+        translation_vector = np.expand_dims(translation_vector, axis=-1)
 
-    translation_matrix = np.array([[1, 0, 0, translation_vector[0]],
-                                   [0, 1, 0, translation_vector[1]],
-                                   [0, 0, 1, translation_vector[2]],
-                                   [0, 0, 0,                    1]])
+    if active:
+        translation_vector = -translation_vector
 
-    return (translation_matrix @ coords_to_translate_4d)[:-1]
+    return coords_to_translate - translation_vector
