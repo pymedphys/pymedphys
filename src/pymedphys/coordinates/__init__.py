fd9ffaec5a0ab2295f02bb7b07ad8285dd410a50
SimonBiggs <me@simonbiggs.net>
2019-01-30 15:09:02 +1100

    fix coordinates.py and fix license text
    Former-commit-id: 1e794fa024c62adee5b8e5ff779b129e3bd5778f [formerly 91a65dd33b9ee2148834aa25df279269667bd960]
    Former-commit-id: d77d70c624d31960e825de9386bfe29b42085c0f

--- src/pymedphys/coordinates/__init__.py
+++ src/pymedphys/coordinates/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2018 Simon Biggs
+# Copyright (C) 2019 Matthew Jennings
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published
@@ -24,7 +24,13 @@
 # program. If not, see <http://www.apache.org/licenses/LICENSE-2.0>.
 
 
-"""
+"""A coordinates toolbox.
+
+Available Functions
+-------------------
+>>> from pymedphys.coordinates import (
+...     rotate_about_z,
+...     translate)
 """
 
 # pylint: disable=W0401,W0614
