cb2a85c887ecb590b7fb87801ef6ca4f6a246805
SimonBiggs <me@simonbiggs.net>
2019-01-30 17:09:38 +1100

    create conda envirnoment

--- scripts/azure-pipelines/02-check-nbstripout.sh
+++ scripts/azure-pipelines/02-check-nbstripout.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 set -ex
-source activate pymedphys
 
 find . -iname \*.ipynb | xargs -d "\n" nbstripout
 git add -A
 git diff HEAD --name-only --exit-code -- '*ipynb'
-
--- scripts/azure-pipelines/03-test.sh
+++ scripts/azure-pipelines/03-test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 set -ex
-source activate pymedphys
 
 python setup.py test
