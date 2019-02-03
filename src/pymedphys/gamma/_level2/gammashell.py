75de0906b0472aa72c5512ea3570c53806d8306b
Matt Jennings <Centrus.007@gmail.com>
2019-02-03 16:09:54 +1100

    Update src/pymedphys/gamma/_level2/gammashell.py
    Co-Authored-By: SimonBiggs <mail@simonbiggs.net>
    Former-commit-id: fa02ae8403dca8be10f91a93d6c00d78fc13f3e6

--- src/pymedphys/gamma/_level2/gammashell.py
+++ src/pymedphys/gamma/_level2/gammashell.py
@@ -136,7 +136,7 @@ def gamma_shell(coords_reference, dose_reference,
         print('Distance threshold set to {} mm'.format(
             options.distance_mm_threshold))
         print('Lower dose cutoff set to {} Gy ({}%)'.format(
-            options.lower_dose_cutoff, lower_percent_dose_cutoff))
+            options.lower_dose_cutoff, options.lower_percent_dose_cutoff))
         print('')
 
     current_gamma = gamma_loop(options)
