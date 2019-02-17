a99847810fef0f502d2f4c31d233ee3f3d21af47
Matt Jennings <Centrus.007@gmail.com>
2019-02-18 08:57:44 +1030

    [WIP] 50: tidy up dicom dose and coords (#54)
    * Introduce DicomDose class that contains internal coordinate handling.
    Rename certains coords to 'xyz' (since they are not coordinate triplet, but rather axis "indices".
    Introduce new coordinates tuple for Dicom dose whose x, y and z values are meshgrids of the same shape as Dicom dose grid.
    Update tesing for most of this. Convert tests to json.
    
    * keep conversion from xyz to coords in dcmdose. (Not sure about this yet)
    
    * Remove extract_dose from docs.
    
    * Remove extract_dose from __init__.py
    
    * Remove "_image" from meshgrid names
    
    * Update __init__.py and dcm.rst with change from "coords" to "xyz"
    
    * Update copyright

--- tests/test_dcm/test_dcmdose.py
+++ tests/test_dcm/test_dcmdose.py
@@ -23,78 +23,89 @@
 # program. If not, see <http://www.apache.org/licenses/LICENSE-2.0>.
 
 """A test suite for the DICOM RT Dose toolbox"""
-import os
-
-import pytest
+import json
+from os import remove
+from os.path import abspath, basename, dirname, join as pjoin
+from zipfile import ZipFile
 
 import numpy as np
-
 import pydicom as dcm
-from pymedphys.dcm import *
 
-HERE = os.path.dirname(os.path.abspath(__file__))
-DATA_DIRECTORY = os.path.join(os.path.dirname(HERE), 'data', 'dcmdose')
+from pymedphys.dcm import (
+    DicomDose,
+    extract_iec_patient_xyz,
+    extract_iec_fixed_xyz,
+    extract_dicom_patient_xyz)
+
+HERE = dirname(abspath(__file__))
+DATA_DIRECTORY = pjoin(dirname(HERE), 'data', 'dcmdose')
+ORIENTATIONS_SUPPORTED = ['FFDL', 'FFDR', 'FFP', 'FFS',
+                          'HFDL', 'HFDR', 'HFP', 'HFS']
 
 
 def get_data_file(orientation_key):
     r"""Read in test DICOM files"""
-    filename = 'RD.DICOMORIENT.Dose_{}.dcm'.format(orientation_key)
-    return os.path.join(DATA_DIRECTORY, filename)
+    filename = 'RD.DICOMORIENT.Dose_{}_empty.dcm'.format(orientation_key)
+    return pjoin(DATA_DIRECTORY, filename)
 
 
-def run_coords_function_tests(coords_function):
-    r"""Run the coordinate extraction test sequence for a given
-    coordinate extraction function"""
+def run_xyz_function_tests(xyz_function, save_new_baseline=False):
+    r"""Run the xyz extraction test sequence for a given
+    xyz extraction function"""
 
-    expected_coords_filename = "expected_{}.npy".format(
-        '_'.join(coords_function.__name__.split('_')[1:]))
+    expected_xyz_filename = "expected_{}.json".format(
+        '_'.join(xyz_function.__name__.split('_')[1:]))
 
-    expected_coords = np.load(os.path.join(
-        DATA_DIRECTORY, expected_coords_filename)).item()
+    if save_new_baseline:
+        expected_xyz = {}
+    else:
+        with open(pjoin(DATA_DIRECTORY, expected_xyz_filename)) as fp:
+            expected_xyz = json.load(fp)
 
-    assert (set(expected_coords.keys()) == set(['FFDL', 'FFDR', 'FFP', 'FFS',
-                                                'HFDL', 'HFDR', 'HFP', 'HFS']))
+        assert set(expected_xyz.keys()) == set(ORIENTATIONS_SUPPORTED)
 
     test_dcms = {key: dcm.dcmread(get_data_file(key))
-                 for key in expected_coords}
+                 for key in ORIENTATIONS_SUPPORTED}
 
     for orient, dicom in test_dcms.items():
-        test_coords = coords_function(dicom)
+        test_xyz = xyz_function(dicom)
 
-        # print(orient)
-        # print("{}, {}".format(test_coords.x[0], test_coords.x[-1]))
-        # print("{}, {}".format(test_coords.y[0], test_coords.y[-1]))
-        # print("{}, {}\n".format(test_coords.z[0], test_coords.z[-1]))
+        if save_new_baseline:
+            print(orient)
+            print("{}, {}".format(test_xyz[0][0], test_xyz[0][-1]))
+            print("{}, {}".format(test_xyz[1][0], test_xyz[1][-1]))
+            print("{}, {}\n".format(test_xyz[2][0], test_xyz[2][-1]))
 
-        # For baseline saving only!
-        # expected_coords[orient] = test_coords
+            # tolist() required for jsonification
+            test_xyz = tuple([axis.tolist() for axis in test_xyz])
+            expected_xyz[orient] = test_xyz
+        else:
+            expected_xyz[orient] = np.array(expected_xyz[orient])
+            assert np.array_equal(test_xyz[0], expected_xyz[orient][0])
+            assert np.array_equal(test_xyz[1], expected_xyz[orient][1])
+            assert np.array_equal(test_xyz[2], expected_xyz[orient][2])
 
-        assert np.array_equal(test_coords.x, expected_coords[orient].x)
-        assert np.array_equal(test_coords.y, expected_coords[orient].y)
-        assert np.array_equal(test_coords.z, expected_coords[orient].z)
-
-    # For baseline saving only!
-    # save_coords_baseline(expected_coords_filename, expected_coords)
+    if save_new_baseline:
+        save_xyz_baseline(expected_xyz_filename, expected_xyz)
 
 
-def save_coords_baseline(filename, coords_dict):
+def save_xyz_baseline(filename, xyz_dict):
     r"""Save a new baseline for any of the coordinate extraction functions.
 
-    `coords_dict` should have key : value in the following form:
-    <orientation string> : (x_coords, y_coords, z_coords)
+    `xyz_dict` should have key : value in the following form:
+    <orientation string> : (x, y, z)
     """
     tuples_are_correct_length = True
 
-    for val in coords_dict.values():
+    for val in xyz_dict.values():
         if len(val) != 3:
             tuples_are_correct_length = False
 
-    if not filename.endswith(".npy"):
-        raise ValueError("Filename must end in \".npy\"")
+    if not filename.endswith(".json"):
+        raise ValueError("Filename must end in \".json\"")
 
-    elif not (set(coords_dict.keys()) == set(['FFDL', 'FFDR', 'FFP', 'FFS',
-                                              'HFDL', 'HFDR', 'HFP', 'HFS'])):
-        raise ValueError("Coordinate baselines must be provided for "
+    elif not set(xyz_dict.keys()) == set(ORIENTATIONS_SUPPORTED):
+        raise ValueError("xyz baselines must be provided for "
                          "all eight supported patient orientations")
 
     elif not tuples_are_correct_length:
@@ -102,44 +113,57 @@ def save_coords_baseline(filename, coords_dict):
                          "of length 3 containing x, y and z values")
 
     else:
-        np.save(os.path.join(DATA_DIRECTORY, filename), coords_dict)
+        with open(pjoin(DATA_DIRECTORY, filename), 'w') as fp:
+            json.dump(xyz_dict, fp)
 
 
-@pytest.mark.skip(reason=(
-    "This test appears to have a dependency on the directory structure of "
-    "pymedphys"))
-def test_extract_iec_patient_coords():
-    run_coords_function_tests(extract_iec_patient_coords)
+def test_extract_iec_patient_xyz():
+    run_xyz_function_tests(extract_iec_patient_xyz)
 
 
-@pytest.mark.skip(reason=(
-    "This test appears to have a dependency on the directory structure of "
-    "pymedphys"))
-def test_extract_iec_fixed_coords():
-    run_coords_function_tests(extract_iec_fixed_coords)
+def test_extract_iec_fixed_xyz():
+    run_xyz_function_tests(extract_iec_fixed_xyz)
 
 
-@pytest.mark.skip(reason=(
-    "This test appears to have a dependency on the directory structure of "
-    "pymedphys"))
-def test_extract_dicom_patient_coords():
-    run_coords_function_tests(extract_dicom_patient_coords)
+def test_extract_dicom_patient_xyz():
+    run_xyz_function_tests(extract_dicom_patient_xyz)
 
 
-def test_extract_dose():
+def test_DicomDose_constancy():
 
-    expected_dose = Dose(*np.load(os.path.join(
-        DATA_DIRECTORY, "expected_wedge_dose.npy")))
+    save_new_baseline = False
 
-    test_file = os.path.join(DATA_DIRECTORY, "RD.wedge.dcm")
-    test_dcm = dcm.dcmread(test_file)
+    baseline_dcmdose_dict_filepath = pjoin(DATA_DIRECTORY, "wedge_dose_baseline.json")
+    baseline_dcmdose_dict_zippath = pjoin(DATA_DIRECTORY, "lfs-wedge_dose_baseline.zip")
 
-    test_dose = extract_dose(test_dcm)
+    test_dcmdose_filepath = pjoin(DATA_DIRECTORY, "RD.wedge.dcm")
+    test_dcmdose = DicomDose(test_dcmdose_filepath)
 
-    # Test - Constancy with wedge field baseline.
-    assert(np.array_equal(test_dose.values, expected_dose.values))
-    assert(test_dose.units == expected_dose.units)
-    assert(test_dose.type == expected_dose.type)
-    assert(test_dose.summation == expected_dose.summation)
-    assert(test_dose.heterogeneity_correction
-           == expected_dose.heterogeneity_correction)
+    if save_new_baseline:
+        # tolist() required for jsonification
+        expected_dcmdose_dict = {
+            'values': test_dcmdose.values.tolist(),
+            'units': test_dcmdose.units,
+            'x': test_dcmdose.x.tolist(),
+            'y': test_dcmdose.y.tolist(),
+            'z': test_dcmdose.z.tolist(),
+            'coords': test_dcmdose.coords.tolist(),
+            'mask': test_dcmdose.mask
+        }
+        with open(baseline_dcmdose_dict_filepath, 'w') as fp:
+            json.dump(expected_dcmdose_dict, fp)
+        ZipFile(baseline_dcmdose_dict_zippath, 'w').write(baseline_dcmdose_dict_filepath,
+                                                          basename(baseline_dcmdose_dict_filepath))
+        remove(baseline_dcmdose_dict_filepath)
+    else:
+        with ZipFile(baseline_dcmdose_dict_zippath, 'r') as zip_ref:
+            zip_ref.extractall(DATA_DIRECTORY)
+        with open(baseline_dcmdose_dict_filepath, 'r') as fp:
+            expected_dcmdose_dict = json.load(fp)
+        remove(baseline_dcmdose_dict_filepath)
+        assert np.allclose(test_dcmdose.values, np.array(expected_dcmdose_dict['values']))
+        assert test_dcmdose.units == expected_dcmdose_dict['units']
+        assert np.allclose(test_dcmdose.x, np.array(expected_dcmdose_dict['x']))
+        assert np.allclose(test_dcmdose.y, np.array(expected_dcmdose_dict['y']))
+        assert np.allclose(test_dcmdose.z, np.array(expected_dcmdose_dict['z']))
+        assert np.allclose(test_dcmdose.coords, np.array(expected_dcmdose_dict['coords']))
