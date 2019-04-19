be4340c9973c03aec4c085ec467b9fa0c46c2aa1
Matthew Jennings <Centrus.007@gmail.com>
2019-04-19 13:43:39 +0930

    Take two.

--- docs/user/library/dicom.rst
+++ docs/user/library/dicom.rst
@@ -1,11 +1,19 @@
-DICOM files
-===========
+############
+DICOM Module
+############
 
 .. automodule:: pymedphys.dicom
     :no-members:
 
+|
+###
 API
----
+###
+
+
+General
+-------
+A suite of functions that apply DICOM datasets of any modality
 
 .. _`pymedphys.geometry.cubify_cube_definition`: geometry.html#pymedphys.geometry.cubify_cube_definition
 
@@ -14,6 +22,17 @@ API
 .. autofunction:: pymedphys.dicom.anonymise_file
 
 .. autofunction:: pymedphys.dicom.anonymise_directory
+
+.. autofunction:: pymedphys.dicom.is_anonymised_dataset
+
+.. autofunction:: pymedphys.dicom.is_anonymised_file
+
+.. autofunction:: pymedphys.dicom.is_anonymised_directory
+
+|
+RT Dose
+-------
+A suite of functions that apply to DICOM RT Dose datasets
 
 .. autofunction:: pymedphys.dicom.extract_iec_patient_xyz
 
@@ -25,8 +44,19 @@ API
 
 .. autofunction:: pymedphys.dicom.load_xyz_from_dicom
 
+|
+RT Structure
+------------
+A suite of functions that apply to DICOM RT Structure sets
+
+.. autofunction:: pymedphys.dicom.get_structure_aligned_cube
+
+|
+Multiple DICOM Modality Combinations
+------------------------------------
+A suite of functions that apply to some combination of two or more different DICOM modalities
+
 .. autofunction:: pymedphys.dicom.find_dose_within_structure
 
 .. autofunction:: pymedphys.dicom.create_dvh
 
-.. autofunction:: pymedphys.dicom.get_structure_aligned_cube
--- src/pymedphys/dicom/__init__.py
+++ src/pymedphys/dicom/__init__.py
@@ -23,46 +23,27 @@
 # You should have received a copy of the Apache-2.0 along with this
 # program. If not, see <http://www.apache.org/licenses/LICENSE-2.0>.
 
-
 """A DICOM toolbox. Available functions include:
 
-General
--------
 >>> from pymedphys.dicom import (
+...
+...     # General functions
 ...     anonymise_dataset,
 ...     anonymise_file,
 ...     anonymise_directory,
 ...     is_anonymised_dataset,
 ...     is_anonymised_file,
-...     is_anonymised_directory
-)
-
-
-RT Dose
--------
-Functions related to manipulating or extracting information from DICOM
-RT Dose datasets.
->>> from pymedphys.dicom import (
+...     is_anonymised_directory,
+...
+...     # RT Dose related functions
 ...     extract_iec_patient_xyz,
 ...     extract_iec_fixed_xyz,
-...     extract_dicom_patient_xyz
-)
-
-
-RT Structure
-------------
-Functions related to manipulating or extracting information from DICOM
-RT Structure datasets.
->>> from pymedphys.dicom import (
-...     get_structure_aligned_cube
-)
-
-
-DICOM RT Combinations
----------------------
-Functions that extracting information from a combination of DICOM
-datasets, e.g. an RT Structure set and an RT Dose dataset.
->>> from pymedphys.dicom import (
+...     extract_dicom_patient_xyz,
+...
+...     # RT Structure related functions
+...     get_structure_aligned_cube,
+...
+...     # Functions using a combination of DICOM Modalities
 ...     find_dose_within_structure,
 ...     create_dvh
 )
