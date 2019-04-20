bdb53b07959f993d6ff2d8cd282b63c79a3dda52
Simon Biggs <me@simonbiggs.net>
2019-04-21 00:27:57 +1000

    xlwings dependency linting complete

---
+++ packages/pymedphys_labs/src/pymedphys_labs/_install_requires.py
@@ -0,0 +1,7 @@
+install_requires = [
+    'scipy',
+    'numpy < 1.16, >= 1.12',
+    'matplotlib',
+    'Pillow',
+    'xarray'
+]
---
+++ packages/pymedphys_xlwings/src/pymedphys_xlwings/_install_requires.py
@@ -0,0 +1,9 @@
+install_requires = [
+    'scipy',
+    'numpy < 1.16, >= 1.12',
+    'matplotlib',
+    'pandas',
+    'xlwings',
+    'pymedphys_dicom',
+    'pymedphys_fileformats'
+]
