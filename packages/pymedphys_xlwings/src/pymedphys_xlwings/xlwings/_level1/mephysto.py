c02d30b06459bda66385baf48a1fe8b3c26757ff
Simon Biggs <me@simonbiggs.net>
2019-04-20 21:41:52 +1000

    delete archive files

--- packages/pymedphys_xlwings/src/pymedphys_xlwings/xlwings/_level1/mephysto.py
+++ packages/pymedphys_xlwings/src/pymedphys_xlwings/xlwings/_level1/mephysto.py
@@ -29,7 +29,7 @@ import numpy as np
 import xlwings as xw
 
 from pymedphys_utilities.utilities import wildcard_file_resolution
-from ...mephysto import load_single_item
+from pymedphys_fileformats.mephysto import load_single_item
 
 from pymedphys_utilities.libutils import get_imports
 IMPORTS = get_imports(globals())
