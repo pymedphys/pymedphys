d5b6378c63c2e193014a6a2901d63d5fc7f96057
Simon Biggs <me@simonbiggs.net>
2019-04-22 01:12:49 +1000

    include dicom_from_dict

--- src/pymedphys/dicom.py
+++ src/pymedphys/dicom.py
@@ -4,5 +4,5 @@ from pymedphys_dicom.dicom import (
     extract_iec_patient_xyz, extract_iec_fixed_xyz,
     extract_dicom_patient_xyz, load_dose_from_dicom,
     load_xyz_from_dicom, find_dose_within_structure, create_dvh,
-    get_structure_aligned_cube
+    get_structure_aligned_cube, dicom_dataset_from_dict
 )
