be642c81f56463c8c8756e676bf44fd66535359f
SimonBiggs <me@simonbiggs.net>
2019-04-23 17:14:10 +1000

    delete bat scripts

--- scripts/developer/developer-install-with-extras.bat
+++
@@ -1,8 +0,0 @@
-conda install -y nbstripout &&^
-nbstripout --install &&^
-nbstripout --is-installed &&^
-conda install -y pymedphys --only-deps &&^
-yarn &&^
-pip install -e .[docs] &&^
-pip install -e .[testing] &&^
-pip install -e .[formatting]
--- scripts/developer/developer-install.bat
+++
@@ -1,5 +0,0 @@
-conda install -y nbstripout &&^
-nbstripout --install &&^
-nbstripout --is-installed &&^
-conda install -y pymedphys --only-deps &&^
-yarn
