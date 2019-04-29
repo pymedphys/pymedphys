5a4d88a0e9902c4e4e89df0d6c8fbf5a3ea2c698
Matthew Jennings <matthew.jennings2@sa.gov.au>
2019-04-29 10:39:36 +0930

    Remove load_xyz_from_dicom. Rename load_dose_from_dicom
    Former-commit-id: 11ca5589cc2ebacfdd137d8fd898593899b3d607 [formerly b43a85f2aebfa119723173ff8facff18f5d86cc3]
    Former-commit-id: 997f6c7fe528ee1d11aa0dab23b3ea0a9c565261

--- src/pymedphys/dicom.py
+++ src/pymedphys/dicom.py
@@ -7,8 +7,6 @@ from pymedphys_dicom.dicom import (
     is_anonymised_directory,
     coords_from_xyz_axes,
     xyz_axes_from_dataset,
-    load_dose_from_dicom,
-    load_xyz_from_dicom,
     find_dose_within_structure,
     create_dvh,
     get_structure_aligned_cube,
