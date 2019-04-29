90abb9e8791fcf9d8c682bed7549034088d36fc5
Matthew Jennings <matthew.jennings2@sa.gov.au>
2019-04-29 12:36:00 +0930

    `xyz` --> `xyz_axes`

--- packages/pymedphys_dicom/src/pymedphys_dicom/dicom/_level1/coords.py
+++ packages/pymedphys_dicom/src/pymedphys_dicom/dicom/_level1/coords.py
@@ -31,14 +31,14 @@ from pymedphys_utilities.libutils import get_imports
 IMPORTS = get_imports(globals())
 
 
-def coords_from_xyz_axes(xyz):
+def coords_from_xyz_axes(xyz_axes):
     """Converts a set of x, y and z axes of a regular grid (e.g. a DICOM
     pixel array) into an array of three grids whose voxels correspond to
     and contain the `x`, `y`, and `z` coordinates of the original grid.
 
     Parameters
     ----------
-    xyz : tuple
+    xyz_axes : tuple
         A tuple containing three `ndarrays` corresponding to the `x`,
         `y` and `z` axes of a given 3D grid - usually a DICOM dataset's
         pixel array.
@@ -51,7 +51,7 @@ def coords_from_xyz_axes(xyz):
         pixel array) from which the original axes were extracted.
     """
     ZZ, YY, XX = np.meshgrid(
-        xyz[2], xyz[1], xyz[0], indexing='ij')
+        xyz_axes[2], xyz_axes[1], xyz_axes[0], indexing='ij')
 
     coords = np.array((XX, YY, ZZ), dtype=np.float64)
     return coords
