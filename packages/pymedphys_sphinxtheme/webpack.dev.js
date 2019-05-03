0e6833469454c69702b36d27a90f6d5772971ba7
Simon Biggs <me@simonbiggs.net>
2019-05-03 13:17:25 +1000

    improve hot reloading

--- packages/pymedphys_sphinxtheme/webpack.dev.js
+++ packages/pymedphys_sphinxtheme/webpack.dev.js
@@ -5,36 +5,16 @@ const exec = require('child_process').exec;
 
 module.exports = merge(common, {
     mode: 'development',
-    plugins: [
-        {
-            apply: (compiler) => {
-                compiler.hooks.environment.tap('StartServer', (compilation) => {
-                    exec('rm -rf docs/_build', (err, stdout, stderr) => {
-                        if (stdout) process.stdout.write(stdout);
-                        if (stderr) process.stderr.write(stderr);
-                    });
-                });
-
-                compiler.hooks.watchRun.tap('CleanWhenTriggered', (compilation) => {
-                    exec('rm -rf docs/_build', (err, stdout, stderr) => {
-                        if (stdout) process.stdout.write(stdout);
-                        if (stderr) process.stderr.write(stderr);
-                    });
-                });
-
-                compiler.hooks.afterEmit.tap('AfterEmitPlugin', (compilation) => {
-                    exec('sphinx-build docs docs/_build/html', (err, stdout, stderr) => {
-                        if (stdout) process.stdout.write(stdout);
-                        if (stderr) process.stderr.write(stderr);
-                    });
-                });
-            }
-        }
-    ],
     watch: true,
     devServer: {
         contentBase: path.join(__dirname, 'docs/_build/html'),
         compress: false,
-        port: 7070
+        port: 7070,
+        after: function (app, server) {
+            exec('rm -rf docs/_build && sphinx-build docs docs/_build/html', (err, stdout, stderr) => {
+                if (stdout) process.stdout.write(stdout);
+                if (stderr) process.stderr.write(stderr);
+            });
+        }
     }
 });
