7860e5a78ea13c9c4ac762c1edb23cf75f77f6aa
Simon Biggs <me@simonbiggs.net>
2019-05-03 12:45:54 +1000

    split dev and production builds

--- packages/pymedphys_sphinxtheme/webpack.common.js
+++ packages/pymedphys_sphinxtheme/webpack.common.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const exec = require('child_process').exec;
 
 module.exports = {
     entry: './src/index.js',
@@ -50,36 +49,6 @@ module.exports = {
             // Options similar to the same options in webpackOptions.output
             // both options are optional
             filename: 'css/theme.css'
-        }),
-        {
-            apply: (compiler) => {
-                compiler.hooks.environment.tap('StartServer', (compilation) => {
-                    exec('rm -rf docs/_build', (err, stdout, stderr) => {
-                        if (stdout) process.stdout.write(stdout);
-                        if (stderr) process.stderr.write(stderr);
-                    });
-                });
-
-                compiler.hooks.watchRun.tap('CleanWhenTriggered', (compilation) => {
-                    exec('rm -rf docs/_build', (err, stdout, stderr) => {
-                        if (stdout) process.stdout.write(stdout);
-                        if (stderr) process.stderr.write(stderr);
-                    });
-                });
-
-                compiler.hooks.afterEmit.tap('AfterEmitPlugin', (compilation) => {
-                    exec('sphinx-build docs docs/_build/html', (err, stdout, stderr) => {
-                        if (stdout) process.stdout.write(stdout);
-                        if (stderr) process.stderr.write(stderr);
-                    });
-                });
-            }
-        }
-    ],
-    watch: true,
-    devServer: {
-        contentBase: path.join(__dirname, 'docs/_build/html'),
-        compress: false,
-        port: 7070
-    }
+        })
+    ]
 };
---
+++ packages/pymedphys_sphinxtheme/webpack.dev.js
@@ -0,0 +1,39 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const exec = require('child_process').exec;
+
+module.exports = merge(common, {
+    mode: 'development',
+    plugins: [
+        {
+            apply: (compiler) => {
+                compiler.hooks.environment.tap('StartServer', (compilation) => {
+                    exec('rm -rf docs/_build', (err, stdout, stderr) => {
+                        if (stdout) process.stdout.write(stdout);
+                        if (stderr) process.stderr.write(stderr);
+                    });
+                });
+
+                compiler.hooks.watchRun.tap('CleanWhenTriggered', (compilation) => {
+                    exec('rm -rf docs/_build', (err, stdout, stderr) => {
+                        if (stdout) process.stdout.write(stdout);
+                        if (stderr) process.stderr.write(stderr);
+                    });
+                });
+
+                compiler.hooks.afterEmit.tap('AfterEmitPlugin', (compilation) => {
+                    exec('sphinx-build docs docs/_build/html', (err, stdout, stderr) => {
+                        if (stdout) process.stdout.write(stdout);
+                        if (stderr) process.stderr.write(stderr);
+                    });
+                });
+            }
+        }
+    ],
+    watch: true,
+    devServer: {
+        contentBase: path.join(__dirname, 'docs/_build/html'),
+        compress: false,
+        port: 7070
+    }
+});
