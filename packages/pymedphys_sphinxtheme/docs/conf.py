e9e1b6a5c23fb9db0a27c059044d0d9162753e3b
Simon Biggs <me@simonbiggs.net>
2019-05-03 11:16:54 +1000

    move supporter placeholder into theme

--- packages/pymedphys_sphinxtheme/docs/conf.py
+++ packages/pymedphys_sphinxtheme/docs/conf.py
@@ -43,7 +43,7 @@ html_theme_options = {
     'logo_only': True
 }
 html_theme_path = ["../.."]
-html_logo = "demo/static/logo-wordmark-light.svg"
+html_logo = "demo/static/logo-pymedphys.svg"
 html_show_sourcelink = True
 
 htmlhelp_basename = slug
