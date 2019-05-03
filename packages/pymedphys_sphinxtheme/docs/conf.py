fa97068ccbf79b6804c62ccc79b91acc612cf4c1
Simon Biggs <me@simonbiggs.net>
2019-05-03 23:52:01 +1000

    [RFR] Sweep out the old to prep for the new (#245)
    * begin clean up
    
    * un uglify the css
    
    * add version changer placeholder
    
    * remove bower
    
    * remove bower.json, reset yarn lock
    
    * begin swapping over to webpack
    
    * delete packaged build artefact
    
    * ignore the js artefact
    
    * swap to webpack
    
    * remove remaining grunt
    
    * no longer distributing fonts, remove licenses
    
    * js not working, but webpack merge complete
    
    * make dependencies not dev dependencies
    
    * add netlify badge, fix version colouring
    
    * add demo docs
    
    * add auto watch server
    
    * further configuration of hot-reloading
    
    * remove intersphinx mapping
    
    * move supporter placeholder into theme
    
    * remove old placeholder
    
    * add favicon to demo docs
    
    * js working again
    
    * split dev and production builds
    
    * fix error in package.json
    
    * add webpack-merge
    
    * add path to webpack dev
    
    * move favicons across
    
    * delete favicons within the docs
    
    * improve hot reloading
    
    * collate gitignore files
    
    * remove unnesesary variable definition
    
    * speed up bootstrap by making sphinxtheme run only when needed
    
    * refine npm scripts
    
    * move from bootstrap to run
    
    * further work on npm scripts
    
    * install pylint for tox
    
    * just install xlwings on macos pipeline
    
    * add extras back into install requires for sphinxtheme
    
    * scrap sphinxcontrib.httpdomain
    
    * add pymssql to conda install for macos
    
    * add matplotlib to macos pipeline
    
    * fix test timeout
    
    
    Former-commit-id: 36b8c9b4acdca7892d6a82d149cec2fbdeaa54b0

---
+++ packages/pymedphys_sphinxtheme/docs/conf.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+
+
+from sphinx.locale import _
+import sys
+import os
+import re
+
+from pymedphys_sphinxtheme import __version__
+
+sys.path.append(os.path.abspath('..'))
+sys.path.append(os.path.abspath('./demo/'))
+
+
+project = u'Read the Docs Sphinx Theme'
+slug = re.sub(r'\W+', '-', project.lower())
+version = __version__
+release = __version__
+author = u'Dave Snider, Read the Docs, Inc. & contributors'
+copyright = author
+language = 'en'
+
+extensions = [
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.viewcode',
+    'sphinxcontrib.httpdomain',
+]
+
+templates_path = ['_templates']
+source_suffix = '.rst'
+exclude_patterns = []
+
+master_doc = 'index'
+suppress_warnings = ['image.nonlocal_uri']
+pygments_style = 'default'
+
+intersphinx_mapping = {}
+
+html_theme = 'pymedphys_sphinxtheme'
+html_theme_options = {
+    'logo_only': True
+}
+html_theme_path = ["../.."]
+html_logo = "demo/static/logo-pymedphys.svg"
+html_show_sourcelink = True
+
+htmlhelp_basename = slug
+
+latex_documents = [
+    ('index', '{0}.tex'.format(slug), project, author, 'manual'),
+]
+
+man_pages = [
+    ('index', slug, project, [author], 1)
+]
+
+texinfo_documents = [
+    ('index', slug, project, author, slug, project, 'Miscellaneous'),
+]
+
+
+# Extensions to theme docs
+def setup(app):
+    from sphinx.domains.python import PyField
+    from sphinx.util.docfields import Field
+
+    app.add_object_type(
+        'confval',
+        'confval',
+        objname='configuration value',
+        indextemplate='pair: %s; configuration value',
+        doc_field_types=[
+            PyField(
+                'type',
+                label=_('Type'),
+                has_arg=False,
+                names=('type',),
+                bodyrolename='class'
+            ),
+            Field(
+                'default',
+                label=_('Default'),
+                has_arg=False,
+                names=('default',),
+            ),
+        ]
+    )
