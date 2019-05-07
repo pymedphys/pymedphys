b2d2a4ddb162d0ce38776f8fbdd32412b3cf505f
Simon Biggs <me@simonbiggs.net>
2019-05-08 07:19:45 +1000

    move arbitrary profile to xlwings

--- src/pymedphys/geometry.py
+++ src/pymedphys/geometry.py
@@ -1,3 +1,2 @@
 from pymedphys_analysis.geometry import (
-    cubify_cube_definition, plot_cube)
-from pymedphys_dicom.dicom import get_structure_aligned_cube
+    cubify_cube_definition, plot_cube, get_structure_aligned_cube)
