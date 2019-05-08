ecb5d278f3b0c733cc760425e8142eca7810ba3f
Simon Biggs <me@simonbiggs.net>
2019-05-08 10:13:40 +1000

    rename test_import

--- conftest.py
+++ conftest.py
@@ -1,14 +1,14 @@
-"""PyTest local plugins.
-"""
+"""PyTest local plugins."""
 
 
 import os
 
 
 def pytest_ignore_collect(path, config):
-    """ return True to prevent considering this path for collection.
-    This hook is consulted for all files and directories prior to calling
-    more specific hooks.
+    """return True to prevent considering this path for collection.
+
+    This hook is consulted for all files and directories prior to
+    calling more specific hooks.
     """
 
     relative_path = os.path.relpath(str(path), os.path.dirname(__file__))
@@ -16,6 +16,5 @@ def pytest_ignore_collect(path, config):
     return (
         relative_path.startswith('notebooks') or
         relative_path.startswith('docs') or
-        'node_modules' in relative_path or
-        'xlwings' in relative_path
+        'node_modules' in relative_path
     )
