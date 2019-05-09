0892b85f7edbdace65b3f3d100b5b7205ccf19e0
Simon Biggs <me@simonbiggs.net>
2019-05-09 17:37:59 +1000

    pins now propegate to dependencies.json

--- packages/pymedphys_sphinxtheme/src/pymedphys_sphinxtheme/_version.py
+++ packages/pymedphys_sphinxtheme/src/pymedphys_sphinxtheme/_version.py
@@ -1,3 +1,2 @@
-version_info = [0, 3, 0, '']
-__version__ = '.'.join(
-    map(str, version_info[:3])) + ''.join(version_info[3:])  # type: ignore
+version_info = [0, 3, 1, 'dev2']
+__version__ = 0.3.1dev2
--- scripts/propegate_versions.py
+++ scripts/propegate_versions.py
@@ -1,7 +1,9 @@
+import os
 import json
 from glob import glob
 
 version_filepath = glob("src/pymedphys*/_version.py")[0]
+package_name = os.path.split(os.path.dirname(version_filepath))[-1]
 
 with open('package.json', 'r') as file:
     data = json.load(file)
@@ -14,10 +16,22 @@ version_info = [
     for number in loaded_version_info[0:3]
 ] + [''.join(loaded_version_info[3::])]  # type: ignore
 
-version_file_contents = """version_info = {}
 __version__ = '.'.join(
     map(str, version_info[:3])) + ''.join(version_info[3:])  # type: ignore
-""".format(version_info)
+
+version_file_contents = """version_info = {}
+__version__ = {}
+""".format(version_info, __version__)
 
 with open(version_filepath, 'w') as file:
     file.write(version_file_contents)
+
+with open('../../dependencies.json', 'r') as file:
+    dependencies_data = json.load(file)
+
+
+dependencies_data['pins']['internal'][package_name] = ">= {}".format(
+    __version__)
+
+with open('../../dependencies.json', 'w') as file:
+    json.dump(dependencies_data, file, indent=2)
