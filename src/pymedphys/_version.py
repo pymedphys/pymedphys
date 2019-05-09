a11c019dcf57644ef9fc81a23f8a37ba74b58448
Simon Biggs <me@simonbiggs.net>
2019-05-09 14:50:59 +1000

    make all python versions exist in _version.py

--- src/pymedphys/_version.py
+++ src/pymedphys/_version.py
@@ -1,17 +1,5 @@
-"""Extracts version number information from gui/package.json."""
-
 # pylint: disable=C0103
 
-import os
-import json
 
-try:
-    HERE = os.path.dirname(__file__)
-except NameError:
-    HERE = os.path.join('src', 'pymedphys')
-
-with open(os.path.join(HERE, 'gui', 'package.json')) as file:
-    data = json.load(file)
-
-version_info = data['version'].replace('.', ' ').replace('-', ' ').split(' ')
+version_info = (0, 8, 0, ".dev1")
 __version__ = '.'.join(map(str, version_info[:3])) + ''.join(version_info[3:])
