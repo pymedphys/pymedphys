59bc4af14d7053f7ccd297a8d14d7845a6915004
Simon Biggs <me@simonbiggs.net>
2019-05-10 23:46:14 +1000

    create trimmed dot file

--- scripts/create_dot_files.py
+++ scripts/create_dot_files.py
@@ -1,14 +1,15 @@
-from build_dependency_tree import build_tree
 import sys
 import os
 from glob import glob
 import subprocess
 import json
 from copy import copy, deepcopy
+import textwrap
 
 import networkx as nx
 
 sys.path.insert(0, '.')
+from build_dependency_tree import build_tree  # nopep8
 
 ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MODULE_PACKAGE_MAP = {
@@ -42,8 +43,31 @@ def full_graph():
 
     levels = get_levels(dag, internal_packages)
     dot_contents = build_dot_contents(dag, levels)
+    save_dot_file(dot_contents, 'graph_full.dot')
 
-    dot_filepath = os.path.join(ROOT, 'full_graph.dot')
+
+def trimmed_graph():
+    tree = build_tree()
+    tree.pop('pymedphys')
+    internal_packages = tuple(tree.keys())
+
+    dag = nx.DiGraph()
+
+    for key, values in tree.items():
+        dag.add_node(key)
+        dag.add_nodes_from(values['internal'])
+        edge_tuples = [
+            (key, value) for value in values['internal']
+        ]
+        dag.add_edges_from(edge_tuples)
+
+    levels = get_levels(dag, internal_packages)
+    dot_contents = build_dot_contents(dag, levels)
+    save_dot_file(dot_contents, 'graph_trimmed.dot')
+
+
+def save_dot_file(dot_contents, filename):
+    dot_filepath = os.path.join(ROOT, filename)
     with open(dot_filepath, 'w') as file:
         file.write(dot_contents)
 
@@ -81,22 +105,23 @@ def build_dot_contents(dag, levels):
     clusters = ""
 
     for level in range(max(levels.keys()) + 1):
-        cluster_packages = ';\n        '.join(levels[level])
-        clusters += """
-        subgraph cluster_{0} {{
-            {1};
-            label = "Level {0}";
-            style = dashed;
-            color = grey80;
-        }}
-        """.format(level, cluster_packages)
+        if levels[level]:
+            cluster_packages = ';\n                '.join(levels[level])
+            clusters += textwrap.indent(textwrap.dedent("""
+            subgraph cluster_{0} {{
+                {1};
+                label = "Level {0}";
+                style = dashed;
+                color = grey80;
+            }}
+            """.format(level, cluster_packages)), '        ')
 
     edges = ""
 
     for edge in dag.edges():
-        edges += "    {} -> {};\n".format(*edge)
+        edges += "        {} -> {};\n".format(*edge)
 
-    dot_file_contents = """
+    dot_file_contents = textwrap.dedent("""\
     strict digraph  {{
 
         rankdir = LR;
@@ -105,129 +130,7 @@ def build_dot_contents(dag, levels):
             width = 3;
         ];
         splines = spline;
-
-    {}
-    {}
-    }}
-    """.format(clusters, edges)
-
-    return dot_file_contents
-
-
-def create_dot_file_contents(internal_packages, package_dirs):
-    # package_dirs = list(map(os.path.dirname, glob(glob_string)))
-    # internal_packages = [
-    #     os.path.basename(directory) for directory in package_dirs]
-
-    dependencies_from_pydeps = {}
-    for directory in package_dirs:
-        package = os.path.basename(directory)
-        dependencies_from_pydeps[package] = json.loads(subprocess.run(
-            ["pydeps", directory, "--external"],
-            stdout=subprocess.PIPE).stdout)
-
-    dependencies_set = {
-        package: {
-            dependency for dependency in dependency_list
-            if dependency in internal_packages
-        }
-        for package, dependency_list in dependencies_from_pydeps.items()
-    }
-
-    dag = nx.DiGraph()
-
-    for key, values in dependencies_set.items():
-        dag.add_node(key)
-        dag.add_nodes_from(values)
-        edge_tuples = [
-            (key, value) for value in values
-        ]
-        dag.add_edges_from(edge_tuples)
-
-    assert nx.is_directed_acyclic_graph(dag)
-
-    topological = list(nx.topological_sort(dag))
-
-    minimal_deps = {}
-
-    for package in topological[::-1]:
-        if package in internal_packages:
-            already_dependend_on = set()
-            package_decendants = nx.descendants(dag, package)
-            for dependency in package_decendants:
-                if dependency in minimal_deps.keys():
-                    for already_in in nx.descendants(dag, dependency):
-                        already_dependend_on.add(already_in)
-
-            minimal_deps[package] = package_decendants.difference(
-                already_dependend_on)
-
-    minimal_dag = nx.DiGraph()
-
-    for package in minimal_deps.keys():
-        minimal_dag.add_node(package)
-
-    for package, dependencies in minimal_deps.items():
-
-        edge_tuples = [
-            (package, dependency) for dependency in dependencies
-        ]
-
-        minimal_dag.add_edges_from(edge_tuples)
-
-    topological = list(nx.topological_sort(minimal_dag))
-
-    level_map = {}
-    for package in topological[::-1]:
-        depencencies = nx.descendants(minimal_dag, package)
-        levels = {0}
-        for dependency in depencencies:
-            try:
-                levels.add(level_map[dependency])
-            except KeyError:
-                pass
-        max_level = max(levels)
-        level_map[package] = max_level + 1
-
-    levels = {
-        i + 1: []
-        for i in range(max(level_map.values()))
-    }
-    for package, level in level_map.items():
-        levels[level].append(package)
-
-    clusters = ""
-
-    for i in range(max(level_map.values())):
-        level = i + 1
-        cluster_packages = ';\n        '.join(levels[level])
-        clusters += """
-        subgraph cluster_{} {{
-            {};
-            label = "Level {}";
-            style = dashed;
-            color = grey80;
-        }}
-        """.format(i, cluster_packages, level)
-
-    edges = ""
-
-    for edge in minimal_dag.edges():
-        edges += "    {} -> {};\n".format(*edge)
-
-    dot_file_contents = """
-    strict digraph  {{
-
-        rankdir = LR;
-        node [
-            shape = box;
-            width = 3;
-        ];
-        splines = polyline;
-
-    {}
-    {}
-    }}
-    """.format(clusters, edges)
+    {}\n{}    }}
+    """.format(clusters, edges))
 
     return dot_file_contents
