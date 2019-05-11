1f56ffc618d654a76edc1ce1ead6350b18b2c577
Simon Biggs <me@simonbiggs.net>
2019-05-11 15:51:03 +1000

    group same levelled packages

--- packages/pymedphys_monomanage/src/pymedphys_monomanage/tree/draw.py
+++ packages/pymedphys_monomanage/src/pymedphys_monomanage/tree/draw.py
@@ -5,6 +5,7 @@ import subprocess
 import json
 from copy import copy, deepcopy
 import textwrap
+import shutil
 
 import networkx as nx
 
@@ -14,31 +15,31 @@ ROOT = os.getcwd()
 
 
 def draw_all(save_directory):
-    full_graph(save_directory)
+    # full_graph(save_directory)
     trimmed_graph(save_directory)
 
 
-def full_graph(save_directory):
-    tree = build_tree()
-    internal_packages = tuple(tree.keys())
-    dag = nx.DiGraph()
+# def full_graph(save_directory):
+#     tree = build_tree()
+#     internal_packages = tuple(tree.keys())
+#     dag = nx.DiGraph()
 
-    for key, values in tree.items():
-        dag.add_node(key)
-        dag.add_nodes_from(values['internal'])
-        dag.add_nodes_from(values['external'])
-        edge_tuples = [
-            (key, value) for value in values['internal']
-        ]
-        dag.add_edges_from(edge_tuples)
-        edge_tuples = [
-            (key, value) for value in values['external']
-        ]
-        dag.add_edges_from(edge_tuples)
+#     for key, values in tree.items():
+#         dag.add_node(key)
+#         dag.add_nodes_from(values['internal'])
+#         dag.add_nodes_from(values['external'])
+#         edge_tuples = [
+#             (key, value) for value in values['internal']
+#         ]
+#         dag.add_edges_from(edge_tuples)
+#         edge_tuples = [
+#             (key, value) for value in values['external']
+#         ]
+#         dag.add_edges_from(edge_tuples)
 
-    levels = get_levels(dag, internal_packages)
-    dot_contents = build_dot_contents(dag, levels)
-    save_dot_file(dot_contents, save_directory, 'graph_full.dot')
+#     levels = get_levels(dag, internal_packages)
+#     dot_contents = build_dot_contents(dag, levels)
+#     save_dot_file(dot_contents, save_directory, 'graph_full.dot')
 
 
 def trimmed_graph(save_directory):
@@ -46,9 +47,14 @@ def trimmed_graph(save_directory):
     tree.pop('pymedphys')
     internal_packages = tuple(tree.keys())
 
+    keys = list(tree.keys())
+    keys.sort(reverse=True)
+
     dag = nx.DiGraph()
 
-    for key, values in tree.items():
+    for key in keys:
+        values = tree[key]
+
         dag.add_node(key)
         dag.add_nodes_from(values['internal'])
         edge_tuples = [
@@ -58,17 +64,18 @@ def trimmed_graph(save_directory):
 
     levels = get_levels(dag, internal_packages)
     dot_contents = build_dot_contents(dag, levels)
-    save_dot_file(dot_contents, save_directory, 'graph_trimmed.dot')
+    save_dot_file(dot_contents, os.path.join(save_directory, 'pymedphys.svg'))
 
 
-def save_dot_file(dot_contents, save_directory, filename):
-    dot_filepath = os.path.join(save_directory, filename)
-    with open(dot_filepath, 'w') as file:
+def save_dot_file(dot_contents, outfilepath):
+    with open("temp.dot", 'w') as file:
         file.write(dot_contents)
 
-    svg_filepath = os.path.splitext(dot_filepath)[0] + ".svg"
+    os.system("cat temp.dot | tred | dot -Tsvg -o temp.svg")
+    os.remove("temp.dot")
 
-    subprocess.call(["dot", "-Tsvg", dot_filepath, "-o", svg_filepath])
+    shutil.move("temp.svg", outfilepath)
+
 
 
 def get_levels(dag, internal_packages):
@@ -100,36 +107,50 @@ def get_levels(dag, internal_packages):
     return levels
 
 
+def remove_prefix(text, prefix):
+    if text.startswith(prefix):
+        return text[len(prefix):]
+    else:
+        raise ValueError("Prefix not found.")
+
+
 def build_dot_contents(dag, levels):
-    clusters = ""
+    nodes = ""
+
+    for node in dag.nodes():
+        trimmed_node = remove_prefix(node, 'pymedphys_')
+        nodes += "{};\n".format(trimmed_node)
+
+    nodes = ""
 
     for level in range(max(levels.keys()) + 1):
         if levels[level]:
-            cluster_packages = ';\n                '.join(levels[level])
-            clusters += textwrap.indent(textwrap.dedent("""
-            subgraph cluster_{0} {{
-                {1};
-                label = "Level {0}";
-                style = dashed;
-                color = grey80;
-            }}
-            """.format(level, cluster_packages)), '        ')
+            trimmed_nodes = [
+                remove_prefix(node, 'pymedphys_') for node in levels[level]
+            ]
+
+            grouped_packages = '; '.join(trimmed_nodes)
+            nodes += """
+            {{ rank = same; {}; }}
+            """.format(grouped_packages)
 
     edges = ""
 
     for edge in dag.edges():
-        edges += "        {} -> {};\n".format(*edge)
+        trimmed_edge = [
+            remove_prefix(node, 'pymedphys_') for node in edge
+        ]
+        edges += "{} -> {};\n".format(*trimmed_edge)
 
-    dot_file_contents = textwrap.dedent("""\
-    strict digraph  {{
+    dot_file_contents = """
+        strict digraph  {{
 
-        rankdir = LR;
-        node [
-            shape = box;
-            width = 3;
-        ];
-        splines = spline;
-    {}\n{}    }}
-    """.format(clusters, edges))
+            rankdir = LR;
+            splines = splines;
+
+
+            {}\n{}
+        }}
+    """.format(nodes, edges)
 
     return dot_file_contents
