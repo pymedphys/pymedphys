db8cc792ad469ed300b5f58d9024f2cca3f93af4
Simon Biggs <me@simonbiggs.net>
2019-05-11 23:08:41 +1000

    wrap scripts in main function

--- packages/pymedphys_monomanage/src/pymedphys_monomanage/propegate/dependencies.py
+++ packages/pymedphys_monomanage/src/pymedphys_monomanage/propegate/dependencies.py
@@ -5,77 +5,84 @@ from glob import glob
 
 ROOT = os.getcwd()
 
-with open(os.path.join(ROOT, 'dependencies.json'), 'r') as file:
-    dependencies_data = json.load(file)
+
+def main():
+    with open(os.path.join(ROOT, 'dependencies.json'), 'r') as file:
+        dependencies_data = json.load(file)
 
 
-tree = dependencies_data['tree']
-pypi_pins = dependencies_data['pins']['pypi']
-npm_pins = dependencies_data['pins']['npm']
+    tree = dependencies_data['tree']
+    pypi_pins = dependencies_data['pins']['pypi']
+    npm_pins = dependencies_data['pins']['npm']
 
-internal_packages = [
-    os.path.basename(filepath)
-    for filepath in glob(os.path.join(ROOT, 'packages', '*'))
-]
+    internal_packages = [
+        os.path.basename(filepath)
+        for filepath in glob(os.path.join(ROOT, 'packages', '*'))
+    ]
 
-assert set(internal_packages) == set(tree.keys())
-assert set(internal_packages) == set(pypi_pins['internal'].keys())
-assert set(internal_packages) == set(npm_pins['internal'].keys())
-
-
-for package, dependency_store in tree.items():
-    install_requires = []
-
-    keys_to_keep = {'internal', 'external'}
-    for where, dependencies in dependency_store.items():
-        if where in keys_to_keep:
-            for dependency in dependencies:
-                try:
-                    pin = " " + pypi_pins[where][dependency]
-                except KeyError:
-                    pin = ""
-
-                requirement_string = dependency + pin
-                install_requires.append(requirement_string)
-
-    install_requires.sort()
-
-    install_requires_filepath = os.path.join(
-        ROOT, "packages", package, "src", package, "_install_requires.py")
-
-    install_requires_contents = textwrap.dedent("""\
-        install_requires = {}
-    """).format(json.dumps(install_requires, indent=4))
-
-    with open(install_requires_filepath, 'w') as file:
-        file.write(install_requires_contents)
+    assert set(internal_packages) == set(tree.keys())
+    assert set(internal_packages) == set(pypi_pins['internal'].keys())
+    assert set(internal_packages) == set(npm_pins['internal'].keys())
 
 
-for package, dependency_store in tree.items():
-    internal_dependencies = {
-        dependency: npm_pins['internal'][dependency]
-        for dependency in dependency_store['internal']
-    }
+    for package, dependency_store in tree.items():
+        install_requires = []
 
-    package_json_filepath = os.path.join(
-        ROOT, "packages", package, "package.json")
+        keys_to_keep = {'internal', 'external'}
+        for where, dependencies in dependency_store.items():
+            if where in keys_to_keep:
+                for dependency in dependencies:
+                    try:
+                        pin = " " + pypi_pins[where][dependency]
+                    except KeyError:
+                        pin = ""
 
-    with open(package_json_filepath, 'r') as file:
-        data = json.load(file)
+                    requirement_string = dependency + pin
+                    install_requires.append(requirement_string)
 
-    try:
-        external_dependencies = {
-            package: pin
-            for package, pin in data['dependencies'].items()
-            if package not in internal_packages
+        install_requires.sort()
+
+        install_requires_filepath = os.path.join(
+            ROOT, "packages", package, "src", package, "_install_requires.py")
+
+        install_requires_contents = textwrap.dedent("""\
+            install_requires = {}
+        """).format(json.dumps(install_requires, indent=4))
+
+        with open(install_requires_filepath, 'w') as file:
+            file.write(install_requires_contents)
+
+
+    for package, dependency_store in tree.items():
+        internal_dependencies = {
+            dependency: npm_pins['internal'][dependency]
+            for dependency in dependency_store['internal']
         }
-    except KeyError:
-        external_dependencies = {}
 
-    data['dependencies'] = {
-        **internal_dependencies,
-        **external_dependencies
-    }
+        package_json_filepath = os.path.join(
+            ROOT, "packages", package, "package.json")
 
-    with open(package_json_filepath, 'w') as file:
-        json.dump(data, file, indent=2, sort_keys=True)
+        with open(package_json_filepath, 'r') as file:
+            data = json.load(file)
+
+        try:
+            external_dependencies = {
+                package: pin
+                for package, pin in data['dependencies'].items()
+                if package not in internal_packages
+            }
+        except KeyError:
+            external_dependencies = {}
+
+        data['dependencies'] = {
+            **internal_dependencies,
+            **external_dependencies
+        }
+
+        with open(package_json_filepath, 'w') as file:
+            json.dump(data, file, indent=2, sort_keys=True)
+
+
+
+if __name__ == "__main__":
+    main()
