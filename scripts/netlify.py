e87b013a03fa7d052726e6e986fc680dbe25cafc
Simon Biggs <me@simonbiggs.net>
2019-05-18 21:45:55 +1000

    add yarn before yarn build

--- scripts/netlify.py
+++ scripts/netlify.py
@@ -11,7 +11,7 @@ if os.environ['SITE'] == 'docs':
 elif os.environ['SITE'] == 'app':
     os.system('yarn monomanage:install')
     os.system('yarn app:wheels')
-    os.system('cd app && yarn build && cd ../')
+    os.system('cd app && yarn && yarn build && cd ../')
     os.system('mv app/build/* site')
 
 elif os.environ['SITE'] == 'home':
