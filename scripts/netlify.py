ede067820712c132558a4ea76014e12ba5e199eb
Simon Biggs <me@simonbiggs.net>
2019-05-18 21:20:08 +1000

    move contents to site

--- scripts/netlify.py
+++ scripts/netlify.py
@@ -6,13 +6,13 @@ if os.environ['SITE'] == 'docs':
     os.system("yarn install:prod && yarn pip:install:docs")
     os.system(
         "export PATH=`pwd`/bin:$PATH && yarn docs:prebuild && sphinx-build -W docs docs/_build/html")
-    os.system("mv docs/_build/html site")
+    os.system("mv docs/_build/html/* site")
 
 elif os.environ['SITE'] == 'app':
     os.system('yarn monomanage:install')
     os.system('yarn app:wheels')
     os.system('pushd app && yarn build && popd')
-    os.system('mv app/build site')
+    os.system('mv app/build/* site')
 
 elif os.environ['SITE'] == 'home':
     pass
