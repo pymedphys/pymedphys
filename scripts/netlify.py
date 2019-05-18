57c04d3aa3f2f092aa52e5ce8b7152cd086b4690
Simon Biggs <me@simonbiggs.net>
2019-05-18 19:25:50 +1000

    make app pull the wheels
    Former-commit-id: 85a0f6338096237f587029a5efdf351c242d0445 [formerly 6da5b36fa14a424fb543a2ea10497e263b57b080]
    Former-commit-id: bc86d6387b60f5c96c4037c870490d87ced6c501

--- scripts/netlify.py
+++ scripts/netlify.py
@@ -6,4 +6,5 @@ if os.environ['SITE'] == 'docs':
         "export PATH=`pwd`/bin:$PATH && yarn docs:prebuild && sphinx-build -W docs docs/_build/html")
     os.system("mv docs/_build/html site")
 elif os.environ['SITE'] == 'app':
+    os.system('yarn app:wheels')
     os.system('mv app site')
