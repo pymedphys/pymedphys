5a4f02c57a2e83044e701383710eeb0a51414cf1
Simon Biggs <me@simonbiggs.net>
2019-05-18 20:33:32 +1000

    fix app not home

--- scripts/netlify.py
+++ scripts/netlify.py
@@ -8,8 +8,8 @@ if os.environ['SITE'] == 'docs':
 
 elif os.environ['SITE'] == 'app':
     os.system('yarn app:wheels')
-    os.system('mv app site')
-
-elif os.environ['SITE'] == 'home':
     os.system('pushd app && yarn build && popd')
     os.system('mv app/build site')
+
+elif os.environ['SITE'] == 'home':
+    os.system('mkdir site')
