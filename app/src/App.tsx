722931ae2cd35b352a0dff40f504dadff11d58e5
Simon Biggs <me@simonbiggs.net>
2019-05-19 22:55:47 +1000

    use rxjs

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,58 +1,99 @@
 import React from 'react';
-import './App.css';
+
+import { Subscription } from 'rxjs';
+
+import { FileInput, H2, Button } from '@blueprintjs/core';
+
 import raw from "raw.macro";
-import { FileInput, H2 } from '@blueprintjs/core';
 import { saveAs } from 'file-saver';
 
+import './App.css';
+
+import { wheelsReady } from './observables/wheels'
+import { inputDirectory, outputDirectory } from './observables/directories'
 
 const decodeTRFPythonCode = raw("./python/decode_trf.py");
 declare let pyodide: any;
 declare var Module: any;
 
 
+function runConversion() {
+  pyodide.runPythonAsync(decodeTRFPythonCode).then(() => {
+    let headerFilepath = pyodide.pyimport("header_filename")
+    let tableFilepath = pyodide.pyimport("table_filename")
+
+    let header = Module.FS.readFile(headerFilepath) as Uint8Array
+    let table = Module.FS.readFile(tableFilepath) as Uint8Array
+    saveAs(new Blob([new Uint8Array(header)]), headerFilepath)
+    saveAs(new Blob([new Uint8Array(table)]), tableFilepath)
+  })
+}
 
 function onFileInputChange(event: React.FormEvent<HTMLInputElement>) {
   let fileInput = event.target as HTMLInputElement;
 
-  window.wheelsPromise.promise.then(() => {
-    let files = fileInput.files as FileList;
-    let fileArray = Array.from(files);
-    fileInput.value = '';
+  let files = fileInput.files as FileList;
+  let fileArray = Array.from(files);
+  fileInput.value = '';
 
-    fileArray.forEach(file => {
-      let fr = new FileReader();
-      fr.onload = function () {
-        let result = fr.result as ArrayBuffer
-        var data = new Uint8Array(result);
+  fileArray.forEach(file => {
+    let fr = new FileReader();
+    fr.onload = function () {
+      let result = fr.result as ArrayBuffer
+      var data = new Uint8Array(result);
 
-        Module['FS_createDataFile']('/', file.name, data, true, true, true);
-        pyodide.runPythonAsync(decodeTRFPythonCode).then(() => {
-          let headerFilepath = pyodide.pyimport("header_filename")
-          let tableFilepath = pyodide.pyimport("table_filename")
+      Module['FS_createDataFile']('/input/', file.name, data, true, true, true);
+      inputDirectory.next(inputDirectory.getValue().add(file.name))
+    };
 
-
-          let header = Module.FS.readFile(headerFilepath) as Uint8Array
-          let table = Module.FS.readFile(tableFilepath) as Uint8Array
-          saveAs(new Blob([new Uint8Array(header)]), headerFilepath)
-          saveAs(new Blob([new Uint8Array(table)]), tableFilepath)
-        })
-      };
-
-      fr.readAsArrayBuffer(file);
-    })
-
+    fr.readAsArrayBuffer(file);
   })
 }
 
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <H2>Testing trf decoding</H2>
-      <p><a href="/data/vmat.trf">Download a demo .trf file</a></p>
-      <FileInput id="trfFileInput" text="Choose file..." onInputChange={onFileInputChange} />
-    </div>
-  );
+interface AppProps {
+
+}
+
+
+
+
+
+class App extends React.Component {
+  subscription!: Subscription
+  state: Readonly<{
+    areWheelsReady: boolean
+  }>
+
+  constructor(props: AppProps) {
+    super(props)
+    this.state = {
+      areWheelsReady: false
+    }
+  }
+
+  componentDidMount() {
+    this.subscription = wheelsReady.subscribe(areWheelsReady => {
+      this.setState({
+        areWheelsReady: areWheelsReady
+      })
+    })
+  }
+
+  componentWillUnmount() {
+    this.subscription.unsubscribe()
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <H2>Testing trf decoding</H2>
+        <p><a href="/data/vmat.trf">Download a demo .trf file</a></p>
+        <FileInput id="trfFileInput" text="Choose file..." onInputChange={onFileInputChange} />
+        <Button icon="refresh" onClick={runConversion} disabled={!this.state.areWheelsReady} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- app/src/python/load_wheels.py
+++ app/src/python/load_wheels.py
@@ -96,6 +96,10 @@ def load_and_copy_wheels():
         def extract_all_wheels(filenames_json):
             filenames = json.load(filenames_json)
 
+            for filename in filenames:
+                print('Loading {} from python-wheels'.format(
+                    filename.split('-')[0]))
+
             urls = [
                 '/python-wheels/{}'.format(filename)
                 for filename in filenames
@@ -112,20 +116,32 @@ def load_and_copy_wheels():
     return Promise.new(run_promise)
 
 
-pydicom_data = (
-    'https://files.pythonhosted.org/packages/97/ae/93aeb6ba65cf976a23e735e9d32b0d1ffa2797c418f7161300be2ec1f1dd/pydicom-1.2.0-py2.py3-none-any.whl',
-    '2132a9b15a927a1c35a757c0bdef30c373c89cc999cf901633dcd0e8bdd22e84'
-)
+def get_packages(pypi_data):
+    promises = []
+    for key, package_data in pypi_data.items():
+        print('Loading {} from pypi'.format(key))
+        promises.append(get_package(*package_data))
 
-packaging_data = (
-    'https://files.pythonhosted.org/packages/91/32/58bc30e646e55eab8b21abf89e353f59c0cc02c417e42929f4a9546e1b1d/packaging-19.0-py2.py3-none-any.whl',
-    '9e1cbf8c12b1f1ce0bb5344b8d7ecf66a6f8a6e91bcb0c84593ed6d3ab5c4ab3'
-)
+    return Promise.all(promises)
 
-get_package(*pydicom_data).then(
-    lambda x: get_package(*packaging_data)
+
+pypi_data = {
+    'pydicom': (
+        'https://files.pythonhosted.org/packages/97/ae/93aeb6ba65cf976a23e735e9d32b0d1ffa2797c418f7161300be2ec1f1dd/pydicom-1.2.0-py2.py3-none-any.whl',
+        '2132a9b15a927a1c35a757c0bdef30c373c89cc999cf901633dcd0e8bdd22e84'
+    ),
+    'packaging': (
+        'https://files.pythonhosted.org/packages/91/32/58bc30e646e55eab8b21abf89e353f59c0cc02c417e42929f4a9546e1b1d/packaging-19.0-py2.py3-none-any.whl',
+        '9e1cbf8c12b1f1ce0bb5344b8d7ecf66a6f8a6e91bcb0c84593ed6d3ab5c4ab3'
+    ),
+    'Rx': (
+        'https://files.pythonhosted.org/packages/33/0f/5ef4ac78e2a538cc1b054eb86285fe0bf7a5dbaeaac2c584757c300515e2/Rx-1.6.1-py2.py3-none-any.whl',
+        '7357592bc7e881a95e0c2013b73326f704953301ab551fbc8133a6fadab84105'
+    )
+}
+
+get_packages(pypi_data).then(
+    lambda _: load_and_copy_wheels()
 ).then(
-    lambda x: load_and_copy_wheels()
-).then(
-    lambda x: window['wheelsPromise'].resolve()
+    lambda _: window['wheelsReady'].next(True)
 )
