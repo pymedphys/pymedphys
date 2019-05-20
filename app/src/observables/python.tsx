162fb53c8f747d7f32ee4339260ea1cca5310985
SimonBiggs <me@simonbiggs.net>
2019-05-20 16:46:10 +1000

    run yarn tree
    Former-commit-id: cdea171b24da2369c54de3e99c8a733b7eb404d2

--- app/src/observables/python.tsx
+++ app/src/observables/python.tsx
@@ -1,3 +1,11 @@
 import { BehaviorSubject } from 'rxjs';
 
+export interface IPythonData {
+    gantryAngle: number
+}
+
+
 export const pythonReady = new BehaviorSubject<boolean>(false);
+export const pythonData = new BehaviorSubject<IPythonData>({
+    gantryAngle: -120
+});
