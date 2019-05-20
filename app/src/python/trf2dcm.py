c5fdef48d27f2b638c27a66170072f24cbbb7dd9
SimonBiggs <me@simonbiggs.net>
2019-05-20 16:47:59 +1000

    fix gantry angle lookup
    Former-commit-id: ff4c3dab9786276592a37308aaa726783b1be80f

--- app/src/python/trf2dcm.py
+++ app/src/python/trf2dcm.py
@@ -12,7 +12,7 @@ from pymedphys_fileformats.trf.trf2csv import trf2csv_by_directory
 def run_trf2dcm():
     def run_promise(resolve, reject):
         try:
-            gantry_angle = window['pythonData']['gantryAngle']
+            gantry_angle = window['pythonData'].getValue()['gantryAngle']
             dcm_template_filepath = glob(os.path.join('/input', '*.dcm'))[0]
             trf_filepath = glob(os.path.join('/input', '*.trf'))[0]
             output_dir = '/output'
