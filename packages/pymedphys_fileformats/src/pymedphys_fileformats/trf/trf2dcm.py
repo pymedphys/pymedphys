05df1f123ba968f825978a2016ae2c45c87c70be
SimonBiggs <me@simonbiggs.net>
2019-05-21 15:11:25 +1000

    fix trf2dcm function usage
    Former-commit-id: 6a8ff75f32395f9d4662b50653ca89eead3806d9 [formerly 4c8ceaa2890b10f15e83eed479b8c6457f516dce]
    Former-commit-id: d30faae0c2cc33f1bc98545851d4d23a5073515e

--- packages/pymedphys_fileformats/src/pymedphys_fileformats/trf/trf2dcm.py
+++ packages/pymedphys_fileformats/src/pymedphys_fileformats/trf/trf2dcm.py
@@ -6,13 +6,12 @@ from pymedphys_core.deliverydata import (
 from .trf2deliverydata import delivery_data_from_logfile
 
 
-def trf2dcm(dcm_template_filepath, trf_filepath, gantry_angle, output_dir):
+def trf2dcm(dcm_template_filepath, trf_filepath, output_dir):
     trf_filename = os.path.basename(trf_filepath)
     filepath_out = os.path.join(output_dir, "{}.dcm".format(trf_filename))
 
     dicom_template = pydicom.read_file(dcm_template_filepath, force=True)
     delivery_data = delivery_data_from_logfile(trf_filepath)
-    edited_dicom = delivery_data_to_dicom(
-        delivery_data, dicom_template, gantry_angle)
+    edited_dicom = delivery_data_to_dicom(delivery_data, dicom_template)
 
     edited_dicom.save_as(filepath_out)
