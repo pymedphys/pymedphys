abf6a1c3e4201d3179a6634dedea6c8d7129f03f
Simon Biggs <me@simonbiggs.net>
2019-05-21 20:17:26 +1000

    page design
    Former-commit-id: f92b013a967e951a415a5b01d07bc9533300ffd9 [formerly e89beef404005c35c58c44f03dc98872e223f02c]
    Former-commit-id: a03c9596e41d3a5327cdf3a1f88f6633f64a7f41

---
+++ app/src/components/main.tsx
@@ -0,0 +1,195 @@
+import React from 'react';
+
+import { Subscription } from 'rxjs';
+
+import {
+  FileInput, H2, Button, ProgressBar,
+  Drawer
+} from '@blueprintjs/core';
+
+import MonacoEditor from 'react-monaco-editor';
+
+import { saveAs } from 'file-saver';
+
+import { AppNavbar } from './navbar';
+import { AppText } from './text';
+import { AppFileTree } from './file-tree'
+
+import {
+  pythonReady, pythonData, IPythonData, pythonCode
+} from '../observables/python'
+import { inputDirectory, outputDirectory } from '../observables/directories'
+
+import trf2dcm from '../python/trf2dcm.py';
+import zipOutput from '../python/zip_output.py';
+import updateOutput from '../python/update_output.py';
+
+declare let pyodide: any;
+declare var Module: any;
+
+
+function runConversion() {
+  pyodide.runPython(trf2dcm)
+    .catch(() => {
+      pyodide.runPython(updateOutput)
+    })
+    .then(() => {
+      pyodide.runPython(updateOutput)
+    })
+}
+
+function downloadOutput() {
+  pyodide.runPython(zipOutput).then(() => {
+    let zip = Module.FS.readFile('/output.zip') as Uint8Array
+    saveAs(new Blob([new Uint8Array(zip)]), 'output.zip')
+  })
+}
+
+function onFileInputChange(event: React.FormEvent<HTMLInputElement>) {
+  let fileInput = event.target as HTMLInputElement;
+
+  let files = fileInput.files as FileList;
+  let fileArray = Array.from(files);
+  fileInput.value = '';
+
+  fileArray.forEach(file => {
+    let fr = new FileReader();
+    fr.onload = function () {
+      let result = fr.result as ArrayBuffer
+      var data = new Uint8Array(result);
+
+      Module['FS_createDataFile']('/input/', file.name, data, true, true, true);
+      inputDirectory.next(inputDirectory.getValue().add(file.name))
+    };
+
+    fr.readAsArrayBuffer(file);
+  })
+}
+
+interface IAppMainProps {
+
+}
+
+interface IAppMainState extends Readonly<{}> {
+  isPythonReady: boolean;
+  pythonData: IPythonData;
+  codeIsOpen: boolean;
+}
+
+export class AppMain extends React.Component<IAppMainProps, IAppMainState> {
+  subscriptions: Subscription[] = []
+  editorSubscription: Subscription | undefined = undefined
+
+  constructor(props: IAppMainProps) {
+    super(props)
+    this.state = {
+      isPythonReady: false,
+      pythonData: pythonData.getValue(),
+      codeIsOpen: false,
+    }
+  }
+
+  componentDidMount() {
+    this.subscriptions.push(
+      pythonReady.subscribe(isPythonReady => {
+        this.setState({
+          isPythonReady: isPythonReady
+        })
+      })
+    )
+    this.subscriptions.push(
+      pythonData.subscribe(data => {
+        this.setState({
+          pythonData: data
+        })
+      })
+    )
+  }
+
+  componentWillUnmount() {
+    this.subscriptions.forEach(subscription => {
+      subscription.unsubscribe();
+    })
+  }
+
+  private showCode = () => this.setState({ codeIsOpen: true });
+  private hideCode = () => {
+    this.setState({ codeIsOpen: false })
+    let editorSubscription = this.editorSubscription as Subscription
+    editorSubscription.unsubscribe()
+  };
+
+  private editorOnChange(code: string, event: any) {
+    pythonCode.next(code)
+  }
+
+  private editorDidMount = (editor: any, monaco: any) => {
+    editor.value = pythonCode.getValue()
+    editor.focus()
+    this.editorSubscription = pythonCode.subscribe(value => {
+      editor.value = value
+    })
+  }
+
+  render() {
+    return (
+      <div className="AppMain">
+        <AppNavbar />
+        <AppText />
+
+        <div hidden={this.state.isPythonReady}>
+          <H2>Currently Loading Python...</H2>
+          <p>Before any file processing begins you need to finish downloading and initialising Python and the required packages.</p>
+          <ProgressBar intent="primary" />
+        </div>
+
+        <H2>File management</H2>
+
+        <div>
+          <FileInput inputProps={{ multiple: true }} id="trfFileInput" text="Choose file..." onInputChange={onFileInputChange} disabled={!this.state.isPythonReady} />
+        </div>
+
+        <br></br>
+
+        <H2>File processing</H2>
+
+        <AppFileTree />
+
+        <br></br>
+
+        <span className="floatleft">
+          <Button
+            intent="primary"
+            text="Process Files"
+            icon="key-enter"
+            onClick={runConversion}
+            disabled={!this.state.isPythonReady || inputDirectory.getValue().size === 0} />
+        </span>
+        <span className="floatright">
+          <Button
+            intent="success"
+            text="Save output"
+            icon="download"
+            onClick={downloadOutput}
+            disabled={!this.state.isPythonReady || outputDirectory.getValue().size === 0} />
+        </span>
+
+        <Button text="Show code" onClick={this.showCode}></Button>
+
+        <Drawer
+          onClose={this.hideCode}
+          isOpen={this.state.codeIsOpen}
+        >
+          <div className="monaco-container">
+            <MonacoEditor
+              language="python" width="100%" height="100%"
+              onChange={this.editorOnChange}
+              editorDidMount={this.editorDidMount}
+              value={pythonCode.getValue()}
+            />
+          </div>
+        </Drawer>
+      </div>
+    );
+  }
+}
---
+++ app/src/components/navbar.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import {
+  Navbar, NavbarGroup, NavbarHeading, NavbarDivider, Alignment, AnchorButton,
+  Button
+} from '@blueprintjs/core';
+
+import logo from '../logo.svg';
+
+export class AppNavbar extends React.Component {
+  render() {
+    return (
+
+      <Navbar fixedToTop>
+        <NavbarGroup align={Alignment.LEFT}>
+          <img className='logo bp3-icon' src={logo} alt="Logo" />
+          <NavbarHeading>PyMedPhys</NavbarHeading>
+          <NavbarDivider />
+          <Button
+            className="bp3-minimal" icon="home" text="Home" />
+        </NavbarGroup>
+        <NavbarGroup align={Alignment.RIGHT}>
+          <AnchorButton
+            className="bp3-minimal" icon="book" text="Documentation"
+            href="https://docs.pymedphys.com" />
+          <AnchorButton
+            className="bp3-minimal" icon="git-repo" text="GitHub"
+            href="https://github.com/pymedphys/pymedphys" />
+        </NavbarGroup>
+      </Navbar>
+
+    )
+  }
+}
