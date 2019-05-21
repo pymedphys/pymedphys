283fb18d3e91184c8d3b2be48f5c8b0d34eb4dec
Simon Biggs <me@simonbiggs.net>
2019-05-21 21:36:24 +1000

    code now showing up within side panel

--- app/src/python/user-scripts/options.tsx
+++ app/src/python/user-scripts/options.tsx
@@ -2,13 +2,13 @@ import trf2csv from './trf2csv.py';
 import muDensityDiff from './mu-density-diff.py';
 import trf2dcm from './trf2dcm.py';
 
-export interface IUserScripts {
+export interface IUserScript {
     name: string;
     description: string;
     code: string;
 }
 
-export const USER_SCRIPTS: IUserScripts[] = [
+export const USER_SCRIPTS: IUserScript[] = [
     {
         name: "trf2csv", code: trf2csv,
         description: "Convert files that end in `.trf` into header and table `.csv` files."
@@ -19,6 +19,6 @@ export const USER_SCRIPTS: IUserScripts[] = [
     },
     {
         name: "trf2dcm", code: trf2dcm,
-        description: "Converts files that end in `.trf` into a RT DICOM plan file using a DICOM template."
+        description: "Converts files that end in `.trf` into an RT DICOM plan file using the a DICOM template."
     }
 ]
