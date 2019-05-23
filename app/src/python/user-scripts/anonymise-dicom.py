f4b06da3f9f8c260eebde3c3e4b97bd4fbc3e50e
Matt Jennings <Centrus.007@gmail.com>
2019-05-23 17:44:39 +0930

    Add dicom anonymisation webapp (#335)
    * Add rudimentary anonymisation script.
    
    * inplace
    
    * uncomment anonymiseDicom import.
    
    * no longer require pymedphys_analysis within pymedphys.dicom
    
    * Replace DicomBase with anonymise_file
    
    * Delete unknown tags for now.
    
    
    Former-commit-id: fbda739a6e9c4e5fad579dc516330feb241c5fbc

--- app/src/python/user-scripts/anonymise-dicom.py
+++ app/src/python/user-scripts/anonymise-dicom.py
@@ -1,10 +1,22 @@
-from pymedphys_dicom.dicom import DicomBase
+from os import rename
+from os.path import basename, join as pjoin
+from glob import glob
 
-dicom = DicomBase.from_dict({
-    'Manufacturer': 'PyMedPhys',
-    'PatientName': 'Python^Monte'
-})
+from pymedphys.dicom import anonymise_file
 
-dicom.anonymise(inplace=True)
+INPUT_DIR = 'input'
+OUTPUT_DIR = 'output'
 
-print(dicom.dataset)
+filepaths = glob(pjoin(INPUT_DIR, "*.dcm"))
+
+for filepath in filepaths:
+    filename = basename(filepath)
+    print("Anonymising {}".format(filename))
+
+    output_filepath = pjoin(OUTPUT_DIR, filename)
+
+    anon_filepath = anonymise_file(filepath, delete_unknown_tags=True)
+
+    rename(anon_filepath, output_filepath)
+
+print("Anonymised {} DICOM file(s)".format(len(filepaths)))
