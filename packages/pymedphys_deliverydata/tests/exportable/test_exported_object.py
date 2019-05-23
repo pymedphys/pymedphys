58f1f66adfd7254ddf5d83cffe79b4fa33d0dd20
SimonBiggs <me@simonbiggs.net>
2019-05-23 14:56:36 +1000

    object consistency passing
    Former-commit-id: ac840f1514b429fd25c40d5510277ced904a7c36 [formerly 47267a0c195d61a79603aa267ebd09de03d07c87]
    Former-commit-id: f2a4d30066ec95781e90a1217cbe26b86024d809

--- packages/pymedphys_deliverydata/tests/exportable/test_exported_object.py
+++ packages/pymedphys_deliverydata/tests/exportable/test_exported_object.py
@@ -4,4 +4,7 @@ from pymedphys.deliverydata import DeliveryData
 def test_object_consistency():
     empty = DeliveryData.empty()
     filtered = empty.filter_cps()
+
+    assert type(filtered.monitor_units) is tuple
+
     metersets = filtered.metersets(0, 0)
