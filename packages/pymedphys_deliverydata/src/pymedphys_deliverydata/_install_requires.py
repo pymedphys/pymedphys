7ec0fef9314ef2fd81fa3f3977efb5466a0eae5d
SimonBiggs <me@simonbiggs.net>
2019-05-23 15:34:30 +1000

    yarn tree
    Former-commit-id: 2ab53969b9f2df5bc768a6608b4e54c6242e7e75 [formerly 28ddc3ee870f823b7bbc4974e0a2744ca52e8160]
    Former-commit-id: bbac768946c0a2b12b5685cf9b6c30e83507fc7d

--- packages/pymedphys_deliverydata/src/pymedphys_deliverydata/_install_requires.py
+++ packages/pymedphys_deliverydata/src/pymedphys_deliverydata/_install_requires.py
@@ -1,5 +1,6 @@
 install_requires = [
     "numpy >= 1.12, < 1.16",
     "pymedphys_dicom >= 0.8.0dev17, < 0.9.0",
+    "pymedphys_mudensity >= 0.8.0dev17, < 0.9.0",
     "pymedphys_utilities >= 0.8.0dev17, < 0.9.0"
 ]
