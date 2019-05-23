29d2b99fe26eedc42512439c4a20e91c562a3894
Simon Biggs <me@simonbiggs.net>
2019-05-24 07:23:55 +1000

    move language-server, load up package wheels

--- app/src/python/load-wheels.py
+++ app/src/python/load-wheels.py
@@ -10,9 +10,15 @@ from js import Promise, XMLHttpRequest
 
 
 WHEEL_BASE_PATH = Path('/lib/python3.7/site-packages/').parent
-WHEEL_BASE_URL = 'https://pyodide.pymedphys.com/wheels/'
-WHEEL_INDEX_FILENAME = 'index-2e1331bc-c5e2-5209-8234-4d85fe03b5dd.json'
-WHEEL_INDEX_URL = '{}{}'.format(WHEEL_BASE_URL, WHEEL_INDEX_FILENAME)
+VENDOR_WHEEL_BASE_URL = 'https://pyodide.pymedphys.com/wheels/'
+VENDOR_WHEEL_INDEX_FILENAME = 'index-2e1331bc-c5e2-5209-8234-4d85fe03b5dd.json'
+VENDOR_WHEEL_INDEX_URL = '{}{}'.format(
+    VENDOR_WHEEL_BASE_URL, VENDOR_WHEEL_INDEX_FILENAME)
+
+PYMEDPHYS_WHEEL_BASE_URL = '/python-wheels/'
+PYMEDPHYS_WHEEL_INDEX_FILENAME = 'paths.json'
+PYMEDPHYS_WHEEL_INDEX_URL = '{}{}'.format(
+    PYMEDPHYS_WHEEL_BASE_URL, PYMEDPHYS_WHEEL_INDEX_FILENAME)
 
 
 def _nullop(*args):
@@ -65,7 +71,7 @@ def get_package(url):
     return Promise.new(do_install)
 
 
-def load_and_copy_wheels():
+def load_and_copy_wheels(base_url, index_url):
     def run_promise(resolve, reject):
         def extract_all_wheels(wheel_index_json):
             wheel_index = json.load(wheel_index_json)
@@ -75,7 +81,7 @@ def load_and_copy_wheels():
                 print('Loading {} from python-wheels'.format(wheel_name))
 
             urls = [
-                '{}{}'.format(WHEEL_BASE_URL, wheel)
+                '{}{}'.format(base_url, wheel)
                 for wheel in wheel_index]
 
             promises = []
@@ -84,9 +90,12 @@ def load_and_copy_wheels():
 
             Promise.all(promises).then(resolve)
 
-        get_url(WHEEL_INDEX_URL).then(extract_all_wheels)
+        get_url(index_url).then(extract_all_wheels)
 
     return Promise.new(run_promise)
 
 
-load_and_copy_wheels()
+Promise.all([
+    load_and_copy_wheels(VENDOR_WHEEL_BASE_URL, VENDOR_WHEEL_INDEX_URL),
+    load_and_copy_wheels(PYMEDPHYS_WHEEL_BASE_URL, PYMEDPHYS_WHEEL_INDEX_URL),
+])
