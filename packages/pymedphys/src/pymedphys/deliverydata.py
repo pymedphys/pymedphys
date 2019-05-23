fb366744c0eaba14004636d710b7c7c739d01c86
SimonBiggs <me@simonbiggs.net>
2019-05-23 12:29:34 +1000

    further clean up

--- packages/pymedphys/src/pymedphys/deliverydata.py
+++ packages/pymedphys/src/pymedphys/deliverydata.py
@@ -1,4 +1,4 @@
-from pymedphys_deliverydata.base import DeliveryData
+from pymedphys_deliverydata import DeliveryData
 from pymedphys_deliverydata.dicom import (
     dicom_to_delivery_data,
     delivery_data_to_dicom)
