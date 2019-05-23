70ca17c1c94c8558fa8f78cfd16d50f6ec917154
SimonBiggs <me@simonbiggs.net>
2019-05-23 12:24:25 +1000

    propegate versions
    Former-commit-id: aa95919874ff0750370e91f3b2604244af4ff4d9 [formerly 79be9e39b2824c97c6be8dcbc5ae664090036d8e]
    Former-commit-id: 6239dbd808b5836952df78efc2db0d8f66296065

--- packages/pymedphys_databases/src/pymedphys_databases/_install_requires.py
+++ packages/pymedphys_databases/src/pymedphys_databases/_install_requires.py
@@ -3,6 +3,7 @@ install_requires = [
     "keyring",
     "numpy >= 1.12, < 1.16",
     "pandas",
-    "pymedphys_core >= 0.8.0dev17, < 0.9.0",
+    "pymedphys_deliverydata >= 0.8.0dev17, < 0.9.0",
+    "pymedphys_utilities >= 0.8.0dev17, < 0.9.0",
     "pymssql"
 ]
--- packages/pymedphys_toolbox/src/pymedphys_toolbox/_install_requires.py
+++ packages/pymedphys_toolbox/src/pymedphys_toolbox/_install_requires.py
@@ -1,6 +1,6 @@
 install_requires = [
     "matplotlib",
     "numpy >= 1.12, < 1.16",
-    "pymedphys_core >= 0.8.0dev17, < 0.9.0",
-    "pymedphys_databases >= 0.8.0dev17, < 0.9.0"
+    "pymedphys_databases >= 0.8.0dev17, < 0.9.0",
+    "pymedphys_mudensity >= 0.8.0dev17, < 0.9.0"
 ]
