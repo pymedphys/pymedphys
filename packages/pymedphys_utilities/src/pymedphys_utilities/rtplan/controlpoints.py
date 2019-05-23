c3bc52bb6a2ef993d2c875712c37ef095fd3e52b
SimonBiggs <me@simonbiggs.net>
2019-05-23 13:28:28 +1000

    added mudensity method to DeliveryData
    Former-commit-id: 6abc14f8444a54b603cb949ca14771635df89644

--- packages/pymedphys_utilities/src/pymedphys_utilities/rtplan/controlpoints.py
+++ packages/pymedphys_utilities/src/pymedphys_utilities/rtplan/controlpoints.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2019 Simon Biggs
+# Copyright (C) 2018 Cancer Care Associates
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published
@@ -23,14 +23,39 @@
 # You should have received a copy of the Apache-2.0 along with this
 # program. If not, see <http://www.apache.org/licenses/LICENSE-2.0>.
 
-from pymedphys_deliverydata.utilities import get_delivery_parameters
 
-from .core import calc_mu_density, __DEFAULT_GRID_RESOLUTION
+import numpy as np
 
 
-def mu_density_from_delivery_data(delivery_data,
-                                  grid_resolution=__DEFAULT_GRID_RESOLUTION):
-    mu, mlc, jaw = get_delivery_parameters(delivery_data)
-    mu_density = calc_mu_density(mu, mlc, jaw, grid_resolution=grid_resolution)
+def find_relevant_control_points(mu):
+    """Returns that control points that had an MU difference either side.
+    """
+    mu_diff = np.diff(mu)
+    no_change = mu_diff == 0
+    no_change_before = no_change[0:-1]
+    no_change_after = no_change[1::]
 
-    return mu_density
+    no_change_before_and_after = no_change_before & no_change_after
+    irrelevant_control_point = np.hstack(
+        [no_change[0], no_change_before_and_after, no_change[-1]])
+    relevant_control_points = np.invert(irrelevant_control_point)
+
+    return relevant_control_points
+
+
+def remove_irrelevant_control_points(mu, mlc, jaw):
+    """Removes control points that don't have MU delivery
+    """
+    assert len(mu) > 0, "No control points found"
+
+    mu = np.array(mu)
+    mlc = np.array(mlc)
+    jaw = np.array(jaw)
+
+    control_points_to_use = find_relevant_control_points(mu)
+
+    mu = mu[control_points_to_use]
+    mlc = mlc[control_points_to_use, :, :]
+    jaw = jaw[control_points_to_use, :]
+
+    return mu, mlc, jaw
