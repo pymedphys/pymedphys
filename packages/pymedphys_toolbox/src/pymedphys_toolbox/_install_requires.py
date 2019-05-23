bfa955d5e2e8e156f27bd70df81350f3fd95a485
SimonBiggs <me@simonbiggs.net>
2019-05-23 16:15:13 +1000

    yarn tree
    Former-commit-id: 72c20f123196c21d3150f74d468838bfdf94c5f3 [formerly 35600acde81dd7ad9326b37c8a27eabae7a80975]
    Former-commit-id: c9faf1b64a8f01dc8cd29987c79d5d377219322d

--- packages/pymedphys_toolbox/src/pymedphys_toolbox/_install_requires.py
+++ packages/pymedphys_toolbox/src/pymedphys_toolbox/_install_requires.py
@@ -1,6 +1,5 @@
 install_requires = [
     "matplotlib",
     "numpy >= 1.12, < 1.16",
-    "pymedphys_databases >= 0.8.0dev17, < 0.9.0",
-    "pymedphys_mudensity >= 0.8.0dev17, < 0.9.0"
+    "pymedphys_databases >= 0.8.0dev17, < 0.9.0"
 ]
