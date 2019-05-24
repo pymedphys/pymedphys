6be0fe27c261618958dbe2c173e090f7fc80329e
SimonBiggs <me@simonbiggs.net>
2019-05-24 13:44:31 +1000

    add fraction group splitting
    Former-commit-id: 4c87b18910ef7be1b31249975781a76780af1336 [formerly 298256d0ca7f05d4dfa4cbf6b9893af24fb268c7]
    Former-commit-id: b590755e85b64f2f0fb993c7ef71a569833e4148

--- packages/pymedphys/src/pymedphys/dicom.py
+++ packages/pymedphys/src/pymedphys/dicom.py
@@ -13,4 +13,6 @@ from pymedphys_dicom.dicom import (
     dicom_dataset_from_dict
 )
 
+from pymedphys_dicom.rtplan import convert_to_one_fraction_group
+
 # from pymedphys_analysis.geometry import get_structure_aligned_cube
