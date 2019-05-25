dff66c064fc769bd767ab2668a797adf0476bd7c
Simon Biggs <me@simonbiggs.net>
2019-05-25 21:22:27 +1000

    writing the webworker code in ts seems to break things... revert

--- app/src/observables/webworker-messaging/webworker.tsx
+++ app/src/observables/webworker-messaging/webworker.tsx
@@ -1,102 +1,100 @@
-import './serviceworker.d.ts'
-
-import loadWheels from '../../python/load-wheels.py';
-import setupDirectories from '../../python/setup-directories.py';
+// import loadWheels from '../../python/load-wheels.py';
+// import setupDirectories from '../../python/setup-directories.py';
 
 
-interface PyodideWorker extends Worker {
-  pyodide: any;
-  Module: any;
-  languagePluginUrl: string;
-  document: any;
-}
+// interface PyodideWorker extends Worker {
+//   pyodide: any;
+//   Module: any;
+//   languagePluginUrl: string;
+//   document: any;
+// }
 
-const ctx: PyodideWorker = self as any;
+// const ctx: PyodideWorker = self as any;
 
-ctx.languagePluginUrl = 'https://pyodide.pymedphys.com/'
-importScripts('https://pyodide.pymedphys.com/pyodide.js')
+// ctx.languagePluginUrl = 'https://pyodide.pymedphys.com/'
+// importScripts('https://pyodide.pymedphys.com/pyodide.js')
 
-import {
-  receiverMessengers, senderMessengers,
-  sendReply, sendFileTransfer, IPyodideMessage
-} from './common';
+// import {
+//   receiverMessengers, senderMessengers,
+//   sendReply, sendFileTransfer, IPyodideMessage
+// } from './common';
 
-receiverMessengers.subscribe((message: IPyodideMessage) => {
-  console.log("Received webworker <-- main")
-  console.log(message)
-})
+// receiverMessengers.subscribe((message: IPyodideMessage) => {
+//   console.log("Received webworker <-- main")
+//   console.log(message)
+// })
 
-senderMessengers.subscribe((message: IPyodideMessage) => {
-  console.log("Sending webworker --> main")
-  console.log(message)
-  ctx.postMessage(message, message.transferables)
-});
+// senderMessengers.subscribe((message: IPyodideMessage) => {
+//   console.log("Sending webworker --> main")
+//   console.log(message)
+//   ctx.postMessage(message, message.transferables)
+// });
 
-ctx.onmessage = function (e) { // eslint-disable-line no-unused-vars
-  receiverMessengers.next(e.data)
-}
+// ctx.onmessage = function (e) { // eslint-disable-line no-unused-vars
+//   receiverMessengers.next(e.data)
+// }
 
-let pythonInitialise = languagePluginLoader.then(() => {
-  return Promise.all([
-    ctx.pyodide.runPython(setupDirectories),
-    ctx.pyodide.runPython(loadWheels),
-    ctx.pyodide.loadPackage(['matplotlib', 'numpy', 'pandas'])
-  ]).then(() => {
-    ctx.document = {
-      createElement: () => { }
-    };
-  })
-})
+// let pythonInitialise = languagePluginLoader.then(() => {
+//   return Promise.all([
+//     ctx.pyodide.runPython(setupDirectories),
+//     ctx.pyodide.runPython(loadWheels),
+//     ctx.pyodide.loadPackage(['matplotlib', 'numpy', 'pandas'])
+//   ]).then(() => {
+//     ctx.document = {
+//       createElement: () => { }
+//     };
+//   })
+// })
 
-receiverMessengers.initialise.subscribe(data => {
-  const uuid = data.uuid
-  pythonInitialise.then(() => {
-    sendReply(uuid, {})
-  })
-})
+// receiverMessengers.initialise.subscribe(data => {
+//   const uuid = data.uuid
+//   pythonInitialise.then(() => {
+//     sendReply(uuid, {})
+//   })
+// })
 
-receiverMessengers.executeRequest.subscribe(message => {
-  const uuid = message.uuid;
-  const code = message.data.code;
+// receiverMessengers.executeRequest.subscribe(message => {
+//   const uuid = message.uuid;
+//   const code = message.data.code;
 
-  pythonInitialise.then(() => {
-    pyodide.runPythonAsync(code)
-      .then((result: any) => {
-        sendReply(uuid, { result })
-      })
-      .catch((error: any) => {
-        sendReply(uuid, { error })
-      });
-  });
-})
+//   pythonInitialise.then(() => {
+//     pyodide.runPythonAsync(code)
+//       .then((result: any) => {
+//         sendReply(uuid, { result })
+//       })
+//       .catch((error: any) => {
+//         sendReply(uuid, { error })
+//       });
+//   });
+// })
 
-receiverMessengers.fileTransferRequest.subscribe(message => {
-  const uuid = message.uuid
-  const filepath = message.data.filepath
+// receiverMessengers.fileTransferRequest.subscribe(message => {
+//   const uuid = message.uuid
+//   const filepath = message.data.filepath
 
-  let file: ArrayBuffer = ctx.Module.FS.readFile(filepath)
-  sendFileTransfer(file, filepath, uuid)
-})
+//   let file: ArrayBuffer = ctx.Module.FS.readFile(filepath)
+//   sendFileTransfer(file, filepath, uuid)
+// })
 
-function dirBasenameSplit(filepath: string) {
-  const split = filepath.split('/')
-  const basename = split[split.length - 1]
-  const dir = split.slice(0, -1).join('/') + '/'
-  const dirbasename = [dir, basename]
+// function dirBasenameSplit(filepath: string) {
+//   const split = filepath.split('/')
+//   const basename = split[split.length - 1]
+//   const dir = split.slice(0, -1).join('/') + '/'
+//   const dirbasename = [dir, basename]
 
-  return dirbasename
-}
+//   return dirbasename
+// }
 
-receiverMessengers.fileTransfer.subscribe(message => {
-  const uuid = message.uuid
-  const filepath = message.data.filepath
-  const file = message.data.file
+// receiverMessengers.fileTransfer.subscribe(message => {
+//   const uuid = message.uuid
+//   const filepath = message.data.filepath
+//   const file = message.data.file
 
-  if (filepath !== undefined) {
-    const dirbasename = dirBasenameSplit(filepath)
-    const data = new Uint8Array(file);
-    ctx.Module['FS_createDataFile'](dirbasename[0], dirbasename[1], data, true, true, true);
+//   if (filepath !== undefined) {
+//     const dirbasename = dirBasenameSplit(filepath)
+//     const data = new Uint8Array(file);
+//     ctx.Module['FS_createDataFile'](dirbasename[0], dirbasename[1], data, true, true, true);
 
-    sendReply(uuid, { result: dirbasename[1] })
-  }
-})
+//     sendReply(uuid, { result: dirbasename[1] })
+//   }
+// })
