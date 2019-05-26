1dff745fce9d837f675b7d2f9d07770d73662300
Simon Biggs <me@simonbiggs.net>
2019-05-27 09:34:48 +1000

    [MRG] Implemented fast caching, began vendoring in python language server, prepared for using webworkers once pyodide bugs are fixed. (#334)
    * webworker working!
    
    * web worker is now loading the python packages
    
    * messages working
    
    * a few renaming things
    
    * vendored wheels into pyodide hosting, now caching and loading these wheels
    
    * add lanuage server
    
    * load more wheels
    
    * vendor in pyodide-language-server
    
    * move language-server, load up package wheels
    
    * base for rxjs messaging
    
    * attempting to hook up rxjs messengers
    
    * BehaviorSubjects to Subjects
    
    * communication up and running
    
    * began hooking up communications
    
    * begin hooking up file transfers
    
    * changed to just one file per transfer request
    
    * files are being added
    
    * code running
    
    * a recursive error somewhere... but making progress
    
    * webworker now written in typescript
    
    * fix import
    
    * code being sent to worker, results coming back
    
    * output files appearing
    
    * observeOn(queueScheduler) used
    
    * fixed recursion bug
    
    * nope... still iterating on the call stack problem
    
    * temp workaround for matplotlib within webworker
    
    * writing the webworker code in ts seems to break things... revert
    
    * fix ctx
    
    * add import matplotlib as a minimum viable example
    
    * turn off offline caching
    
    * add serve to --dev install
    
    * create a feature switch for webworkers
    
    * mocking is complete
    
    * file input still not working
    
    * inputs now working :) yayy...
    
    * working with rxjs messaging
    
    * use new determinisitc json file
    
    
    Former-commit-id: 62ff139472fbc0fde570100bea09ea3c6160eb13 [formerly e52c8f2b25b4c3e7a776dde9b520d09ab2832c09]
    Former-commit-id: 504e751ae663ad37d494dc8465e048d78ef59c6d

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -4,15 +4,15 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
 import App from './root';
-import { initPython } from './python/init';
 import './index.scss';
 
+import { startPyodide } from './pyodide';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-initPython()
+startPyodide();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.unregister();
