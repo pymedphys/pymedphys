0c091ec5665d7374137808469f46fe1c700a2ad3
SimonBiggs <me@simonbiggs.net>
2019-05-27 15:39:07 +1000

    yarn tree

--- packages/pymedphys_databases/src/pymedphys_databases/_install_requires.py
+++ packages/pymedphys_databases/src/pymedphys_databases/_install_requires.py
@@ -4,6 +4,8 @@ install_requires = [
     "numpy >= 1.12, < 1.16",
     "pandas",
     "pymedphys_base >= 0.8.0dev17, < 0.9.0",
+    "pymedphys_fileformats >= 0.8.0dev17, < 0.9.0",
+    "pymedphys_mudensity >= 0.8.0dev17, < 0.9.0",
     "pymedphys_utilities >= 0.8.0dev17, < 0.9.0",
     "pymssql"
 ]
--- packages/pymedphys_logfiles/src/pymedphys_logfiles/_install_requires.py
+++ packages/pymedphys_logfiles/src/pymedphys_logfiles/_install_requires.py
@@ -3,7 +3,6 @@ install_requires = [
     "matplotlib",
     "numpy >= 1.12, < 1.16",
     "pandas",
-    "pymedphys_base >= 0.8.0dev17, < 0.9.0",
     "pymedphys_databases >= 0.8.0dev17, < 0.9.0",
     "pymedphys_fileformats >= 0.8.0dev17, < 0.9.0",
     "pymedphys_mudensity >= 0.8.0dev17, < 0.9.0",
