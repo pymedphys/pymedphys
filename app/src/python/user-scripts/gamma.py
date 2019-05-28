c00314cb4b1f3e38ec1593b51aa4b894e796dc70
SimonBiggs <me@simonbiggs.net>
2019-05-28 11:03:58 +1000

    temp pull in old code, scale original dose to 1 fraction

--- app/src/python/user-scripts/gamma.py
+++ app/src/python/user-scripts/gamma.py
@@ -18,16 +18,17 @@ gamma_options = {
     'distance_mm_threshold': 3,
     'lower_percent_dose_cutoff': 20,
     'interp_fraction': 2,  # Should be about 10 for more accurate results
-    'max_gamma': 2,
+    'max_gamma': 1.1,
+    'random_subset': 10000,
     'local_gamma': False,
     'ram_available': 2**29  # 1/2 GB
-
 }
 
 first_reference_dicom_file = glob(os.path.join(
     input_dir, '{}*.dcm'.format(reference_tag)))[0]
 first_evaluation_dicom_file = glob(os.path.join(
     input_dir, '{}*.dcm'.format(evaluation_tag)))[0]
+
 
 gamma = gamma_dicom(
     first_reference_dicom_file, first_evaluation_dicom_file, **gamma_options)
