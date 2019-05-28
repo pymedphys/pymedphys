2dd6c5837e7a793304b2715f01b753bfef97263c
SimonBiggs <me@simonbiggs.net>
2019-05-28 13:09:57 +1000

    show histogram only

--- app/src/python/user-scripts/gamma.py
+++ app/src/python/user-scripts/gamma.py
@@ -17,12 +17,12 @@ reference_tag = "original"
 evaluation_tag = "logfile"
 
 gamma_options = {
-    'dose_percent_threshold': 3,
-    'distance_mm_threshold': 3,
+    'dose_percent_threshold': 1,
+    'distance_mm_threshold': 1,
     'lower_percent_dose_cutoff': 20,
-    'interp_fraction': 5,  # Should be about 10 for more accurate results
-    'max_gamma': 1,
-    'random_subset': None,
+    'interp_fraction': 10,  # Should be about 10 for more accurate results
+    'max_gamma': 2,
+    'random_subset': 10000,
     'local_gamma': False,
     'ram_available': 2**29  # 1/2 GB
 }
@@ -56,84 +56,84 @@ plt.title("Gamma Percent Pass: {0:.2f} %".format(pass_ratio*100))
 plt.savefig(os.path.join(output_dir, "gamma_histogram.png"))
 
 
-(z_ref, y_ref, x_ref), dose_reference = zyx_and_dose_from_dataset(reference)
-(z_eval, y_eval, x_eval), dose_evaluation = zyx_and_dose_from_dataset(evaluation)
+# (z_ref, y_ref, x_ref), dose_reference = zyx_and_dose_from_dataset(reference)
+# (z_eval, y_eval, x_eval), dose_evaluation = zyx_and_dose_from_dataset(evaluation)
 
 
-lower_dose_cutoff = (
-    gamma_options['lower_percent_dose_cutoff'] / 100 * np.max(dose_reference))  # type: ignore
+# lower_dose_cutoff = (
+#     gamma_options['lower_percent_dose_cutoff'] / 100 * np.max(dose_reference))  # type: ignore
 
-relevant_slice = (
-    np.max(dose_reference, axis=(1, 2)) >
-    lower_dose_cutoff)
-slice_start = np.max([
-    np.where(relevant_slice)[0][0],
-    0])
-slice_end = np.min([
-    np.where(relevant_slice)[0][-1],
-    len(z_ref)])
-
-
-max_ref_dose = np.max(dose_reference)
-
-z_vals = z_ref[slice(slice_start, slice_end)]
-
-eval_slices = [
-    dose_evaluation[np.where(z_i == z_eval)[0][0], :, :]
-    for z_i in z_vals
-]
-
-ref_slices = [
-    dose_reference[np.where(z_i == z_ref)[0][0], :, :]
-    for z_i in z_vals
-]
-
-gamma_slices = [
-    gamma[np.where(z_i == z_ref)[0][0], :, :]
-    for z_i in z_vals
-]
-
-diffs = [
-    eval_slice - ref_slice
-    for eval_slice, ref_slice
-    in zip(eval_slices, ref_slices)
-]
-
-max_diff = np.max(np.abs(diffs))
+# relevant_slice = (
+#     np.max(dose_reference, axis=(1, 2)) >
+#     lower_dose_cutoff)
+# slice_start = np.max([
+#     np.where(relevant_slice)[0][0],
+#     0])
+# slice_end = np.min([
+#     np.where(relevant_slice)[0][-1],
+#     len(z_ref)])
 
 
-for z_val, eval_slice, ref_slice, diff, gamma_slice in zip(
-        z_vals, eval_slices, ref_slices, diffs, gamma_slices):
-    fig, ax = plt.subplots(figsize=(13, 10), nrows=2, ncols=2)
+# max_ref_dose = np.max(dose_reference)
 
-    c00 = ax[0, 0].contourf(
-        x_eval, y_eval, eval_slice, 30,
-        vmin=0, vmax=max_ref_dose, cmap=plt.get_cmap('viridis'))
-    ax[0, 0].set_title("Evaluation")
-    fig.colorbar(c00, ax=ax[0, 0])
-    ax[0, 0].invert_yaxis()
+# z_vals = z_ref[slice(slice_start, slice_end)]
 
-    c01 = ax[0, 1].contourf(
-        x_ref, y_ref, ref_slice, 30,
-        vmin=0, vmax=max_ref_dose, cmap=plt.get_cmap('viridis'))
-    ax[0, 1].set_title("Reference")
-    fig.colorbar(c01, ax=ax[0, 1])
-    ax[0, 1].invert_yaxis()
+# eval_slices = [
+#     dose_evaluation[np.where(z_i == z_eval)[0][0], :, :]
+#     for z_i in z_vals
+# ]
 
-    c10 = ax[1, 0].contourf(
-        x_ref, y_ref, diff, 30,
-        vmin=-max_diff, vmax=max_diff, cmap=plt.get_cmap('seismic'))
-    ax[1, 0].set_title("Dose difference")
-    fig.colorbar(c10, ax=ax[1, 0])
-    ax[1, 0].invert_yaxis()
+# ref_slices = [
+#     dose_reference[np.where(z_i == z_ref)[0][0], :, :]
+#     for z_i in z_vals
+# ]
 
-    c11 = ax[1, 1].contourf(
-        x_ref, y_ref, gamma_slice, 30,
-        vmin=0, vmax=2, cmap=plt.get_cmap('seismic'))
-    ax[1, 1].set_title("Gamma")
-    fig.colorbar(c11, ax=ax[1, 1])
-    ax[1, 1].invert_yaxis()
+# gamma_slices = [
+#     gamma[np.where(z_i == z_ref)[0][0], :, :]
+#     for z_i in z_vals
+# ]
 
-    fig.savefig(os.path.join(output_dir, 'slice_{:+06.1f}.gamma.png'.format(
-        z_val
-    )))
+# diffs = [
+#     eval_slice - ref_slice
+#     for eval_slice, ref_slice
+#     in zip(eval_slices, ref_slices)
+# ]
+
+# max_diff = np.max(np.abs(diffs))
+
+
+# for z_val, eval_slice, ref_slice, diff, gamma_slice in zip(
+#         z_vals, eval_slices, ref_slices, diffs, gamma_slices):
+#     fig, ax = plt.subplots(figsize=(13, 10), nrows=2, ncols=2)
+
+#     c00 = ax[0, 0].contourf(
+#         x_eval, y_eval, eval_slice, 30,
+#         vmin=0, vmax=max_ref_dose, cmap=plt.get_cmap('viridis'))
+#     ax[0, 0].set_title("Evaluation")
+#     fig.colorbar(c00, ax=ax[0, 0])
+#     ax[0, 0].invert_yaxis()
+
+#     c01 = ax[0, 1].contourf(
+#         x_ref, y_ref, ref_slice, 30,
+#         vmin=0, vmax=max_ref_dose, cmap=plt.get_cmap('viridis'))
+#     ax[0, 1].set_title("Reference")
+#     fig.colorbar(c01, ax=ax[0, 1])
+#     ax[0, 1].invert_yaxis()
+
+#     c10 = ax[1, 0].contourf(
+#         x_ref, y_ref, diff, 30,
+#         vmin=-max_diff, vmax=max_diff, cmap=plt.get_cmap('seismic'))
+#     ax[1, 0].set_title("Dose difference")
+#     fig.colorbar(c10, ax=ax[1, 0])
+#     ax[1, 0].invert_yaxis()
+
+#     c11 = ax[1, 1].contourf(
+#         x_ref, y_ref, gamma_slice, 30,
+#         vmin=0, vmax=2, cmap=plt.get_cmap('seismic'))
+#     ax[1, 1].set_title("Gamma")
+#     fig.colorbar(c11, ax=ax[1, 1])
+#     ax[1, 1].invert_yaxis()
+
+#     fig.savefig(os.path.join(output_dir, 'slice_{:+06.1f}.gamma.png'.format(
+#         z_val
+#     )))
