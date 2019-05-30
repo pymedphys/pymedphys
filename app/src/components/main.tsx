455bbe4d35b15f2ab1b46fb0de33d7812ab7408f
Simon Biggs <me@simonbiggs.net>
2019-05-30 10:18:38 +1000

    make react router use tabs

--- app/src/components/main.tsx
+++ app/src/components/main.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 
 import {
   Tabs, Tab, H3, Icon
@@ -7,6 +9,7 @@ import {
 
 import { AppNavbar } from './navbar';
 import { AppUserScripts } from './user-scripts';
+import { placeholder } from '@babel/types';
 
 
 interface IAppMainProps {
@@ -31,7 +34,7 @@ export class AppMain extends React.Component<IAppMainProps, IAppMainState> {
       <div className="AppMain">
         <AppNavbar />
 
-        <Tabs
+        {/* <Tabs
           animate={true}
           id="Tabs"
           vertical={true}
@@ -40,7 +43,35 @@ export class AppMain extends React.Component<IAppMainProps, IAppMainState> {
           <Tab id="UserScripts" title={<span><Icon icon={"code"} /> User Scripts</span>} panel={<AppUserScripts />} />
           <Tab id="GammaAnalysis" title={<span><Icon icon={"grid"} /> Gamma Analysis</span>} panel={<Placeholder />} />
           <Tab id="PythonEngine" title={<span><Icon icon={"function"} /> Python Engine</span>} panel={<Placeholder />} />
-        </Tabs>
+        </Tabs> */}
+
+
+
+        <Router>
+
+          <Tabs
+            animate={true}
+            id="Tabs"
+            vertical={true}
+          >
+            <Tab id="UserScripts" title={
+              <Link to="/user-scripts/"><Icon icon={"code"} /> User Scripts</Link>
+            } />
+            <Tab id="GammaAnalysis" title={
+              <Link to="/gamma-analysis/"><Icon icon={"grid"} /> Gamma Analysis</Link>
+            } />
+            <Tab id="PythonEngine" title={
+              <Link to="/python-engine/"><Icon icon={"function"} /> Python Engine</Link>
+            } />
+          </Tabs>
+
+          <div className="TabPadding">
+            <Route path="/" exact component={Placeholder} />
+            <Route path="/user-scripts/" exact component={AppUserScripts} />
+            <Route path="/gamma-analysis/" exact component={Placeholder} />
+            <Route path="/python-engine/" exact component={Placeholder} />
+          </div>
+        </Router>
 
 
       </div>
