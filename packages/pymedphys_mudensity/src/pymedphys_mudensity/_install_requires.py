c4413a1a6baa1baca75e7866ca0c8afff9911227
Simon Biggs <me@simonbiggs.net>
2019-06-01 20:55:39 +1000

    bump version to 0.8.2
    Former-commit-id: ea306719c5b3450c1cfa5f54c3205d7abf9e200d

--- packages/pymedphys_mudensity/src/pymedphys_mudensity/_install_requires.py
+++ packages/pymedphys_mudensity/src/pymedphys_mudensity/_install_requires.py
@@ -1,6 +1,6 @@
 install_requires = [
     "matplotlib",
     "numpy >= 1.12",
-    "pymedphys_base >= 0.8.1, < 0.9.0",
-    "pymedphys_utilities >= 0.8.1, < 0.9.0"
+    "pymedphys_base >= 0.8.2, < 0.9.0",
+    "pymedphys_utilities >= 0.8.2, < 0.9.0"
 ]
