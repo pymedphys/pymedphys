978a9b3c8f5d868d50dd75e4dc99732d438508c3
Simon Biggs <me@simonbiggs.net>
2019-06-01 19:04:37 +1000

    bump 0.8.0.dev17 --> 0.8.0
    Former-commit-id: 2118bcaf6321f97028a40098fc61e7f9ba93b99f [formerly 0a9249e10bfc91e63663b10a8a8775578df3302f]
    Former-commit-id: 65f4e8cf58951c2f2e3c4eb61c309a9f83dfcba3

--- packages/pymedphys_logfiles/src/pymedphys_logfiles/_install_requires.py
+++ packages/pymedphys_logfiles/src/pymedphys_logfiles/_install_requires.py
@@ -3,9 +3,9 @@ install_requires = [
     "matplotlib",
     "numpy >= 1.12, < 1.16",
     "pandas",
-    "pymedphys_databases >= 0.8.0dev17, < 0.9.0",
-    "pymedphys_fileformats >= 0.8.0dev17, < 0.9.0",
-    "pymedphys_mudensity >= 0.8.0dev17, < 0.9.0",
-    "pymedphys_utilities >= 0.8.0dev17, < 0.9.0",
+    "pymedphys_databases >= 0.8.0, < 0.9.0",
+    "pymedphys_fileformats >= 0.8.0, < 0.9.0",
+    "pymedphys_mudensity >= 0.8.0, < 0.9.0",
+    "pymedphys_utilities >= 0.8.0, < 0.9.0",
     "python_dateutil"
 ]
