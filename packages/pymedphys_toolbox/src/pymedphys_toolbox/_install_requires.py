2f725615e25aef8b58f061e0ceef604ef79053d9
Simon Biggs <me@simonbiggs.net>
2019-06-01 19:52:11 +1000

    bump patch version after removing numpy upper limit

--- packages/pymedphys_toolbox/src/pymedphys_toolbox/_install_requires.py
+++ packages/pymedphys_toolbox/src/pymedphys_toolbox/_install_requires.py
@@ -1,5 +1,5 @@
 install_requires = [
     "matplotlib",
     "numpy >= 1.12",
-    "pymedphys_databases >= 0.8.0, < 0.9.0"
+    "pymedphys_databases >= 0.8.1, < 0.9.0"
 ]
--- packages/pymedphys_xlwings/src/pymedphys_xlwings/_install_requires.py
+++ packages/pymedphys_xlwings/src/pymedphys_xlwings/_install_requires.py
@@ -1,9 +1,9 @@
 install_requires = [
     "numpy >= 1.12",
     "pydicom",
-    "pymedphys_dicom >= 0.8.0, < 0.9.0",
-    "pymedphys_fileformats >= 0.8.0, < 0.9.0",
-    "pymedphys_utilities >= 0.8.0, < 0.9.0",
+    "pymedphys_dicom >= 0.8.1, < 0.9.0",
+    "pymedphys_fileformats >= 0.8.1, < 0.9.0",
+    "pymedphys_utilities >= 0.8.1, < 0.9.0",
     "scipy",
     "xlwings"
 ]
