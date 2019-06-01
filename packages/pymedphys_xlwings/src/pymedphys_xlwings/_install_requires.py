0a9249e10bfc91e63663b10a8a8775578df3302f
Simon Biggs <me@simonbiggs.net>
2019-06-01 19:04:37 +1000

    bump 0.8.0.dev17 --> 0.8.0

--- packages/pymedphys_xlwings/src/pymedphys_xlwings/_install_requires.py
+++ packages/pymedphys_xlwings/src/pymedphys_xlwings/_install_requires.py
@@ -1,9 +1,9 @@
 install_requires = [
     "numpy >= 1.12, < 1.16",
     "pydicom",
-    "pymedphys_dicom >= 0.8.0dev17, < 0.9.0",
-    "pymedphys_fileformats >= 0.8.0dev17, < 0.9.0",
-    "pymedphys_utilities >= 0.8.0dev17, < 0.9.0",
+    "pymedphys_dicom >= 0.8.0, < 0.9.0",
+    "pymedphys_fileformats >= 0.8.0, < 0.9.0",
+    "pymedphys_utilities >= 0.8.0, < 0.9.0",
     "scipy",
     "xlwings"
 ]
