0861814cea9fa1901d425995e0776a64624b1a47
SimonBiggs <me@simonbiggs.net>
2019-06-04 12:04:09 +1000

    fix MANIFEST file within pymedphys_fileformats

--- packages/pymedphys_labs/src/pymedphys_labs/_install_requires.py
+++ packages/pymedphys_labs/src/pymedphys_labs/_install_requires.py
@@ -2,7 +2,7 @@ install_requires = [
     "Pillow",
     "matplotlib",
     "numpy >= 1.12",
-    "pymedphys_utilities >= 0.8.2, < 0.9.0",
+    "pymedphys_utilities >= 0.8.3, < 0.9.0",
     "scipy",
     "xarray"
 ]
