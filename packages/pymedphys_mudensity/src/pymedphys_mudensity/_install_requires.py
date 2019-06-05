3d8fb455789d2bc9295907c59dc99b119fe2dc15
SimonBiggs <me@simonbiggs.net>
2019-06-05 14:47:53 +1000

    make Python be forced to >= 3.7
    Former-commit-id: 00a2576b94869b7f2833bdc39f9f5713b241d66b

--- packages/pymedphys_mudensity/src/pymedphys_mudensity/_install_requires.py
+++ packages/pymedphys_mudensity/src/pymedphys_mudensity/_install_requires.py
@@ -1,6 +1,6 @@
 install_requires = [
     "matplotlib",
     "numpy >= 1.12",
-    "pymedphys_base >= 0.8.4, < 0.9.0",
-    "pymedphys_utilities >= 0.8.4, < 0.9.0"
+    "pymedphys_base >= 0.8.5, < 0.9.0",
+    "pymedphys_utilities >= 0.8.5, < 0.9.0"
 ]
