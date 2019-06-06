e3e0764633738155bb089a110cd1b265fcd00a02
SimonBiggs <me@simonbiggs.net>
2019-06-07 08:58:44 +1000

    run yarn tree

--- packages/pymedphys_labs/src/pymedphys_labs/_install_requires.py
+++ packages/pymedphys_labs/src/pymedphys_labs/_install_requires.py
@@ -1,8 +1,11 @@
 install_requires = [
     "Pillow",
+    "fitz",
     "matplotlib",
     "numpy >= 1.12",
+    "pydicom",
     "pymedphys_utilities >= 0.9.0dev0, < 0.10.0",
     "scipy",
+    "tqdm",
     "xarray"
 ]
