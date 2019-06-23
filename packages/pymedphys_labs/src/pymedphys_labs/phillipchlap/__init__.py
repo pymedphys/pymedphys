a222a631983a00748dcce22ae48c44b3889eeac4
Phillip Chlap <phillip.chlap@unsw.edu.au>
2019-06-23 15:36:46 -0400

    Basic restructuring of Pinnacle export code to satisfy level and depth

--- packages/pymedphys_labs/src/pymedphys_labs/phillipchlap/__init__.py
+++ packages/pymedphys_labs/src/pymedphys_labs/phillipchlap/__init__.py
@@ -1,5 +1,1 @@
-from .pinnacle.pinnacle import Pinnacle
-from .pinnacle.pinnacle_image import PinnacleImage
-from .pinnacle.pinnacle_plan import PinnaclePlan
-
-from .pinnacle import export_pinnacle
+from .pinnacle import Pinnacle
