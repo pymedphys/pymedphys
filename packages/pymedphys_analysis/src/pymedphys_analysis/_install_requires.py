d1e8be0af583688514e72c257c0ca5861c16d77a
SimonBiggs <me@simonbiggs.net>
2019-07-02 16:58:37 +1000

    bump to version 0.10.x
    Former-commit-id: f3d3a34dca353b7adc76aff7fa96cd93a9b976ff

--- packages/pymedphys_analysis/src/pymedphys_analysis/_install_requires.py
+++ packages/pymedphys_analysis/src/pymedphys_analysis/_install_requires.py
@@ -2,6 +2,6 @@ install_requires = [
     "matplotlib",
     "numpy >= 1.12",
     "pydicom",
-    "pymedphys_dicom >= 0.10.0dev0, < 0.11.0",
+    "pymedphys_dicom >= 0.10.0, < 0.11.0",
     "scipy"
 ]
