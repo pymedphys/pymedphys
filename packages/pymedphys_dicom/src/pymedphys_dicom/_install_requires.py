f3d3a34dca353b7adc76aff7fa96cd93a9b976ff
SimonBiggs <me@simonbiggs.net>
2019-07-02 16:58:37 +1000

    bump to version 0.10.x

--- packages/pymedphys_dicom/src/pymedphys_dicom/_install_requires.py
+++ packages/pymedphys_dicom/src/pymedphys_dicom/_install_requires.py
@@ -3,6 +3,6 @@ install_requires = [
     "numpy >= 1.12",
     "packaging",
     "pydicom",
-    "pymedphys_base >= 0.10.0dev0, < 0.11.0",
-    "pymedphys_utilities >= 0.10.0dev0, < 0.11.0"
+    "pymedphys_base >= 0.10.0, < 0.11.0",
+    "pymedphys_utilities >= 0.10.0, < 0.11.0"
 ]
