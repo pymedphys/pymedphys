d34ead19d6c3167a971a4ac34a85908121991477
Simon Biggs <me@simonbiggs.net>
2019-07-06 21:21:05 +1000

    fix dependency cycle

--- packages/pymedphys_analysis/src/pymedphys_analysis/_install_requires.py
+++ packages/pymedphys_analysis/src/pymedphys_analysis/_install_requires.py
@@ -3,9 +3,7 @@ install_requires = [
     "matplotlib",
     "numpy >= 1.12",
     "pydicom",
-    "pymedphys >= 0.10.0, < 0.11.0",
     "pymedphys_dicom >= 0.10.0, < 0.11.0",
-    "pytest",
     "scikit-image",
     "scipy"
 ]
