daa17587aa367c40c787f6feb6e72df8f1b7ce71
Simon Biggs <me@simonbiggs.net>
2019-07-06 15:57:27 +1000

    make prescan be the image that moves

--- packages/pymedphys_analysis/src/pymedphys_analysis/film/__init__.py
+++ packages/pymedphys_analysis/src/pymedphys_analysis/film/__init__.py
@@ -2,4 +2,4 @@ from .align import (load_cal_scans, align_images, load_image,
                     interpolated_rotation, create_image_interpolation,
                     shift_and_rotate)
 from .dicom_dose_extract import dicom_dose_extract
-from .optical_density import calc_net_od
+from .optical_density import calc_net_od, get_aligned_image
