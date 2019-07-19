566d3231e0a279838afc9b2dc73d360981558e69
SimonBiggs <me@simonbiggs.net>
2019-07-19 12:00:47 +1000

    yarn tree

--- packages/pymedphys_analysis/src/pymedphys_analysis/_install_requires.py
+++ packages/pymedphys_analysis/src/pymedphys_analysis/_install_requires.py
@@ -3,5 +3,6 @@ install_requires = [
     "numpy >= 1.12",
     "pydicom",
     "pymedphys_dicom >= 0.10.0, < 0.11.0",
+    "pymedphys_fileformats >= 0.10.0, < 0.11.0",
     "scipy"
 ]
--- packages/pymedphys_dicom/src/pymedphys_dicom/_install_requires.py
+++ packages/pymedphys_dicom/src/pymedphys_dicom/_install_requires.py
@@ -4,5 +4,6 @@ install_requires = [
     "packaging",
     "pydicom",
     "pymedphys_base >= 0.10.0, < 0.11.0",
-    "pymedphys_utilities >= 0.10.0, < 0.11.0"
+    "pymedphys_utilities >= 0.10.0, < 0.11.0",
+    "scipy"
 ]
