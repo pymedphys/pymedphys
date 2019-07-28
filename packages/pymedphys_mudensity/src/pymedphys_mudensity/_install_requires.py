f9bc75df6ea74d1c9de01eece6fcf94d7b44a868
Matthew Jennings <Centrus.007@gmail.com>
2019-07-28 12:25:25 +0930

    Revert "Revert "Permit unblackified yarn tree results.""
    This reverts commit 5137c0c38bb161c6094578f6cd51924583e1015b.

--- packages/pymedphys_mudensity/src/pymedphys_mudensity/_install_requires.py
+++ packages/pymedphys_mudensity/src/pymedphys_mudensity/_install_requires.py
@@ -2,5 +2,5 @@ install_requires = [
     "matplotlib",
     "numpy >= 1.12",
     "pymedphys_base >= 0.11.0dev0, < 0.12.0",
-    "pymedphys_utilities >= 0.11.0dev0, < 0.12.0",
+    "pymedphys_utilities >= 0.11.0dev0, < 0.12.0"
 ]
