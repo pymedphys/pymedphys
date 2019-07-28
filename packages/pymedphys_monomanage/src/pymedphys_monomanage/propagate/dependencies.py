95ccab6fa543627eb358b1fee4ceea27fde30c2f
Matthew Jennings <Centrus.007@gmail.com>
2019-07-28 13:47:40 +0930

    Fix blackification of empty install_requires
    Former-commit-id: 8809ecec4e6484db9057e1496d1c65e30994ad26 [formerly 19529d679d906873797d42bfeb20300397252436]
    Former-commit-id: ca40b0cfc102fc81be17e40c713ab3ecf0affc69

--- packages/pymedphys_monomanage/src/pymedphys_monomanage/propagate/dependencies.py
+++ packages/pymedphys_monomanage/src/pymedphys_monomanage/propagate/dependencies.py
@@ -98,9 +98,12 @@ def main():
         """
         ).format(json.dumps(install_requires, indent=4))
 
-        install_requires_contents_black_compliant = (
-            install_requires_contents[:-3] + ",\n]\n"
-        )
+        if install_requires:
+            install_requires_contents_black_compliant = (
+                install_requires_contents[:-3] + ",\n]\n"
+            )
+        else:
+            install_requires_contents_black_compliant = install_requires_contents
 
         with open(install_requires_filepath, "w") as file:
             file.write(install_requires_contents_black_compliant)
