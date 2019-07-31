9d745b4bb5de79ded6c3c7741b69b454d0b16df7
Matthew Jennings <matthew.jennings2@sa.gov.au>
2019-07-31 10:27:55 +0930

    import json in constants. Don't save new baseline now. Avoid UnboundLocalError for temp_filepath

--- packages/pymedphys_dicom/src/pymedphys_dicom/dicom/constants.py
+++ packages/pymedphys_dicom/src/pymedphys_dicom/dicom/constants.py
@@ -33,6 +33,7 @@
 # valid UIDs (http://www.medicalconnections.co.uk/FreeUID.html)
 # Their service was used to obtain the following root UID for PyMedPhys:
 
+import json
 from os import path
 
 PYMEDPHYS_ROOT_UID = "1.2.826.0.1.3680043.10.188"
