08d04c5fc83bc7c736498358aa2a2e3f25514868
Simon Biggs <me@simonbiggs.net>
2019-08-30 21:46:32 +1000

    fix tests
    Former-commit-id: 881b5691a0946622554638876fd12bc0c7829507 [formerly 87060de805386063d30fd451b9aabc2ab1efa772]
    Former-commit-id: aa46f876e03afb5a322894ee1f62f4eb3f11ecd2

--- packages/pymedphys_labs/tests/tpscompare/test_dose_profiles.py
+++ packages/pymedphys_labs/tests/tpscompare/test_dose_profiles.py
@@ -37,16 +37,20 @@ import pandas as pd
 import pydicom
 
 from pymedphys_dicom.dicom import depth_dose, profile
-from pymedphys_analysis.tpscompare import load_and_normalise_mephysto
+from pymedphys_labs.tpscompare import load_and_normalise_mephysto
 
-from shared import (BASELINES_DIR, DATA_DIR,
-                    DICOM_DOSE_FILEPATHS, DICOM_PLAN_FILEPATH, DICOM_DIR,
-                    MEASUREMENTS_DIR)
+from shared import (
+    BASELINES_DIR,
+    DATA_DIR,
+    DICOM_DOSE_FILEPATHS,
+    DICOM_PLAN_FILEPATH,
+    DICOM_DIR,
+    MEASUREMENTS_DIR,
+)
 
 
 CREATE_BASELINE = False
-BASELINE_FILEPATH = os.path.join(BASELINES_DIR,
-                                 'dicom_dose_profiles.json')
+BASELINE_FILEPATH = os.path.join(BASELINES_DIR, "dicom_dose_profiles.json")
 
 
 @pytest.fixture
@@ -69,27 +73,30 @@ def loaded_plan():
 
 
 def test_bulk_compare(loaded_doses, loaded_plan):
-    absolute_dose_table = pd.read_csv(MEASUREMENTS_DIR.joinpath(
-        'AbsoluteDose.csv'), index_col=0)
-    absolute_dose = absolute_dose_table['d10 @ 90 SSD']['6 MV']
+    absolute_dose_table = pd.read_csv(
+        MEASUREMENTS_DIR.joinpath("AbsoluteDose.csv"), index_col=0
+    )
+    absolute_dose = absolute_dose_table["d10 @ 90 SSD"]["6 MV"]
 
     output_factors = pd.read_csv(
-        MEASUREMENTS_DIR.joinpath('OutputFactors.csv'), index_col=0)
+        MEASUREMENTS_DIR.joinpath("OutputFactors.csv"), index_col=0
+    )
 
     absolute_doses = {
-        key: output_factors[key]['6 MV'] * absolute_dose
+        key: output_factors[key]["6 MV"] * absolute_dose
         for key in output_factors.columns
     }
 
     absolute_scans_per_field = load_and_normalise_mephysto(
-        MEASUREMENTS_DIR, '06MV_(\d\dx\d\d)\.mcc', absolute_doses, 100)
+        MEASUREMENTS_DIR, "06MV_(\d\dx\d\d)\.mcc", absolute_doses, 100
+    )
 
-    getter = operator.itemgetter('displacement', 'dose')
+    getter = operator.itemgetter("displacement", "dose")
 
     for key, absolute_scans in absolute_scans_per_field.items():
         dose_dataset = loaded_doses[key]
 
-        depths, meas_dose = getter(absolute_scans['depth_dose'])
+        depths, meas_dose = getter(absolute_scans["depth_dose"])
         tps_dose = depth_dose(depths, dose_dataset, loaded_plan) / 10
         diff = tps_dose - meas_dose
 
@@ -108,22 +115,23 @@ def test_baseline_profiles(loaded_doses, loaded_plan):
 
         extracted_dose = depth_dose(depths, dose_dataset, loaded_plan)
         rounded_result = np.around(extracted_dose, decimals=2)
-        baselines[key]['depth'] = rounded_result.tolist()
+        baselines[key]["depth"] = rounded_result.tolist()
 
-        for direction in ['inplane', 'crossplane']:
+        for direction in ["inplane", "crossplane"]:
             baselines[key][direction] = {}
             for depth in [50, 100]:
                 extracted_dose = profile(
-                    displacements, depth, direction, dose_dataset, loaded_plan)
+                    displacements, depth, direction, dose_dataset, loaded_plan
+                )
                 rounded_result = np.around(extracted_dose, decimals=2)
                 baselines[key][direction][str(depth)] = rounded_result.tolist()
 
     if CREATE_BASELINE:
-        with open(BASELINE_FILEPATH, 'w') as a_file:
+        with open(BASELINE_FILEPATH, "w") as a_file:
             json.dump(baselines, a_file)
 
     else:
-        with open(BASELINE_FILEPATH, 'r') as a_file:
+        with open(BASELINE_FILEPATH, "r") as a_file:
             baseline_result = json.load(a_file)
 
         assert baseline_result == baselines
