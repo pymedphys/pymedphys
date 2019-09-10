85164538a8d5c413052651cd6ee16e1030b0ffcc
SimonBiggs <me@simonbiggs.net>
2019-09-10 13:56:52 +1000

    yarn tree
    Former-commit-id: 6b58c0b79a564b9fb955c6d5ac9344f874fb1b52 [formerly 1e9d45181019cc9595d720bf6b3fa810635ca5a1]
    Former-commit-id: 0f8e7774262b1280d4edeb9cc6cb11b85714801c

--- packages/pymedphys_analysis/src/pymedphys_analysis/_install_requires.py
+++ packages/pymedphys_analysis/src/pymedphys_analysis/_install_requires.py
@@ -3,7 +3,7 @@ install_requires = [
     "matplotlib",
     "numpy >= 1.12",
     "pydicom",
-    "pymedphys_dicom >= 0.10.0, < 0.11.0",
+    "pymedphys_dicom >= 0.11.0dev0, < 0.12.0",
     "scikit-image",
     "scipy",
 ]
