3ed7bdd9160e4d15a0645ff3a4959277896486c4
Simon Biggs <me@simonbiggs.net>
2019-09-10 21:35:02 +1000

    remove xlwings

--- packages/pymedphys_xlwings/src/pymedphys_xlwings/_install_requires.py
+++
@@ -1,9 +0,0 @@
-install_requires = [
-    "numpy >= 1.12",
-    "pydicom",
-    "pymedphys_dicom >= 0.12.0dev0, < 0.13.0",
-    "pymedphys_fileformats >= 0.12.0dev0, < 0.13.0",
-    "pymedphys_utilities >= 0.12.0dev0, < 0.13.0",
-    "scipy",
-    'xlwings ; platform_system != "Linux"',
-]
