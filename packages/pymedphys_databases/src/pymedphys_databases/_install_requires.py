8bb734797017d79557134914a013b22122e2a5fb
Simon Biggs <me@simonbiggs.net>
2019-09-10 19:17:16 +1000

    bump version
    Former-commit-id: 142543f407f857a458ef83af4854ad982726f316

--- packages/pymedphys_databases/src/pymedphys_databases/_install_requires.py
+++ packages/pymedphys_databases/src/pymedphys_databases/_install_requires.py
@@ -3,9 +3,9 @@ install_requires = [
     "keyring",
     "numpy >= 1.12",
     "pandas",
-    "pymedphys_base >= 0.11.0dev0, < 0.12.0",
-    "pymedphys_fileformats >= 0.11.0dev0, < 0.12.0",
-    "pymedphys_mudensity >= 0.11.0dev0, < 0.12.0",
-    "pymedphys_utilities >= 0.11.0dev0, < 0.12.0",
+    "pymedphys_base >= 0.11.0, < 0.12.0",
+    "pymedphys_fileformats >= 0.11.0, < 0.12.0",
+    "pymedphys_mudensity >= 0.11.0, < 0.12.0",
+    "pymedphys_utilities >= 0.11.0, < 0.12.0",
     "pymssql",
 ]
--- packages/pymedphys_dicom/src/pymedphys_dicom/_install_requires.py
+++ packages/pymedphys_dicom/src/pymedphys_dicom/_install_requires.py
@@ -3,6 +3,6 @@ install_requires = [
     "numpy >= 1.12",
     "packaging",
     "pydicom",
-    "pymedphys_base >= 0.11.0dev0, < 0.12.0",
-    "pymedphys_utilities >= 0.11.0dev0, < 0.12.0",
+    "pymedphys_base >= 0.11.0, < 0.12.0",
+    "pymedphys_utilities >= 0.11.0, < 0.12.0",
 ]
