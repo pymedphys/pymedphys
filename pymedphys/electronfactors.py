4ff7c8f061743a6e30c25ec8a656387101c7cfee
Simon Biggs <me@simonbiggs.net>
2019-09-12 20:01:50 +1000

    add electronfactors
    Former-commit-id: fbb7eb5e3ac03902ede86b5cd895dde5339a1cc1

---
+++ pymedphys/electronfactors.py
@@ -0,0 +1,10 @@
+from ._electronfactors import (
+    parameterise_insert,
+    spline_model,
+    calculate_deformability,
+    spline_model_with_deformability,
+    calculate_percent_prediction_differences,
+    visual_alignment_of_equivalent_ellipse,
+    convert2_ratio_perim_area,
+    create_transformed_mesh,
+)
