e37f588f0781a1ade7b0b4b1377abc03657863f2
Simon Biggs <me@simonbiggs.net>
2019-09-22 21:06:19 +1000

    fixing module imports
    Former-commit-id: c5f7ed5024bd1e9b286449164e2ae5c970eaab90 [formerly 2813ea3182b5edfc0a32c0c0b45a2adb0872d2be]
    Former-commit-id: 42e5d01ee83302325737e223acc7f42c5f7f6458

--- pymedphys/_dicom/create.py
+++ pymedphys/_dicom/create.py
@@ -27,7 +27,7 @@ from copy import deepcopy
 
 import numpy as np
 
-from pymedphys._dicom.shim import pydicom
+import pydicom
 
 
 DICOM_NAMES = [item[-1] for _, item in pydicom.datadict.DicomDictionary.items()]
