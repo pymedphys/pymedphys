30310715fba36b74adf06b321371ebf69b9cf2d6
Simon Biggs <me@simonbiggs.net>
2019-09-23 21:29:25 +1000

    update dicom gamma demo

--- src/pymedphys/dicom.py
+++ src/pymedphys/dicom.py
@@ -1,5 +1,5 @@
 # pylint: disable=unused-import, missing-docstring
 
 from ._dicom.anonymise import anonymise
-from ._dicom.dose import depth_dose, profile
+from ._dicom.dose import depth_dose, profile, zyx_and_dose_from_dataset
 from ._dicom.ct.extend import extend_ct
