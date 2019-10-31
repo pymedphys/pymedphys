292dcd2feaae2a5585c9f05cabc1b3feb2dc34e3
SimonBiggs <me@simonbiggs.net>
2019-10-31 13:05:46 +1100

    handle zenodo zip files

--- pymedphys/__init__.py
+++ pymedphys/__init__.py
@@ -1,7 +1,7 @@
 """Module docstring."""
 
 from . import dicom, electronfactors, mosaiq, mudensity
-from ._data import data_path
+from ._data import data_path, zip_data_paths
 from ._delivery import Delivery
 from ._gamma.implementation.shell import gamma_shell as gamma
 from ._trf import read_trf
