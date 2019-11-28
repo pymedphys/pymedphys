e63005ca19277591cf0b6c8b7130915d0ec65fbb
Simon Biggs <me@simonbiggs.net>
2019-11-28 21:03:51 +1100

    added tornado

--- app/main.py
+++ app/main.py
@@ -1,15 +1,26 @@
+import pathlib
 import sys
 
 from cefpython3 import cefpython as cef
+
+HERE = pathlib.Path(__file__).resolve().parent
 
 
 def main():
     sys.excepthook = cef.ExceptHook
 
-    switches = {"disable-web-security": ""}
+    switches = {
+        "disable-web-security": "",
+        "file_access_from_file_urls_allowed": "",
+        "universal_access_from_file_urls_allowed": "",
+    }
     cef.Initialize(switches=switches)
 
-    cef.CreateBrowserSync(url="file:///build/index.html")
+    entry_point = HERE.joinpath("build", "index.html")
+    url = f"file://{entry_point}"
+    print(url)
+
+    cef.CreateBrowserSync(url=url)
     cef.MessageLoop()
     cef.Shutdown()
 
