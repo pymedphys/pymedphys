e600f325c52753179d1a6314cee57b007bd82947
Simon Biggs <me@simonbiggs.net>
2019-11-28 23:11:44 +1100

    jupyter inside CEF
    Former-commit-id: bbf7311b66754c8cd306b6f84c16d6f962545f94

--- app/main.py
+++ app/main.py
@@ -1,25 +1,48 @@
-import pathlib
+import multiprocessing
+import secrets
 import sys
 
+import notebook.notebookapp
+import pymedphys
 from cefpython3 import cefpython as cef
 
-HERE = pathlib.Path(__file__).resolve().parent
+IP = "127.0.0.1"
+
+
+class PyMedPhys(notebook.notebookapp.NotebookApp):
+    name = "PyMedPhys"
+    version = pymedphys.__version__
+    description = """
+        PyMedPhys
+        Kernel hosting for PyMedPhys app.
+    """
+    # default_url = "/"
+    queue = None
+
+    def initialize(self, argv=None):
+        super_result = super().initialize(argv=argv)
+
+        self.queue.put((self.port, self.token))
+
+        return super_result
+
+
+def launch_kernel_server(queue):
+    PyMedPhys.launch_instance(queue=queue, ip=IP, open_browser=False)
 
 
 def main():
     sys.excepthook = cef.ExceptHook
 
-    switches = {
-        "disable-web-security": "",
-        "file_access_from_file_urls_allowed": "",
-        "universal_access_from_file_urls_allowed": "",
-    }
-    cef.Initialize(switches=switches)
+    queue = multiprocessing.Queue()
 
-    entry_point = HERE.joinpath("build", "index.html")
-    url = f"file://{entry_point}"
-    print(url)
+    kernel_process = multiprocessing.Process(target=launch_kernel_server, args=(queue,))
+    kernel_process.start()
 
+    port, token = queue.get()
+    url = f"http://{IP}:{port}/?token={token}"
+
+    cef.Initialize()
     cef.CreateBrowserSync(url=url)
     cef.MessageLoop()
     cef.Shutdown()
