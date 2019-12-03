c7761c10a5a45fe6fc301bcfc8467d96dbe9e6d3
peterg1t <pedro.enrique.83@gmail.com>
2019-12-02 17:13:56 -0700

    remove containing folders
    Former-commit-id: e18a115eb9ed1ba882930c781f3d4a4ed5d877fb

---
+++ pymedphys/labs/pedromartinez/running_mean.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def running_mean(x, N):
+    out = np.zeros_like(x, dtype=np.float64)
+    dim_len = x.shape[0]
+    for i in range(dim_len):
+        if N % 2 == 0:
+            a, b = i - (N - 1) // 2, i + (N - 1) // 2 + 2
+        else:
+            a, b = i - (N - 1) // 2, i + (N - 1) // 2 + 1
+
+        # cap indices to min and max indices
+        a = max(0, a)
+        b = min(dim_len, b)
+        out[i] = np.mean(x[a:b])
+    return out
