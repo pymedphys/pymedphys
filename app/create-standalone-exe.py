46171146fa1b9ce5cdfb3f4c518bfda3ac9df615
SimonBiggs <me@simonbiggs.net>
2019-12-04 16:46:23 +1100

    begin standalone exe build script
    Former-commit-id: 278370ba3afd269495d766c104947c1e30b5d312

---
+++ app/create-standalone-exe.py
@@ -0,0 +1,10 @@
+import pymedphys
+
+
+def main():
+    embedded_python_path = pymedphys.data_path("python-windows-64-embedded.zip")
+    get_pip_path = pymedphys.data_path("get-pip.py")
+
+
+if __name__ == "__main__":
+    main()
