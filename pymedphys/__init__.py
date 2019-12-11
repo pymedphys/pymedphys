fbe44b4b467a1cfb29b225da4b421c26fedaf01b
Simon Biggs <me@simonbiggs.net>
2019-12-11 19:23:41 +1100

    pymedphys starting tkinter having issues
    Former-commit-id: 8edb54ab376bd4ae91e35b1e9431e294e9b764de [formerly de2024460d0bf670133c7ab9c475c7a4a779f297]
    Former-commit-id: 596917458f2a4a03bc13e1be9bb349152e79c4d4

--- pymedphys/__init__.py
+++ pymedphys/__init__.py
@@ -1,6 +1,5 @@
 """Module docstring."""
 
-from . import dicom, electronfactors, mosaiq, mudensity
 from ._data import data_path, zip_data_paths
 from ._delivery import Delivery
 from ._gamma.implementation.shell import gamma_shell as gamma
