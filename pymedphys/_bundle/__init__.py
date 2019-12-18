0155cdef17bd05682f97d95a23cf1e21ef826927
Simon Biggs <me@simonbiggs.net>
2019-12-18 20:12:12 +1100

    begin miniconda exp
    Former-commit-id: 0af387879305f94341fcccb1667588108b55341c [formerly f3195f7010bf29239c345d7c630e9a52ccacd5b2]
    Former-commit-id: e268f3698c9cd8fd5cd96b3315cf3b6c84f8f8da

--- pymedphys/_bundle/__init__.py
+++ pymedphys/_bundle/__init__.py
@@ -39,86 +39,89 @@ def call_embedded_python(embedded_python_dir, *args):
     subprocess.check_call(to_be_called, cwd=embedded_python_dir)
 
 
-def main(_):
+def main(args):
     cwd = pathlib.Path(os.getcwd())
     build = cwd.joinpath("build")
 
-    embedded_python_dir = build.joinpath("python")
+    if args.clean:
+        shutil.rmtree(build, ignore_errors=True)
 
-    notebooks_dir = cwd.joinpath("notebooks")
-    requirements_txt = cwd.joinpath("requirements.txt")
+    # embedded_python_dir = build.joinpath("python")
+    # copied_notebooks_dir = build.joinpath("notebooks")
 
-    if not notebooks_dir.exists():
-        raise ValueError(
-            "Need to have a `notebooks` directory where this command is called"
-        )
+    # notebooks_dir = cwd.joinpath("notebooks")
+    # requirements_txt = cwd.joinpath("requirements.txt")
 
-    if not requirements_txt.exists():
-        raise ValueError(
-            "Need to have a `requirements.txt` file where this command is called"
-        )
+    # if not notebooks_dir.exists():
+    #     raise ValueError(
+    #         "Need to have a `notebooks` directory where this command is called"
+    #     )
 
-    # shutil.rmtree(embedded_python_dir, ignore_errors=True)
+    # if not requirements_txt.exists():
+    #     raise ValueError(
+    #         "Need to have a `requirements.txt` file where this command is called"
+    #     )
 
-    embedded_python_path = pymedphys.data_path("python-windows-64-embedded.zip")
+    # embedded_python_path = pymedphys.data_path("python-windows-64-embedded.zip")
 
-    with zipfile.ZipFile(embedded_python_path, "r") as zip_obj:
-        zip_obj.extractall(embedded_python_dir)
+    # with zipfile.ZipFile(embedded_python_path, "r") as zip_obj:
+    #     zip_obj.extractall(embedded_python_dir)
 
-    copied_notebooks_dir = build.joinpath("notebooks")
-    shutil.rmtree(copied_notebooks_dir, ignore_errors=True)
-    shutil.copytree(notebooks_dir, copied_notebooks_dir)
+    # try:
+    #     shutil.copytree(notebooks_dir, copied_notebooks_dir)
+    # except FileExistsError:
+    #     pass
 
-    get_pip_path = pymedphys.data_path("get-pip.py")
-    call_embedded_python(embedded_python_dir, get_pip_path)
+    # get_pip_path = pymedphys.data_path("get-pip.py")
+    # call_embedded_python(embedded_python_dir, get_pip_path)
 
-    python_path_file = next(embedded_python_dir.glob("python*._pth"))
+    # python_path_file = next(embedded_python_dir.glob("python*._pth"))
 
-    temp_python_path_file = f"{python_path_file}.temp"
-    file_contents_replace(python_path_file, "#import site", "import site")
+    # temp_python_path_file = f"{python_path_file}.temp"
+    # file_contents_replace(python_path_file, "#import site", "import site")
 
-    call_embedded_python(embedded_python_dir, "-m", "pip", "--version")
+    # call_embedded_python(embedded_python_dir, "-m", "pip", "--version")
 
-    os.rename(python_path_file, temp_python_path_file)
-    call_embedded_python(
-        embedded_python_dir,
-        "-m",
-        "pip",
-        "install",
-        "-r",
-        requirements_txt,
-        "--no-warn-script-location",
-    )
-    os.rename(temp_python_path_file, python_path_file)
+    # os.rename(python_path_file, temp_python_path_file)
+    # call_embedded_python(
+    #     embedded_python_dir,
+    #     "-m",
+    #     "pip",
+    #     "install",
+    #     "-r",
+    #     requirements_txt,
+    #     "--no-warn-script-location",
+    # )
+    # os.rename(temp_python_path_file, python_path_file)
 
-    call_embedded_python(
-        embedded_python_dir,
-        "-c",
-        "import pymedphys._jupyterlab.main; pymedphys._jupyterlab.main.get_build()",
-    )
+    # call_embedded_python(
+    #     embedded_python_dir,
+    #     "-c",
+    #     "import pymedphys._jupyterlab.main; pymedphys._jupyterlab.main.get_build()",
+    # )
 
-    build_files_to_copy = ["package.json", "yarn.lock"]
-    build_dirs_to_copy = ["src"]
+    # build_files_to_copy = ["package.json", "yarn.lock"]
+    # build_dirs_to_copy = ["src"]
 
-    for file_to_copy in build_files_to_copy:
-        try:
-            shutil.copy2(HERE.joinpath(file_to_copy), build.joinpath(file_to_copy))
-        except FileExistsError:
-            pass
+    # for file_to_copy in build_files_to_copy:
+    #     try:
+    #         shutil.copy2(HERE.joinpath(file_to_copy), build.joinpath(file_to_copy))
+    #     except FileExistsError:
+    #         pass
 
-    for dirs_to_copy in build_dirs_to_copy:
-        try:
-            shutil.copytree(HERE.joinpath(dirs_to_copy), build.joinpath(dirs_to_copy))
-        except FileExistsError:
-            pass
+    # for dirs_to_copy in build_dirs_to_copy:
+    #     try:
+    #         shutil.copytree(HERE.joinpath(dirs_to_copy), build.joinpath(dirs_to_copy))
+    #     except FileExistsError:
+    #         pass
 
-    subprocess.check_call(["yarn"], cwd=build)
-    subprocess.check_call(["yarn", "dist"], cwd=build)
+    # subprocess.check_call(["yarn"], cwd=build)
+    # subprocess.check_call(["yarn", "dist"], cwd=build)
 
-    final_exe = cwd.joinpath("JupyterLab Setup.exe")
-    try:
-        final_exe.unlink()
-    except FileNotFoundError:
-        pass
+    # final_exe = cwd.joinpath("JupyterLab Setup.exe")
+    # try:
+    #     final_exe.unlink()
+    # except FileNotFoundError:
+    #     pass
 
-    shutil.copy2(build.joinpath("release", "JupyterLab Setup 0.0.0.exe"), final_exe)
+    # shutil.copy2(build.joinpath("release", "JupyterLab Setup 0.0.0.exe"), final_exe)
