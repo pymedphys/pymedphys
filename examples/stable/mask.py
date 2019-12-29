1d4bc69e5c9d3a00ac1438ba931d27b7d0c2ec24
Simon Biggs <me@simonbiggs.net>
2019-12-30 07:37:25 +1100

    include a simple contour and overlay matplotlibs contour interpretation

--- examples/stable/mask.py
+++ examples/stable/mask.py
@@ -1,11 +1,12 @@
 import functools
 
+import numpy as np
+
 import matplotlib.pyplot as plt
 
 import tensorflow as tf
 
 
-@tf.function
 def reduce_expanded_mask(expanded_mask, img_size, expansion):
     expanded_mask = tf.dtypes.cast(expanded_mask, tf.float32)
     return tf.reduce_mean(
@@ -17,7 +18,6 @@ def reduce_expanded_mask(expanded_mask, img_size, expansion):
     )
 
 
-@tf.function
 def get_circle_mask(bb_centre, bb_radius_sqrd, img_size, expansion):
     _, _, xx_expand, yy_expand = get_grids(img_size, expansion)
     expanded_mask = (xx_expand - bb_centre[0]) ** 2 + (
@@ -25,6 +25,15 @@ def get_circle_mask(bb_centre, bb_radius_sqrd, img_size, expansion):
     ) ** 2 <= bb_radius_sqrd
 
     circle_mask = reduce_expanded_mask(expanded_mask, img_size, expansion)
+    return circle_mask * 2 - 1
+
+
+def get_simple_circle_mask(bb_centre, bb_radius_sqrd, x, y):
+    xx, yy = tf.meshgrid(x, y)
+
+    circle_mask = (xx - bb_centre[0]) ** 2 + (yy - bb_centre[1]) ** 2 <= bb_radius_sqrd
+    circle_mask = tf.dtypes.cast(circle_mask, tf.float32)
+
     return circle_mask * 2 - 1
 
 
@@ -42,17 +51,40 @@ def get_grids(img_size, expansion):
 
 
 def main():
-    img_size = 128
-    expansion = 16
+    img_size = 32
+    expansion = 4
 
     x, y, _, _ = get_grids(img_size, expansion)
 
     bb_diameter = 8.0
-    bb_radius_sqrd = (bb_diameter / 2) ** 2
+    bb_radius = bb_diameter / 2
+    bb_radius_sqrd = bb_radius ** 2
 
-    circle_mask = get_circle_mask([60.5, 20.1], bb_radius_sqrd, img_size, expansion)
+    bb_centre_1 = [10.5, 10.1]
+    bb_centre_2 = [20.5, 20.1]
 
-    plt.pcolormesh(x - 0.5, y - 0.5, circle_mask)
+    circle_mask = get_circle_mask(bb_centre_1, bb_radius_sqrd, img_size, expansion)
+    simple_circle_mask = get_simple_circle_mask(bb_centre_2, bb_radius_sqrd, x, y)
+
+    total_mask = tf.stack([circle_mask, simple_circle_mask], axis=-1)
+    total_mask = tf.reduce_sum(total_mask, axis=-1) + 1
+
+    theta = tf.range(0, 2 * np.pi, 0.1)
+    circle_x_base = tf.sin(theta) * bb_radius
+    circle_y_base = tf.cos(theta) * bb_radius
+
+    bb_1_x = circle_x_base + bb_centre_1[0]
+    bb_1_y = circle_y_base + bb_centre_1[1]
+    bb_2_x = circle_x_base + bb_centre_2[0]
+    bb_2_y = circle_y_base + bb_centre_2[1]
+
+    plt.pcolormesh(x - 0.5, y - 0.5, total_mask)
+    plt.colorbar()
+
+    plt.contour(x, y, total_mask, [0], cmap="bwr_r")
+    plt.plot(bb_1_x, bb_1_y, "k--", alpha=0.5)
+    plt.plot(bb_2_x, bb_2_y, "k--", alpha=0.5)
+
     plt.axis("equal")
     plt.show()
 
