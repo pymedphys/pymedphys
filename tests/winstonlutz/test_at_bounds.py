ab7e90fc0f9a708c6418e86f31596e1482f70f98
SimonBiggs <me@simonbiggs.net>
2020-01-09 09:28:23 +1100

    update tests to new api

--- tests/winstonlutz/test_at_bounds.py
+++ tests/winstonlutz/test_at_bounds.py
@@ -19,13 +19,10 @@ import pymedphys._wlutz.findbb
 
 
 def test_bb_at_bounds_check():
-    penumbra = 2
     edge_lengths = [20, 24]
     bb_diameter = 8
 
-    bb_bounds = pymedphys._wlutz.findbb.define_bb_bounds(
-        bb_diameter, edge_lengths, penumbra
-    )
+    bb_bounds = pymedphys._wlutz.findbb.define_bb_bounds(bb_diameter, edge_lengths)
     assert pymedphys._wlutz.findbb.check_if_at_bounds((0, -7.5), bb_bounds)
     assert not pymedphys._wlutz.findbb.check_if_at_bounds((0, -5.5), bb_bounds)
     assert pymedphys._wlutz.findbb.check_if_at_bounds((5.5, 0), bb_bounds)
