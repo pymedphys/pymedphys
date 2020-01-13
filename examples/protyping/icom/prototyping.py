4d6049548b3ce4a0ce6aea9f336141c0d5c7d2aa
SimonBiggs <me@simonbiggs.net>
2020-01-13 16:01:29 +1100

    refining icom extraction
    Former-commit-id: 37ca736a68a3ce7351054d5b84a90c075333c6b5 [formerly 2f47755cfd70bdb0b1abcb1c31f487418e69fd99]
    Former-commit-id: 80503076b3cba71f9e099920f4d0df9f1e29f86d

--- examples/protyping/icom/prototyping.py
+++ examples/protyping/icom/prototyping.py
@@ -1,20 +1,19 @@
-import copy
 import datetime
 import functools
-import lzma
 import os
-import pathlib
 import re
 
 import numpy as np
-import pandas as pd
+
+# pylint: disable = protected-access
+import pymedphys._utilities.filesystem
 
 offset = 20000
-date_pattern = re.compile(b"\d\d\d\d-\d\d-\d\d\d\d:\d\d:\d\d.")
-item_pattern = re.compile(b"\x00\x00\x00([a-zA-Z0-9 \.-]+)")
+DATE_PATTERN = re.compile(rb"\d\d\d\d-\d\d-\d\d\d\d:\d\d:\d\d.")
+ITEM_PATTERN = re.compile(rb"\x00\x00\x00([a-zA-Z0-9 \.-]+)")
 
 
-def get_most_recent_data_point():
+def get_most_recent_data_point(live_path):
     with pymedphys._utilities.filesystem.open_no_lock(live_path, "rb") as f:
         f.seek(0, os.SEEK_END)
         file_size = f.tell()
@@ -26,7 +25,7 @@ def get_most_recent_data_point():
 
 
 def get_data_points(data):
-    date_index = [m.span() for m in date_pattern.finditer(data)]
+    date_index = [m.span() for m in DATE_PATTERN.finditer(data)]
     start_points = [span[0] - 8 for span in date_index]
 
     end_points = start_points[1::] + [None]
@@ -54,8 +53,8 @@ def get_jaw_and_mlc(results_by_line):
 
 @functools.lru_cache()
 def get_coll_regex(label, number):
-    header = b"0\xb8\x00DS\x00R[\x03\x04\x05\x06]\x00\x00\x00" + label + b"\n"
-    item = b"0\x1c\x01DS\x00R[\x03\x04\x05\x06]\x00\x00\x00(-?\d+\.\d+)"
+    header = rb"0\xb8\x00DS\x00R.\x00\x00\x00" + label + b"\n"
+    item = rb"0\x1c\x01DS\x00R.\x00\x00\x00(-?\d+\.\d+)"
 
     regex = re.compile(header + b"\n".join([item] * number))
     return regex
@@ -75,85 +74,103 @@ def extract_coll(data, label, number):
 
 @functools.lru_cache()
 def get_lookup_regex(key):
-    regex = re.compile(
-        b"[0\x00pP]"
-        + key
-        + b"\x00[PSR][\x01\x02\x03\x04\x05\x06]\x00\x00\x00([a-zA-Z0-9 \.-]+)"
-    )
+    regex = re.compile(rb"[0\x00pP]" + key + rb".\x00\x00\x00([a-zA-Z0-9 \.-]+)")
     return regex
 
 
-def extract_by_lookup(data, key):
+def extract_by_lookup(data, key, this_type):
     regex = get_lookup_regex(key)
     match = regex.search(data)
 
-    span = match.span()
+    try:
+        span = match.span()
+    except AttributeError:
+        return data, None
+
     removed_data = data[0 : span[0]] + data[span[1] + 1 : :]
 
-    return removed_data, match.group(1)
+    result = match.group(1)
+    if result == b"-32767":
+        removed_data, result = extract_by_lookup(removed_data, key, this_type)
+    elif this_type is str:
+        result = result.decode()
+    else:
+        result = this_type(result)
+
+    return removed_data, result
 
 
 def extract_all_lookup(data, result):
     for key, label in lookup.items():
-        try:
-            data, result[label] = extract_by_lookup(data, key)
-        except AttributeError:
-            result[label] = None
+        data, result[label] = extract_by_lookup(data, key, types[label])
 
     return data
 
 
-def run_all_assertions(data, result):
-    for key, label in assert_equal.items():
-        try:
-            while True:
-                data, new_result = extract_by_lookup(data, key)
-                if result[label] != new_result:
-                    raise ValueError(
-                        f"Expected these to be the same:\n{label}\n{result[label]}\n{new_result}"
-                    )
-        except AttributeError:
-            pass
+def extract_all_multiples(data, result):
+    for key, label in multiples.items():
+        result[label] = []
+
+        while True:
+            data, a_result = extract_by_lookup(data, key, types[label])
+            if a_result is not None:
+                result[label].append(a_result)
+            else:
+                break
 
     return data
+
+
+# def run_all_assertions(data, result):
+#     for key, label in assert_equal.items():
+#         try:
+#             while True:
+#                 data, new_result = extract_by_lookup(data, key, types[label])
+#                 if result[label] != new_result:
+#                     raise ValueError(
+#                         f"Expected these to be the same:\n{label}\n{result[label]}\n{new_result}"
+#                     )
+#         except AttributeError:
+#             pass
+
+#     return data
 
 
 def strict_extract(data):
     result = dict()
 
+    timestamp_and_counter = DATE_PATTERN.search(data).group(0)
+    timestamp = timestamp_and_counter[:-1]
+    counter = int(timestamp_and_counter[-1])
+    if counter > 9:
+        counter = counter - 1
+
+    result["Timestamp"] = datetime.datetime.strptime(
+        timestamp.decode(), "%Y-%m-%d%H:%M:%S"
+    ).isoformat()
+    result["Counter"] = counter
+
     data = extract_all_lookup(data, result)
+    data = extract_all_multiples(data, result)
 
-    data, result["mlc"] = extract_coll(data, b"MLCX", 160)
-    data, result["jaw_x"] = extract_coll(data, b"ASYMX", 2)
-    data, result["jaw_y"] = extract_coll(data, b"ASYMY", 2)
+    data, result["MLCX"] = extract_coll(data, b"MLCX", 160)
+    data, result["ASYMX"] = extract_coll(data, b"ASYMX", 2)
+    data, result["ASYMY"] = extract_coll(data, b"ASYMY", 2)
 
-    data = run_all_assertions(data, result)
-
-    for key, a_type in types.items():
-        if result[key] == b"-32767":
-            result[key] = None
-            continue
-
-        if a_type is str:
-            result[key] = result[key].decode()
-        else:
-            result[key] = a_type(result[key])
-
+    # data = run_all_assertions(data, result)
     return data, result
 
 
 def get_patient_id(data_point):
-    try:
-        _, patient_id = extract_by_lookup(data_point, inverse_lookup["Patient ID"])
-        patient_id = patient_id.decode()
-    except AttributeError:
-        patient_id = None
-
+    _, patient_id = extract_by_lookup(data_point, inverse_lookup["Patient ID"], str)
     return patient_id
 
 
 types = {
     "Patient ID": str,
+    "Patient Name": str,
+    "Rx Site": str,
+    "Field ID": str,
     "Machine ID": str,
     "Radiation Type": str,
     "Energy": str,
@@ -171,36 +188,48 @@ types = {
     "Table Vertical": float,
     "Table Longitudinal": float,
     "Table Lateral": float,
+    "Interlocks": str,
+    "Previous Interlocks": str,
+    "Beam Description": str,
 }
 
 
 lookup = {
-    b" \x00LO": "Patient ID",
-    b"\xb2\x00SH": "Machine ID",
-    b"\xc6\x00CS": "Radiation Type",
-    b"\x14\x01SH": "Energy",
-    b"\x18\x01CS": "Wedge",
-    b"\x07\x10DS": "Segment",
-    b"\t\x10DS": "Total MU",
-    b"2\x00DS": "Delivery MU",
-    b"3\x00DS": "Backup Delivery MU",
-    b"8\x00SH": "Beam Timer",
-    b"\x0b\x00DS": "Segment MU",
-    b"\x1e\x01DS": "Gantry",
-    b" \x01DS": "Collimator",
-    b'"\x01DS': "Table Column",
-    b"\%\x01DS": "Table Isocentric",
-    b"\(\x01DS": "Table Vertical",
-    b"\)\x01DS": "Table Longitudinal",
-    b"\*\x01DS": "Table Lateral",
+    b" \x00LO\x00P": "Patient ID",
+    b"\x10\x00PN\x00P": "Patient Name",
+    b"\x01\x10LO\x00P": "Rx Site",
+    b"\x03\x10LO\x00P": "Field ID",
+    b"\xb2\x00SH\x00P": "Machine ID",
+    b"\xc6\x00CS\x00R": "Radiation Type",
+    b"\x14\x01SH\x00R": "Energy",
+    b"\x18\x01CS\x00R": "Wedge",
+    b"\x07\x10DS\x00R": "Segment",
+    b"\t\x10DS\x00R": "Total MU",
+    b"2\x00DS\x00R": "Delivery MU",
+    b"3\x00DS\x00R": "Backup Delivery MU",
+    b"8\x00SH\x00R": "Beam Timer",
+    b"\x0b\x00DS\x00R": "Segment MU",
+    b"\x1e\x01DS\x00R": "Gantry",
+    b" \x01DS\x00R": "Collimator",
+    b'"\x01DS\x00R': "Table Column",
+    rb"\%\x01DS\x00R": "Table Isocentric",
+    rb"\(\x01DS\x00R": "Table Vertical",
+    rb"\)\x01DS\x00R": "Table Longitudinal",
+    rb"\*\x01DS\x00R": "Table Lateral",
+    b"\x0c\x00SH\x00R": "Beam Description",
+}
+
+multiples = {
+    b"\x16\x10LO\x00R": "Interlocks",
+    b"\x18\x10LO\x00R": "Previous Interlocks",
 }
 
 inverse_lookup = {key: item for item, key in lookup.items()}
 
-assert_equal = {
-    #     b'\x07\x10DS': 'Segment',
-    b"\x06\x10LO": "Machine ID"
-}
+# assert_equal = {
+#     #     b'\x07\x10DS': 'Segment',
+# #     b"\x06\x10LO": "Machine ID"
+# }
 
 
 def initial_results_parse(data_point):
