ea7f697341f259be5d618aa3031ea01adc60f091
SimonBiggs <me@simonbiggs.net>
2020-01-16 10:24:48 +1100

    change to cyclic file storage method
    Former-commit-id: 6bd64a6150f41208a457eab98f33addfd705585e [formerly 5e09b7f9679fe60749123bf8a275999b8ead8db4]
    Former-commit-id: a80778789cba22f42183c8f331323cc2a37606d6

--- pymedphys/_icom/listener.py
+++ pymedphys/_icom/listener.py
@@ -1,35 +1,20 @@
-import lzma
-import multiprocessing
 import pathlib
-import shutil
+import re
 import socket
-from datetime import datetime
 
-MINUTES_OF_DATA = 5
-ICOM_HZ = 4
-SECONDS_OF_DATA = MINUTES_OF_DATA * 60
-BATCH = int(ICOM_HZ * SECONDS_OF_DATA)
-BUFFER_SIZE = 16384
+BUFFER_SIZE = 65536
 ICOM_PORT = 1706
 
 
-def compress_and_move_data(input_file, output_file):
-    input_file = pathlib.Path(input_file)
-    output_file = pathlib.Path(output_file)
-
-    with open(input_file, "rb") as in_file:
-        with lzma.open(output_file, "w") as out_file:
-            out_file.write(in_file.read())
-
-    input_file.unlink()
-
-
 def listen(ip, data_dir):
+    date_pattern = re.compile(rb"^\d\d\d\d-\d\d-\d\d\d\d:\d\d:\d\d$")
+
     data_dir = pathlib.Path(data_dir)
     live_dir = data_dir.joinpath("live")
+    ip_directory = live_dir.joinpath(ip)
     compressed_dir = data_dir.joinpath("compressed")
 
-    live_dir.mkdir(exist_ok=True, parents=True)
+    ip_directory.mkdir(exist_ok=True, parents=True)
     compressed_dir.mkdir(exist_ok=True)
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -37,32 +22,21 @@ def listen(ip, data_dir):
     print(s)
 
     try:
-        live_path = live_dir.joinpath(f"{ip}.txt")
-
         while True:
-            with open(live_path, "ba+") as f:
-                for _ in range(BATCH):
-                    f.write(s.recv(BUFFER_SIZE))
+            data = s.recv(BUFFER_SIZE)
 
-            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S_%f")
-            filename = f"{ip}_{timestamp}"
+            if not date_pattern.match(data[8:26]):
+                raise ValueError("Unexpected iCOM stream format")
 
-            temp_path = live_dir.joinpath(f"{filename}.txt")
-            shutil.move(live_path, temp_path)
+            counter = str(int(data[26])).zfill(3)
+            filepath = ip_directory.joinpath(f"{counter}.txt")
 
-            compressed_path = compressed_dir.joinpath(f"{filename}.xz")
-
-            multiprocessing.Process(
-                target=compress_and_move_data, args=(temp_path, compressed_path)
-            ).start()
+            with open(filepath, "bw+") as f:
+                f.write(data)
 
     finally:
         s.close()
         print(s)
-
-        multiprocessing.Process(
-            target=compress_and_move_data, args=(live_path, compressed_path)
-        ).start()
 
 
 def listen_cli(args):
