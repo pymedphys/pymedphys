37e4ddb27faf1662df8dcd728b8f589ed8c7ddb7
SimonBiggs <me@simonbiggs.net>
2020-01-17 16:19:56 +1100

    remove bb_at_bounds test file

--- pymedphys/_wlutz/iview.py
+++ pymedphys/_wlutz/iview.py
@@ -78,9 +78,39 @@ def batch_process(
     )
 
 
-def iview_image_transform(image_path):
+def iview_image_transform_from_path(image_path):
     img = imageio.imread(image_path)
 
+    if np.shape(img) == (1024, 1024):
+        pixels_per_mm = 4
+    elif np.shape(img) == (512, 512):
+        pixels_per_mm = 2
+    else:
+        raise ValueError(
+            "Expect iView images to be either 1024x1024 or 512x512 "
+            f"pixels\nShape = {np.shape(img)}"
+        )
+
+    img = img[:, 1:-1]
+
+    if img.dtype != np.dtype("uint16") and img.dtype != np.dtype("int32"):
+        raise ValueError(
+            "Expect iView images to have a pixel type of unsigned 16 bit "
+            "or signed 32 bit."
+            f"Instead the type was {img.dtype}\n"
+            f"  Min pixel value was {np.min(img)}\n"
+            f"  Max pixel value was {np.max(img)}"
+        )
+    img = 1 - img[::-1, :] / 2 ** 16
+
+    shape = np.shape(img)
+    x = np.arange(-shape[1] / 2, shape[1] / 2) / pixels_per_mm
+    y = np.arange(-shape[0] / 2, shape[0] / 2) / pixels_per_mm
+
+    return x, y, img
+
+
+def iview_image_transform(img):
     if np.shape(img) == (1024, 1024):
         pixels_per_mm = 4
     elif np.shape(img) == (512, 512):
