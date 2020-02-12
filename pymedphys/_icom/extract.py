b28545a93b1a247f45f7d6f549a1f2c647fd7a69
SimonBiggs <me@simonbiggs.net>
2020-02-13 10:15:50 +1100

    move prototype over to library
    Former-commit-id: 4a526d6638b51ca0f2adfa3e13bdf222d2df8b6a [formerly ba6263cdac39d0aa0ae51bbc6d97c68aeecf54fe]
    Former-commit-id: f6d6849f68dbc5ba7730fdd457a7ff724fb63afa

--- pymedphys/_icom/extract.py
+++ pymedphys/_icom/extract.py
@@ -3,6 +3,39 @@ import re
 
 from . import mappings
 
+DATE_PATTERN = re.compile(rb"\d\d\d\d-\d\d-\d\d\d\d:\d\d:\d\d")
+
+
+def get_data_points(data):
+    date_index = [m.span() for m in DATE_PATTERN.finditer(data)]
+    start_points = [span[0] - 8 for span in date_index]
+
+    end_points = start_points[1::] + [None]
+
+    data_points = [data[start:end] for start, end in zip(start_points, end_points)]
+    return data_points
+
+
+@functools.lru_cache()
+def get_coll_regex(label, number):
+    header = rb"0\xb8\x00DS\x00R.\x00\x00\x00" + label + b"\n"
+    item = rb"0\x1c\x01DS\x00R.\x00\x00\x00(-?\d+\.\d+)"
+
+    regex = re.compile(header + b"\n".join([item] * number))
+    return regex
+
+
+def extract_coll(data, label, number):
+    regex = get_coll_regex(label, number)
+
+    match = regex.search(data)
+    span = match.span()
+
+    data = data[0 : span[0]] + data[span[1] + 1 : :]
+    items = [float(item) for item in match.groups()]
+
+    return data, items
+
 
 @functools.lru_cache()
 def get_extraction_regex(key):
