2fe0b2aec6f36a7baa4c5fd22be3cac8bef49c26
SimonBiggs <me@simonbiggs.net>
2020-03-06 17:09:00 +1100

    fix pylint
    Former-commit-id: ba64bf9ace69514d1c3518b34f8fbf9b5246b3b2 [formerly be5b02175c0c5fe88fccff92c27d1c58e0958501]
    Former-commit-id: 25eff1838a87f281eed653b9de83c24104ce8994

--- pymedphys/_wlutz/findbb.py
+++ pymedphys/_wlutz/findbb.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import warnings
+# import warnings
 
 from pymedphys._imports import numpy as np
 from pymedphys._imports import scipy
