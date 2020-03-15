9cae693fc3c8458548e86a13f61aa2343565de50
Matthew Cooper <matthewdeancooper@gmail.com>
2020-03-15 23:17:05 +1100

    Added license

--- examples/protyping/pymedphys-segmentation/model.py
+++ examples/protyping/pymedphys-segmentation/model.py
@@ -1,3 +1,17 @@
+# Copyright (C) 2020 Matthew Cooper, Simon Biggs.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import config
 import tensorflow as tf
 
@@ -193,8 +207,3 @@ def Model(
     x = tf.keras.activations.sigmoid(x)
 
     return tf.keras.Model(inputs=inputs, outputs=x)
-
-
-# # with strategy.scope():
-# model = Model()
-# model.compile(optimizer='adam', loss=tf.keras.losses.MeanAbsoluteError())
