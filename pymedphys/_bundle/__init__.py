aa9e17293c57987202a75b3cbe9d7bcea87c5190
Simon Biggs <me@simonbiggs.net>
2020-04-22 19:04:49 +1000

    update bundle
    Former-commit-id: 8fff7d2547b5356bd32d1574f033879c8523c9ab [formerly 0aa15c5ac3cb27cfc87de5854d3137a53f613df5]
    Former-commit-id: 9c37cc9285cf5b198925e823618581bc9a7a34df

--- pymedphys/_bundle/__init__.py
+++ pymedphys/_bundle/__init__.py
@@ -1,3 +1,17 @@
+# Copyright (C) 2019 Simon Biggs
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 import os
 import pathlib
 import platform
@@ -38,7 +52,7 @@ def call_embedded_python(embedded_python_dir, *args):
     subprocess.check_call(to_be_called, cwd=embedded_python_dir)
 
 
-def main():
+def main(_):
     cwd = pathlib.Path(os.getcwd())
     build = cwd.joinpath("build")
 
