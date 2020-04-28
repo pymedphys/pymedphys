73f6638d7a184548640f95d464bb6fc409d1089b
SimonBiggs <me@simonbiggs.net>
2020-04-29 08:57:58 +1000

    further test refactor

--- tests/e2e/cypress/support/commands.js
+++ tests/e2e/cypress/support/commands.js
@@ -23,3 +23,20 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('compute', () => {
+  cy.get("#ReportStatus").should("be.visible")
+  cy.get("#ReportStatus").should("not.be.visible")
+})
+
+Cypress.Commands.add('textMatch', (label, length, result) => {
+  cy.get(`.stMarkdown p:contains(${label})`).should("have.length", length).find('code').each((el) => {
+    cy.wrap(el).should("have.text", result)
+  })
+})
+
+Cypress.Commands.add('start', () => {
+  cy.visit("http://localhost:8501/");
+  cy.compute()
+  cy.get(".decoration").invoke("css", "display", "none");
+})
