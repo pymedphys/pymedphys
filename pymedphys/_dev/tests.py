f3af0cc5970c990cc9750c5c27e310e292437f3a
Simon Biggs <me@simonbiggs.net>
2020-06-01 19:19:06 +1000

    move conftest, make doctest only run doctests
    Former-commit-id: 64ceb6bcae104f8806d9cf96f3ee445f1934ec2e

--- pymedphys/_dev/tests.py
+++ pymedphys/_dev/tests.py
@@ -1,8 +1,14 @@
+import pathlib
 import subprocess
+
+LIBRARY_ROOT = pathlib.Path(__file__).parent.parent.resolve()
 
 
 def run_tests(_, remaining):
+    print(remaining)
+
     command = " ".join(["pytest"] + remaining + ["--pyargs", "pymedphys"])
+
     print(command)
 
     subprocess.check_call(command, shell=True)
