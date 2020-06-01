eb105dddcf632de937cde5d77a904c82e9d8bf2c
Simon Biggs <me@simonbiggs.net>
2020-06-01 20:08:22 +1000

    update lossless jpeg wrapper

--- pymedphys/_dev/tests.py
+++ pymedphys/_dev/tests.py
@@ -5,6 +5,8 @@ LIBRARY_ROOT = pathlib.Path(__file__).parent.parent.resolve()
 
 
 def run_tests(_, remaining):
-    command = " ".join(["pytest"] + remaining + ["--pyargs", "pymedphys"])
+    command = " ".join(
+        ["pytest"] + remaining + ["--pyargs", "pymedphys", "--failed-first"]
+    )
 
     subprocess.check_call(command, shell=True)
