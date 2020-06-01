cff284f93473ca801ca275a4f5cfa1b3fd2398ad
Simon Biggs <me@simonbiggs.net>
2020-06-01 19:20:17 +1000

    remove excess prints
    Former-commit-id: 9364378e771d73e4cc7b91cd811a918272f18aed

--- pymedphys/_dev/tests.py
+++ pymedphys/_dev/tests.py
@@ -5,10 +5,6 @@ LIBRARY_ROOT = pathlib.Path(__file__).parent.parent.resolve()
 
 
 def run_tests(_, remaining):
-    print(remaining)
-
     command = " ".join(["pytest"] + remaining + ["--pyargs", "pymedphys"])
-
-    print(command)
 
     subprocess.check_call(command, shell=True)
