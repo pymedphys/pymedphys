887ab6cbc4d4cdd27baafb1de087225ceffcbd92
ayalalazaro <rafael.ayala@salud.madrid.org>
2020-06-10 13:30:51 +0200

    Fixed license, pymedphys imports, unspecific exception

--- pymedphys/labs/quickcheck/quickcheck_cli.py
+++ pymedphys/labs/quickcheck/quickcheck_cli.py
@@ -1,3 +1,17 @@
+# Copyright (C) 2020 Rafael Ayala
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import logging
 import sys
 
@@ -11,13 +25,9 @@ def export_cli(args):
 
     ip = args.ip
     csv_path = args.csv_path
-    verbose = args.verbose
-
     # Create a logger to std out for cli
     log_level = logging.INFO
     logger = logging.getLogger(__name__)
-    if verbose:
-        log_level = logging.DEBUG
     logger.setLevel(log_level)
 
     ch = logging.StreamHandler(sys.stdout)
@@ -30,6 +40,6 @@ def export_cli(args):
     qc.connect()
     if qc.connected:
         qc.get_measurements()
-        print("Saving data to " + csv_path)
+        print(" Saving data to " + csv_path)
         qc.close()
         qc.measurements.to_csv(csv_path)
