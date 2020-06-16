eb8e218ceb4ab09c044c0e1b9dcd9d7bef57051a
Simon Biggs <me@simonbiggs.net>
2020-06-16 15:01:13 +1000

    create a top level api that produces three datasets

--- pymedphys/labs/autosegmentation/pipeline.py
+++ pymedphys/labs/autosegmentation/pipeline.py
@@ -16,12 +16,114 @@
 import json
 import logging
 import pathlib
+import random
 
 from pymedphys._imports import numpy as np
 from pymedphys._imports import pydicom
 from pymedphys._imports import tensorflow as tf
 
-from . import mask
+from . import filtering, indexing, mask
+
+
+def create_datasets(data_path_root, names_map, structures_to_learn, filters):
+
+    data_path_root = pathlib.Path(data_path_root)
+    dicom_directory = data_path_root.joinpath("dicom")
+
+    structure_value_error = ValueError(
+        "Expected DICOM files to be organised into directories "
+        "testing, training and validation under data_path_root/dicom."
+    )
+
+    if not dicom_directory.exists():
+        raise structure_value_error
+
+    (
+        ct_image_paths,
+        structure_set_paths,
+        ct_uid_to_structure_uid,
+        structure_uid_to_ct_uids,
+    ) = indexing.get_uid_cache(data_path_root)
+
+    uid_to_dataset_type = {}
+    for structure_uid, ct_uids in structure_uid_to_ct_uids.items():
+        structure_set_path = pathlib.Path(structure_set_paths[structure_uid])
+        structure_set_type = structure_set_path.relative_to(dicom_directory).parts[0]
+
+        uid_to_dataset_type[structure_uid] = structure_set_type
+
+        if not structure_set_type in ["testing", "training", "validation"]:
+            raise structure_value_error
+
+        for ct_uid in ct_uids:
+            ct_path = pathlib.Path(ct_image_paths[ct_uid])
+            ct_type = structure_set_path.relative_to(dicom_directory).parts[0]
+
+            if ct_type != structure_set_type:
+                raise ValueError(
+                    f"{str(ct_path)} uses the structure set at "
+                    f"{str(structure_set_path)}, yet their top level directory "
+                    "within data_path_root/dicom does not have the same "
+                    "value of 'testing', 'training', or 'validation'."
+                )
+
+            uid_to_dataset_type[ct_uid] = ct_type
+
+    verification = filtering.verify_all_names_have_mapping(
+        data_path_root, structure_set_paths, names_map
+    )
+    false_mapping = verification["Names mapped that don't exist in DICOM files"]
+    to_be_mapped = verification[
+        "Names within DICOM files that have not been mapped yet"
+    ]
+
+    if false_mapping != set([]):
+        raise ValueError(
+            "Name mapping has structures within it that don't exist within "
+            f"your dataset. {false_mapping}"
+        )
+
+    if to_be_mapped != set([]):
+        raise ValueError(
+            "Name mapping is not complete. The following structures have not yet "
+            f"been mapped: {to_be_mapped}"
+        )
+
+    (
+        structure_names_by_ct_uid,
+        structure_names_by_structure_set_uid,
+    ) = indexing.get_cached_structure_names_by_uids(
+        data_path_root, structure_set_paths, names_map
+    )
+
+    filtered_ct_uids = filtering.filter_ct_uids(
+        structure_uid_to_ct_uids,
+        structure_names_by_structure_set_uid,
+        structure_names_by_ct_uid,
+        **filters,
+    )
+
+    filtered_uids_by_type = {"training": [], "validation": [], "testing": []}
+    for uid in filtered_ct_uids:
+        uid_dataset_type = uid_to_dataset_type[uid]
+        filtered_uids_by_type[uid_dataset_type].append(uid)
+
+    datasets = {}
+    for dataset_type in ["training", "validation", "testing"]:
+        uids = filtered_uids_by_type[dataset_type]
+        random.shuffle(uids)
+
+        datasets[dataset_type] = create_numpy_generator_dataset(
+            data_path_root,
+            structure_set_paths,
+            ct_image_paths,
+            ct_uid_to_structure_uid,
+            names_map,
+            uids,
+            structures_to_learn,
+        )
+
+    return datasets
 
 
 def create_numpy_generator_dataset(
