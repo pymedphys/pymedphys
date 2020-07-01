4c82b932d7f955361817a376ccd2d4a2ab39065c
SimonBiggs <me@simonbiggs.net>
2020-07-01 22:29:06 +1000

    vendor deprecated
    Former-commit-id: 20c80c1679030031d6b2f259b766065a99c62be9

--- pymedphys/beta/trf.py
+++ pymedphys/beta/trf.py
@@ -1,11 +1,10 @@
 # pylint: disable = unused-import, missing-docstring
 
-from deprecated import deprecated as _deprecated
 from pymedphys._trf.decode import read_trf as _read
 from pymedphys._trf.manage.identify import identify_logfile as identify
+from pymedphys._vendor.deprecated import deprecated as _deprecated
 
-_read.__name__ = "`pymedphys.beta.trf.read`"
+_read.__name__ = "pymedphys.beta.trf.read"
 read = _deprecated(
-    version="0.31.0",
-    reason="This API has left beta. It is found at `pymedphys.trf.read`.",
+    reason="This API has left beta. It is found at `pymedphys.trf.read`."
 )(_read)
