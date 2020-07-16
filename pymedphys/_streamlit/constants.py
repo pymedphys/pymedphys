9fdc858aed8ff619d543f3fac6808e8bccdc207e
SimonBiggs <me@simonbiggs.net>
2020-07-16 15:06:12 +1000

    update constants, hardcoded for now

--- pymedphys/_streamlit/constants.py
+++ pymedphys/_streamlit/constants.py
@@ -1,8 +1,7 @@
 # TODO: Make this configurable
+HOSTNAME = "physics-server"
+BASE_URL_PATHS = ["/delivery/", "/monaco-anonymisation/"]
 
-BASENAME = "localhost"
-
-# URLS = ["http://physics-server/delivery", "http://physics-server/anonymise-monaco"]
 NAMES = ["MU Density Delivery Comparison", "Monaco Anonymisation"]
 SCRIPTS = ["mudensity-compare.py", "anonymise-monaco.py"]
 PORTS = [28888, 28889]
