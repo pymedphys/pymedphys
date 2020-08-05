f21835b52e222a85aa5f2a94175f4850beab6bef
Stuart J Swerdloff <sjswerdloff@gmail.com>
2020-08-05 17:47:27 +1200

    add pseudonymisation strategy and invoke it through the experimental cli

--- pymedphys/_experimental/pseudonymisation/__init__.py
+++ pymedphys/_experimental/pseudonymisation/__init__.py
@@ -1,8 +1,57 @@
-from pymedphys._dicom.anonymise import anonymise_cli
+import logging
+from os.path import abspath, basename, dirname, isdir, isfile
+
+from pymedphys._dicom.anonymise import anonymise_directory, anonymise_file
+
+from . import strategy
 
 
 def anonymise_with_pseudo_cli(args):
-    if args.pseudo:
-        print("Was run with pseudo!")
 
-    anonymise_cli(args)
+    if args.delete_unknown_tags:
+        handle_unknown_tags = True
+    elif args.ignore_unknown_tags:
+        handle_unknown_tags = False
+    else:
+        handle_unknown_tags = None
+
+    if not args.keywords_to_leave_unchanged:
+        keywords_to_leave_unchanged = ()
+    else:
+        keywords_to_leave_unchanged = args.keywords_to_leave_unchanged
+
+    replacement_strategy = None
+    if args.pseudo:
+        logging.info("Was run with pseudo!")
+        replacement_strategy = strategy.pseudonymisation_dispatch
+
+    if isfile(args.input_path):
+        anonymise_file(
+            dicom_filepath=args.input_path,
+            output_filepath=args.output_path,
+            delete_original_file=args.delete_original_files,
+            anonymise_filename=not args.preserve_filenames,
+            replace_values=not args.clear_values,
+            keywords_to_leave_unchanged=keywords_to_leave_unchanged,
+            delete_private_tags=not args.keep_private_tags,
+            delete_unknown_tags=handle_unknown_tags,
+            replacement_strategy=replacement_strategy,
+        )
+
+    elif isdir(args.input_path):
+        anonymise_directory(
+            dicom_dirpath=args.input_path,
+            output_dirpath=args.output_path,
+            delete_original_files=args.delete_original_files,
+            anonymise_filenames=not args.preserve_filenames,
+            replace_values=not args.clear_values,
+            keywords_to_leave_unchanged=keywords_to_leave_unchanged,
+            delete_private_tags=not args.keep_private_tags,
+            delete_unknown_tags=handle_unknown_tags,
+            replacement_strategy=replacement_strategy,
+        )
+
+    else:
+        raise FileNotFoundError(
+            "No file or directory was found at the supplied input path."
+        )
