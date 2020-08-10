39bfacb1ef3d19e3f17d0a83d3cef70cfedd419f
CCA-Physics <sbiggs@riverinacancercare.com.au>
2020-08-10 11:17:39 +0000

    working on windows for GPU and CPU inference
    Former-commit-id: 0df4108a85e90990a60db9f361999265e3a5fce0

--- protyping/auto-segmentation/config.py
+++ protyping/auto-segmentation/config.py
@@ -1,11 +1,11 @@
 # ------------------------------------------------------------
 PORT = 11112
 HOST = "locahost"
-# EXPORT_PATH =
+EXPORT_PATH = "C:/Users/Public/Documents/vacunet/scp_output"
 
 # ------------------------------------------------------------
-# DEVICE = "GPU"
-DEVICE = "CPU"
+DEVICE = "GPU"
+# DEVICE = "CPU"
 
 # ------------------------------------------------------------
 TRAINING_DATA_MEAN = 168.3172158554484
@@ -14,4 +14,4 @@ TRAINING_DATA_STD = 340.21625683608994
 # ------------------------------------------------------------
 MODEL_OUTPUT_CHANNELS = 1
 BATCH_SIZE = 1
-MODEL_WEIGHTS = "/media/matthew/secondary/unet_vacbag_512_dsc_epoch_120.hdf5"
+MODEL_WEIGHTS = "C:/Users/Public/Documents/vacunet/unet_vacbag_512_dsc_epoch_120.hdf5"
