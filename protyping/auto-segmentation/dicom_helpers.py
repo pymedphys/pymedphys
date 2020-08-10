70f3030d67975f5a16dbc135e6cd7b137253e74a
matthewdeancooper <matthewdeancooper@gmail.com>
2020-08-10 20:31:48 +1000

    refactoring and added add_transfer_syntax_to_path function

--- protyping/auto-segmentation/dicom_helpers.py
+++ protyping/auto-segmentation/dicom_helpers.py
@@ -2,10 +2,11 @@ import glob
 
 import numpy as np
 import pydicom
+import os_helpers
 
 
-def read_dicom_paths(dicom_paths):
-    return [pydicom.dcmread(dicom_path, force=False) for dicom_path in dicom_paths]
+def read_dicom_paths(dicom_paths, force=False):
+    return [pydicom.dcmread(dicom_path, force=force) for dicom_path in dicom_paths]
 
 
 def sort_slice_location(dicom_series):
@@ -45,10 +46,22 @@ def add_transfer_syntax(dicom_files):
     return dicom_files
 
 
+def add_transfer_syntax_to_path(data_path):
+    save_parent = data_path + "/with_transfer_syntax"
+    os_helpers.make_path(save_parent)
+
+    dicom_paths = glob.glob(data_path + "/*.dcm")
+    dicom_files = read_dicom_paths(dicom_paths, force=True)
+
+    for dicom in dicom_files:
+        save_path = save_parent + "/" + dicom.SOPInstanceUID + ".dcm"
+        print(save_path)
+        dicom.save_as(save_path, write_like_original=False)
+
+
 def print_dicom_file(dicom_file):
     print("\nFile meta")
     print(dicom_file.file_meta)
-
     print("\nFile main")
     for data in dicom_file:
         print(data)
