110b3198096bf35918337fe2c6e42f35c953815e
SimonBiggs <me@simonbiggs.net>
2020-08-18 06:28:55 +1000

    make imports for apipkg be parsed from .imports
    Former-commit-id: 2a21b6d7bdbfd3495a50798f01c74e2fa2b1c41e

---
+++ pymedphys/_imports/imports.py
@@ -0,0 +1,57 @@
+# pylint: disable = unused-import, reimported, import-error
+
+import attr
+import dateutil
+import dbfread
+import keyring
+import libjpeg
+import packaging
+import pymssql
+import pynetdicom
+import requests
+import shapely
+import shapely.affinity
+import shapely.geometry
+import shapely.ops
+import streamlit
+import timeago
+import tkinter
+import tkinter.filedialog
+import toml
+import tqdm
+import watchdog
+import watchdog.events
+import watchdog.observers
+import watchdog.observers.polling
+import yaml
+
+import numpy
+import pandas
+import scipy
+import scipy.interpolate
+import scipy.ndimage
+import scipy.ndimage.measurements
+import scipy.optimize
+import scipy.signal
+import scipy.special
+
+import matplotlib
+import matplotlib.patches
+import matplotlib.path
+import matplotlib.pyplot
+import matplotlib.pyplot as plt
+import matplotlib.transforms
+import mpl_toolkits
+import mpl_toolkits.mplot3d.art3d
+
+import imageio
+import PIL
+import skimage
+import skimage.draw
+import skimage.measure
+
+import pydicom
+import pydicom.dataset
+import pydicom.filebase
+import pydicom.sequence
+import pydicom.uid
