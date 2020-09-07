a3490bec426b7dfa4efcac3f98d632709403ca86
SimonBiggs <me@simonbiggs.net>
2020-09-07 21:34:45 +1000

    remove film and Profile for now

--- pymedphys/experimental/__init__.py
+++ pymedphys/experimental/__init__.py
@@ -1,6 +1,5 @@
 # pylint: disable = unused-import, missing-docstring
 
 from pymedphys._experimental.cube import align_cube_to_structure, cubify
-from pymedphys._experimental.paulking.profile import Profile
 
-from . import collimation, fileformats, film, pinnacle, pseudonymisation, sinogram
+from . import collimation, fileformats, pinnacle, pseudonymisation, sinogram
