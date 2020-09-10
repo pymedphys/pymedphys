fdc7d1e28974c77c156d33ca3dab03d6ed5c2cf7
Stuart J Swerdloff <sjswerdloff@gmail.com>
2020-09-10 18:37:08 +1200

    fix broken test that was prefixed with an underscore and had mangled construction of RequestAttributesSequence Add a function for confirming whether a strategy supports the VR for each of the keywords include testing that the pseudonymisation does not support VR of UR regardless of what might be in the data

--- pymedphys/experimental/pseudonymisation.py
+++ pymedphys/experimental/pseudonymisation.py
@@ -3,5 +3,6 @@
 
 from pymedphys._experimental.pseudonymisation import (
     get_default_pseudonymisation_keywords,
+    is_valid_strategy_for_keywords,
 )
 from pymedphys._experimental.pseudonymisation.strategy import pseudonymisation_dispatch
