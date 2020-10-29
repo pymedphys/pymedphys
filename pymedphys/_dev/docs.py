f4c7bdb1ed9fbbf9c848b62814fd05c4bfe8797b
SimonBiggs <me@simonbiggs.net>
2020-10-29 20:41:21 +1100

    add --live flag to documentation CLI
    Former-commit-id: 253b8560e47b0ccf22453f93ea559f8454cd6b9d

--- pymedphys/_dev/docs.py
+++ pymedphys/_dev/docs.py
@@ -1,5 +1,6 @@
 import pathlib
 import subprocess
+import webbrowser
 
 ROOT = pathlib.Path(__file__).resolve().parent.parent.parent
 DOCS_DIR = ROOT.joinpath("docs")
@@ -7,17 +8,34 @@ DOCS_BUILD_DIR = DOCS_DIR.joinpath("_build")
 DOCS_HTML_BUILD_DIR = DOCS_BUILD_DIR.joinpath("html")
 
 
-def build_docs(_):
-    subprocess.check_call(
-        " ".join(
-            [
-                "poetry",
-                "run",
-                "sphinx-build",
-                "-W",
-                str(DOCS_DIR),
-                str(DOCS_HTML_BUILD_DIR),
-            ]
-        ),
-        shell=True,
-    )
+def build_docs(args):
+    webbrowser.open("http://127.0.0.1:8000")
+
+    if args.live:
+        subprocess.check_call(
+            " ".join(
+                [
+                    "poetry",
+                    "run",
+                    "sphinx-autobuild",
+                    str(DOCS_DIR),
+                    str(DOCS_HTML_BUILD_DIR),
+                ]
+            ),
+            shell=True,
+        )
+
+    else:
+        subprocess.check_call(
+            " ".join(
+                [
+                    "poetry",
+                    "run",
+                    "sphinx-build",
+                    "-W",
+                    str(DOCS_DIR),
+                    str(DOCS_HTML_BUILD_DIR),
+                ]
+            ),
+            shell=True,
+        )
