253b8560e47b0ccf22453f93ea559f8454cd6b9d
SimonBiggs <me@simonbiggs.net>
2020-10-29 20:41:21 +1100

    add --live flag to documentation CLI

--- pymedphys/cli/dev.py
+++ pymedphys/cli/dev.py
@@ -13,6 +13,8 @@ def dev_cli(subparsers):
 def add_docs_parser(dev_subparsers):
     parser = dev_subparsers.add_parser("docs")
 
+    parser.add_argument("--live", help="Make the docs live reload", action="store_true")
+
     parser.set_defaults(func=docs.build_docs)
 
 
