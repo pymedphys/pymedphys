97ba5c7545d2bdfc1aeab86c851e250e3343ec70
SimonBiggs <me@simonbiggs.net>
2020-10-31 16:30:30 +1100

    fix pylint tests

--- lib/pymedphys/_dev/tests.py
+++ lib/pymedphys/_dev/tests.py
@@ -7,7 +7,13 @@ LIBRARY_ROOT = pathlib.Path(__file__).parent.parent.resolve()
 
 def run_tests(_, remaining):
     original_cwd = os.getcwd()
-    os.chdir(LIBRARY_ROOT)
+
+    if "--pylint" in remaining and LIBRARY_ROOT.parent.name == "lib":
+        working_directory_to_use = LIBRARY_ROOT.parent.parent
+    else:
+        working_directory_to_use = LIBRARY_ROOT
+
+    os.chdir(working_directory_to_use)
     print(f"Running tests with cwd set to {os.getcwd()}")
 
     try:
