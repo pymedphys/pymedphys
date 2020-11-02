3eba2b6c5f09112a0764663e4573a54f2e9b6b4a
SimonBiggs <me@simonbiggs.net>
2020-11-02 17:21:05 +1100

    exctract out _utilities
    Former-commit-id: 819e65d10eaf8132db28d8852a20c62c39d905e9

--- pymedphys/_gui/streamlit/mudensity/main.py
+++ pymedphys/_gui/streamlit/mudensity/main.py
@@ -161,19 +161,6 @@ def read_monaco_patient_name(monaco_patient_directory):
     return mnc_patient.read_patient_name(monaco_patient_directory)
 
 
-def filter_patient_names(patient_names):
-    patient_names = list(set(patient_names))
-
-    if len(patient_names) == 1:
-        patient_name = patient_names[0]
-    elif len(patient_names) == 0:
-        patient_name = ""
-    else:
-        patient_name = f"Multiple Names Found: f{', '.join(patient_names)}"
-
-    return patient_name
-
-
 def monaco_input_method(
     patient_id="", key_namespace="", advanced_mode_local=False, site=None, **_
 ):
