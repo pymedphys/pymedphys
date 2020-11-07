6970ad37c9baeadd6b6a088e1ee4a4e514ecd3e0
SimonBiggs <me@simonbiggs.net>
2020-11-07 11:40:56 +1100

    further troubleshooting of pylint
    Former-commit-id: d7720820cb47c0b7ab3350bb4704e2b257049240

--- lib/pymedphys/_dev/tests.py
+++ lib/pymedphys/_dev/tests.py
@@ -3,16 +3,15 @@ import pathlib
 import subprocess
 
 LIBRARY_ROOT = pathlib.Path(__file__).parent.parent.resolve()
+PYLINT_RC_FILE = LIBRARY_ROOT.joinpath(".pylintrc")
 
 
 def run_tests(_, remaining):
     original_cwd = os.getcwd()
 
     if "--pylint" in remaining:
-        if LIBRARY_ROOT.parent.name == "lib":
-            working_directory_to_use = LIBRARY_ROOT.parent.parent
-        else:
-            working_directory_to_use = LIBRARY_ROOT.parent
+        remaining.append(["--pylint-rcfile", str(PYLINT_RC_FILE)])
+        working_directory_to_use = LIBRARY_ROOT.parent
     else:
         working_directory_to_use = LIBRARY_ROOT
 
@@ -23,6 +22,8 @@ def run_tests(_, remaining):
         command = " ".join(
             ["pytest"] + remaining + ["--pyargs", "pymedphys", "--failed-first"]
         )
+
+        print(command)
 
         subprocess.check_call(command, shell=True)
     finally:
