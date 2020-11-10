b30dc58801fa3ffbc87b876830e4d6ef1c406e7f
SimonBiggs <me@simonbiggs.net>
2020-11-10 17:49:45 +1100

    make it clear which functions are private
    Former-commit-id: 2469cec47ed7070e134e26574dda5e39220e780d

--- lib/pymedphys/_streamlit/apps/wlutz/_dbf.py
+++ lib/pymedphys/_streamlit/apps/wlutz/_dbf.py
@@ -20,64 +20,6 @@ from pymedphys._imports import streamlit as st
 
 from pymedphys._streamlit.utilities import dbf
 
-DBF_DATABASE_LOADING_CONFIG = {
-    "frame": {
-        "filename": "FRAME.dbf",
-        "columns_to_keep": ["DBID", "PIMG_DBID", "DELTA_MS"],
-        "column_rename_map": {"DBID": "FRAME_DBID"},
-    },
-    "patimg": {
-        "filename": "PATIMG.dbf",
-        "columns_to_keep": [
-            "DBID",
-            "DICOM_UID",
-            "IMG_DATE",
-            "IMG_TIME",
-            "PORT_DBID",
-            "ORG_DTL",
-        ],
-        "column_rename_map": {
-            # These are renamed so that when a pd.DataFrame.merge()
-            # function is undergone the PIMG_DBID columns in both
-            # database have the same name
-            "DBID": "PIMG_DBID",
-            "ORG_DTL": "machine_id",
-        },
-    },
-    "port": {
-        "filename": "PORT.dbf",
-        "columns_to_keep": ["DBID", "TRT_DBID", "ID"],
-        # ID is renamed here so that when pd.DataFrame.merge() is
-        # undergone the ID columns between the databases don't clash
-        "column_rename_map": {"DBID": "PORT_DBID", "ID": "port"},
-    },
-    "trtmnt": {
-        "filename": "TRTMNT.dbf",
-        "columns_to_keep": ["DBID", "PAT_DBID", "ID"],
-        "column_rename_map": {"DBID": "TRT_DBID", "ID": "treatment"},
-    },
-    "patient": {
-        "filename": "PATIENT.dbf",
-        "columns_to_keep": ["DBID", "ID", "LAST_NAME", "FIRST_NAME"],
-        "column_rename_map": {"DBID": "PAT_DBID", "ID": "patient_id"},
-    },
-}
-
-
-def load_dbf(
-    database_directory: pathlib.Path, refresh_cache: bool, config_key: str
-) -> "pd.DataFrame":
-
-    current_config = DBF_DATABASE_LOADING_CONFIG[config_key]
-    filename = cast(str, current_config["filename"])
-    columns_to_keep = cast(List[str], current_config["columns_to_keep"])
-    column_rename_map = cast(Dict[str, str], current_config["column_rename_map"])
-
-    table = _load_dbf_base(
-        database_directory, refresh_cache, filename, columns_to_keep, column_rename_map
-    )
-    return table
-
 
 def load_and_merge_dbfs(
     database_directory: pathlib.Path, refresh_cache: bool
@@ -144,7 +86,7 @@ def load_and_merge_dbfs(
     DBF files by passing ``refresh_cache=True``
 
     """
-    patimg = load_dbf(database_directory, refresh_cache, "patimg")
+    patimg = _load_dbf(database_directory, refresh_cache, "patimg")
 
     dates = pd.to_datetime(patimg["IMG_DATE"], format="%Y%m%d").dt.date
     date_options = dates.sort_values(ascending=False).unique()
@@ -159,7 +101,7 @@ def load_and_merge_dbfs(
         ("trtmnt", "TRT_DBID"),
         ("patient", "PAT_DBID"),
     ]:
-        dbf_to_be_merged = load_dbf(database_directory, refresh_cache, database_key)
+        dbf_to_be_merged = _load_dbf(database_directory, refresh_cache, database_key)
         merged = merged.merge(dbf_to_be_merged, left_on=merge_key, right_on=merge_key)
 
     timestamps_string = (
@@ -190,12 +132,71 @@ def load_and_merge_dbfs(
     return table
 
 
-def dbf_to_pandas_without_cache(path: pathlib.Path) -> "pd.DataFrame":
+_DBF_DATABASE_LOADING_CONFIG = {
+    "frame": {
+        "filename": "FRAME.dbf",
+        "columns_to_keep": ["DBID", "PIMG_DBID", "DELTA_MS"],
+        "column_rename_map": {"DBID": "FRAME_DBID"},
+    },
+    "patimg": {
+        "filename": "PATIMG.dbf",
+        "columns_to_keep": [
+            "DBID",
+            "DICOM_UID",
+            "IMG_DATE",
+            "IMG_TIME",
+            "PORT_DBID",
+            "ORG_DTL",
+        ],
+        "column_rename_map": {
+            # These are renamed so that when a pd.DataFrame.merge()
+            # function is undergone the PIMG_DBID columns in both
+            # database have the same name
+            "DBID": "PIMG_DBID",
+            "ORG_DTL": "machine_id",
+        },
+    },
+    "port": {
+        "filename": "PORT.dbf",
+        "columns_to_keep": ["DBID", "TRT_DBID", "ID"],
+        # ID is renamed here so that when pd.DataFrame.merge() is
+        # undergone the ID columns between the databases don't clash
+        "column_rename_map": {"DBID": "PORT_DBID", "ID": "port"},
+    },
+    "trtmnt": {
+        "filename": "TRTMNT.dbf",
+        "columns_to_keep": ["DBID", "PAT_DBID", "ID"],
+        "column_rename_map": {"DBID": "TRT_DBID", "ID": "treatment"},
+    },
+    "patient": {
+        "filename": "PATIENT.dbf",
+        "columns_to_keep": ["DBID", "ID", "LAST_NAME", "FIRST_NAME"],
+        "column_rename_map": {"DBID": "PAT_DBID", "ID": "patient_id"},
+    },
+}
+
+
+def _load_dbf(
+    database_directory: pathlib.Path, refresh_cache: bool, config_key: str
+) -> "pd.DataFrame":
+
+    current_config = _DBF_DATABASE_LOADING_CONFIG[config_key]
+    filename = cast(str, current_config["filename"])
+    columns_to_keep = cast(List[str], current_config["columns_to_keep"])
+    column_rename_map = cast(Dict[str, str], current_config["column_rename_map"])
+
+    table = _load_dbf_base(
+        database_directory, refresh_cache, filename, columns_to_keep, column_rename_map
+    )
+    return table
+
+
+def _dbf_to_pandas_without_cache(path: pathlib.Path) -> "pd.DataFrame":
     return pd.DataFrame(iter(dbf.get_dbf_table(path)))
 
 
 @st.cache()
-def dbf_to_pandas_with_cache(path: pathlib.Path) -> "List[pd.DataFrame]":
+def _dbf_to_pandas_with_cache(path: pathlib.Path) -> "List[pd.DataFrame]":
     """Streamlit cached dbf read.
 
     Notes
@@ -203,14 +204,14 @@ def dbf_to_pandas_with_cache(path: pathlib.Path) -> "List[pd.DataFrame]":
     Returns a mutable list so that the cache can be updated inplace.
 
     """
-    return [dbf_to_pandas_without_cache(path)]
+    return [_dbf_to_pandas_without_cache(path)]
 
 
 # TODO: Consider replacing the `refresh_cache` logic with a watchdog
 # driven event model. See
 # <https://github.com/pymedphys/pymedphys/pull/1143#discussion_r520242368>
 # for details.
-def dbf_to_pandas(path: pathlib.Path, refresh_cache=True) -> "pd.DataFrame":
+def _dbf_to_pandas(path: pathlib.Path, refresh_cache=True) -> "pd.DataFrame":
     """Retrieves the pandas.DataFrame representation of a DBF database.
 
     Parameters
@@ -227,7 +228,7 @@ def dbf_to_pandas(path: pathlib.Path, refresh_cache=True) -> "pd.DataFrame":
     can be overridden by passing ``refresh_cache=True``
 
     """
-    result = dbf_to_pandas_with_cache(path)
+    result = _dbf_to_pandas_with_cache(path)
 
     if refresh_cache:
         result[0] = dbf_to_pandas_without_cache(path)
@@ -243,7 +244,7 @@ def _load_dbf_base(
     column_rename_map: Dict[str, str],
 ) -> "pd.DataFrame":
     dbf_path = database_directory.joinpath(filename)
-    table = dbf_to_pandas(dbf_path, refresh_cache)[columns_to_keep]
+    table = _dbf_to_pandas(dbf_path, refresh_cache)[columns_to_keep]
     table.rename(column_rename_map, axis="columns", inplace=True)
 
     return table
