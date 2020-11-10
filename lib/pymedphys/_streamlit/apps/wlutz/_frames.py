feaf88b867248ad465f16638578660fc9baab161
SimonBiggs <me@simonbiggs.net>
2020-11-10 18:36:19 +1100

    add module docstring
    Former-commit-id: c86f6afad2600f476841e597437df923a9d675e0

--- lib/pymedphys/_streamlit/apps/wlutz/_frames.py
+++ lib/pymedphys/_streamlit/apps/wlutz/_frames.py
@@ -1,3 +1,30 @@
+# Copyright (C) 2020 Cancer Care Associates
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Functions for loading up the iViewDB frame contents.
+
+The frame database format has changed accross the iView versions. The
+older iView version has a FRAME.DBF file with all of the required
+information. The new iView version spreads this information out over
+nested _Frame.xml files.
+
+To support these two database formats two functions are exposed from
+this module. ``dbf_frame_based_database`` and
+``xml_frame_based_database``.
+"""
+
+
 from pymedphys._imports import pandas as pd
 from pymedphys._imports import streamlit as st
 from pymedphys._imports import xmltodict
@@ -6,6 +33,9 @@ from pymedphys._streamlit.apps.wlutz import _dbf
 
 
 def dbf_frame_based_database(database_directory, refresh_cache, filtered_table):
+    """
+
+    """
     frame = _dbf.load_iview_dbf(database_directory, refresh_cache, "frame")
     with_frame = filtered_table.merge(frame, left_on="PIMG_DBID", right_on="PIMG_DBID")
 
