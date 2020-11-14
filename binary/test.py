84d6152f37ec635dbf5f79c06c157d331ba9745b
SimonBiggs <me@simonbiggs.net>
2020-11-14 22:34:09 +1100

    install wheel

--- binary/test.py
+++ binary/test.py
@@ -13,9 +13,56 @@
 # limitations under the License.
 
 import pathlib
+import subprocess
+import sys
+
+from contextlib import contextmanager
+
+import psutil
 
 
 HERE = pathlib.Path("__file__").parent.resolve()
 REPO_ROOT = HERE.parent
 BUILD = REPO_ROOT.joinpath("build")
 BUILD_DIST = BUILD.joinpath("dist")
+
+CYPRESS = REPO_ROOT.joinpath("lib", "pymedphys", "tests", "e2e")
+
+
+@contextmanager
+def process(*args, **kwargs):
+    """Provides a process running with the provided arguments, useful for CLI unit tests
+
+    Yields
+    -------
+    subprocess.Popen
+        The process being run
+    """
+    proc = subprocess.Popen(*args, **kwargs)
+    try:
+        yield proc
+    finally:
+        for child in psutil.Process(proc.pid).children(recursive=True):
+            child.kill()
+        proc.kill()
+
+
+def main():
+    if sys.platform == "win32":
+        prepend = ""
+    else:
+        prepend = "wine "
+
+    built_executables = list(BUILD_DIST.glob("*.exe"))
+    if len(built_executables) != 1:
+        raise ValueError("There should be only one executable.")
+
+    exe = built_executables[0]
+
+    with process(f"{prepend}{exe}", cwd=BUILD_DIST, shell=True):
+        subprocess.check_call("yarn", cwd=CYPRESS, shell=True)
+        subprocess.check_call("yarn cypress run", cwd=CYPRESS, shell=True)
+
+
+if __name__ == "__main__":
+    main()
