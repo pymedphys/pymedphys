38f2934d01040038ec685bcd4f595bf134d01f41
SimonBiggs <me@simonbiggs.net>
2020-11-14 23:38:50 +1100

    simplify pymedphys.bat
    Former-commit-id: dbf3d82a6ec5422e2214662f220672b1373fbaa2

--- binary/pymedphys.bat
+++ binary/pymedphys.bat
@@ -1,9 +1,6 @@
 @echo off
 pushd "%~dp0"
-
 FOR /F "tokens=*" %%g IN ('resolve-path.cmd') do (SET PYTHON_DIR=%%g)
-SET PATH="%PYTHON_DIR%;%PYTHON_DIR%\Scripts;%PATH%"
-
 popd
 
 "%PYTHON_DIR%\python" -m pymedphys %*
