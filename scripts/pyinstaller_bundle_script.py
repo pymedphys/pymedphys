a244d4c53a6cf8407872955313fb3de5b987248b
SimonBiggs <me@simonbiggs.net>
2020-11-14 21:38:46 +1100

    create bundle script
    Former-commit-id: d916489332ae11dd73be2323c5cecf3183a208bc

---
+++ scripts/pyinstaller_bundle_script.py
@@ -0,0 +1,39 @@
+import os
+import subprocess
+import sys
+import pathlib
+import tarfile
+import shutil
+
+
+def boot_streamlit_app(installation_path):
+    subprocess.check_call(
+        "python.exe -m pymedphys gui", cwd=installation_path, shell=True
+    )
+
+
+def main():
+    cwd = pathlib.Path(os.getcwd())
+    installation_path = cwd.joinpath("python-embed")
+
+    if installation_path.exists():
+        boot_streamlit_app(installation_path)
+    else:
+        pyinstaller_temp_dir = pathlib.Path(
+            sys._MEIPASS  # pylint: disable = no-member, protected-access
+        )
+        data_path = pyinstaller_temp_dir.joinpath("data")
+
+        for filename in ["resolve_path.cmd", "pymedphys.bat"]:
+            shutil.copy(data_path.joinpath(filename), cwd.joinpath(filename))
+
+        python_xztar = data_path.joinpath("python-embed.tar.xz")
+
+        with tarfile.open(python_xztar) as f:
+            f.extractall(cwd)
+
+        boot_streamlit_app(installation_path)
+
+
+if __name__ == "__main__":
+    main()
