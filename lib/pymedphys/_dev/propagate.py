53f6b739260cc9d264cab89937908dadd2b4bb1c
SimonBiggs <me@simonbiggs.net>
2020-11-15 08:52:55 +1100

    add deploy flag

--- lib/pymedphys/_dev/propagate.py
+++ lib/pymedphys/_dev/propagate.py
@@ -140,7 +140,7 @@ def propagate_requirements():
     )
 
     subprocess.check_call(
-        "poetry export -E user -E test -f requirements.txt --output requirements-deploy.txt",
+        "poetry export -E user -E test -E deploy -f requirements.txt --output requirements-deploy.txt",
         shell=True,
     )
 
