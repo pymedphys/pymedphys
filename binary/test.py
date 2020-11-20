a3effd952415e03186caee17e625988e64a3c51e
SimonBiggs <me@simonbiggs.net>
2020-11-21 08:43:49 +1100

    show tkinter issue

--- binary/test.py
+++ binary/test.py
@@ -59,9 +59,9 @@ def main():
     # TODO: For now just testing cypress above. Some work needs to be
     # done re the environment before all tests can be run as below.
 
-    # subprocess.check_call(
-    #     f"{prepend}cmd.exe /C pymedphys dev tests -v", cwd=BUILD_DIST, shell=True
-    # )
+    subprocess.check_call(
+        f"{prepend}cmd.exe /C pymedphys dev tests -v", cwd=BUILD_DIST, shell=True
+    )
 
 
 @contextmanager
