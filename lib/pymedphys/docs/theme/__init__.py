2a4f026ee303ea111adb0338e2348372927f6a04
SimonBiggs <me@simonbiggs.net>
2020-12-02 05:19:53 +1100

    attempt to include sphinx theme entrypoint

--- lib/pymedphys/docs/theme/__init__.py
+++ lib/pymedphys/docs/theme/__init__.py
@@ -1,8 +1,16 @@
+from typing import Any, Dict, Optional
+
 import sphinx_book_theme
+from docutils.nodes import document
 from sphinx.application import Sphinx
+from sphinx.util import logging
+
+SPHINX_LOGGER = logging.getLogger(__name__)
 
 
 def setup(app: Sphinx):
+    raise ValueError("Make noise")
+
     results = sphinx_book_theme.setup(app)
 
     app.connect("html-page-context", add_to_context)
@@ -11,9 +19,16 @@ def setup(app: Sphinx):
 
 
 def add_to_context(
-    app, pagename, templatename, context, doctree  # pylint: disable = unused-argument
+    app: Sphinx,  # pylint: disable = unused-argument
+    pagename: str,  # pylint: disable = unused-argument
+    templatename: str,  # pylint: disable = unused-argument
+    context: Dict[str, Any],
+    doctree: Optional[document],  # pylint: disable = unused-argument
 ):
+    SPHINX_LOGGER.error("a test")
+
     sbt_generate_toc_html = context["generate_toc_html"]
+    print(context)
 
     def generate_toc_html():
         initial_html = sbt_generate_toc_html()
