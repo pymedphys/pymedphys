5f1c315c3c2f7968d1d40cc73605c7ab78c45dca
SimonBiggs <me@simonbiggs.net>
2020-12-17 15:30:23 +1100

    use hypothesis for PyMedPhys field wlutz again, begin testing it for pylinac

---
+++ lib/pymedphys/tests/winstonlutz/test_vendored_pylinac.py
@@ -0,0 +1,59 @@
+# Copyright (C) 2020 Cancer Care Associates
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import datetime
+
+import pytest
+from hypothesis import Verbosity, given, settings
+from hypothesis.strategies import floats
+
+import numpy as np
+
+import pymedphys._mocks.profiles as mock_profiles
+import pymedphys._wlutz.pylinac as wrapped_pylinac
+
+
+@pytest.mark.slow
+@settings(
+    deadline=datetime.timedelta(milliseconds=4000),
+    max_examples=10,
+    verbosity=Verbosity.verbose,
+)
+@given(
+    floats(-20, 20),
+    floats(-20, 20),
+    floats(10, 20),
+    floats(10, 20),
+    floats(0.5, 3),
+    floats(-360, 360),
+)
+def test_field_finding(x_centre, y_centre, x_edge, y_edge, penumbra, actual_rotation):
+    edge_lengths = [x_edge, y_edge]
+    actual_centre = [x_centre, y_centre]
+
+    field = mock_profiles.create_rectangular_field_function(
+        actual_centre, edge_lengths, penumbra, actual_rotation
+    )
+
+    x = np.arange(-50, 50, 0.1)
+    y = np.arange(-50, 50, 0.1)
+    xx, yy = np.meshgrid(x, y)
+    zz = field(xx, yy)
+
+    results = wrapped_pylinac.run_wlutz(
+        field, edge_lengths, penumbra, [0, 0], actual_rotation, find_bb=False
+    )
+
+    for key, item in results.items():
+        assert np.allclose(actual_centre, item["field_centre"])
