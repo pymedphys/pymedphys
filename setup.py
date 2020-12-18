74f5fce6dfb1d788b0d4fdc3debb87923868fd31
SimonBiggs <me@simonbiggs.net>
2020-12-18 22:59:33 +1100

    streamlit 0.71.0 --> 0.73.0

--- setup.py
+++ setup.py
@@ -169,7 +169,7 @@ extras_require = {
         "scikit-image",
         "dbfread",
         "xmltodict",
-        "streamlit==0.71.0",
+        "streamlit==0.73.0",
         "timeago",
         "jupyter-book>=0.8.3",
         "sphinx-argparse",
@@ -240,7 +240,7 @@ extras_require = {
         "scikit-image",
         "dbfread",
         "xmltodict",
-        "streamlit==0.71.0",
+        "streamlit==0.73.0",
         "timeago",
     ],
 }
