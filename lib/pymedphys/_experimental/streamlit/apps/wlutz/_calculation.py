43941d05af5a468c1f07e9442579606a80534615
SimonBiggs <me@simonbiggs.net>
2020-12-21 12:13:36 +1100

    formatting
    Former-commit-id: 9e6ea6f014644cc06f17b2c095739e182dca0ef3

--- lib/pymedphys/_experimental/streamlit/apps/wlutz/_calculation.py
+++ lib/pymedphys/_experimental/streamlit/apps/wlutz/_calculation.py
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+
 import base64
 
 from pymedphys._imports import numpy as np
