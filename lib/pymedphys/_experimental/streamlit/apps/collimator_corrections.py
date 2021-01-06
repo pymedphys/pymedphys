0d02e0a7d6926296f98c0368a997074aac35c2c2
SimonBiggs <me@simonbiggs.net>
2021-01-06 19:50:36 +1100

    pylint skip coll

--- lib/pymedphys/_experimental/streamlit/apps/collimator_corrections.py
+++ lib/pymedphys/_experimental/streamlit/apps/collimator_corrections.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# pylint: skip-file
+
 from pymedphys._imports import numpy as np
 from pymedphys._imports import pandas as pd
 from pymedphys._imports import plt
