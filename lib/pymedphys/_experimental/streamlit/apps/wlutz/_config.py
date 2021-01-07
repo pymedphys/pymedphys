805db8e1eac292cb33ea39b728ee0b9e270360a0
SimonBiggs <me@simonbiggs.net>
2021-01-07 18:27:38 +1100

    remove cache config

--- lib/pymedphys/_experimental/streamlit/apps/wlutz/_config.py
+++ lib/pymedphys/_experimental/streamlit/apps/wlutz/_config.py
@@ -6,7 +6,6 @@ import pymedphys
 from pymedphys._streamlit.utilities import config as st_config
 
 
-@st.cache
 def download_demo_data():
     cwd = pathlib.Path.cwd()
     pymedphys.zip_data_paths("wlutz-demo-files.zip", extract_directory=cwd)
