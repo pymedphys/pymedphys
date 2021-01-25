07ef42084f598519931c910fb5bef664ba096702
Lane, Derek <dg1an3@2nd-messenger.com>
2021-01-25 17:18:12 -0600

    adding dummy test case to query for system dbs

---
+++ lib/pymedphys/tests/mosaiq/test_can_we_db.py
@@ -0,0 +1,14 @@
+import pymssql
+
+
+def test_can_we_db():
+    conn = pymssql.connect(".", user="sa", password="sqlServerPassw0rd")
+    cursor = conn.cursor()
+    cursor.execute("select * from sys.databases")
+
+    # should print the four system databases
+    databases = list(cursor)
+    print(databases)
+
+    # and check that the correct number are present
+    assert len(databases) == 4
