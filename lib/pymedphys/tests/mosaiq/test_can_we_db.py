e4f04f44cd76302dec994a07b3168015f75b9487
Lane, Derek <dg1an3@2nd-messenger.com>
2021-01-26 15:35:12 -0600

    adding mosaiqdb pytest marker to run db tests from separate workflow

--- lib/pymedphys/tests/mosaiq/test_can_we_db.py
+++ lib/pymedphys/tests/mosaiq/test_can_we_db.py
@@ -1,9 +1,10 @@
 import pymssql
+import pytest
 
 
-@pytest.mark.msq_db
+@pytest.mark.mosaiqdb
 def test_can_we_db():
-    conn = pymssql.connect(".", user="sa", password="passWord123")
+    conn = pymssql.connect(".", user="sa", password="sqlServerPassw0rd")
     cursor = conn.cursor()
     cursor.execute("select * from sys.databases")
 
@@ -12,4 +13,4 @@ def test_can_we_db():
     print(databases)
 
     # and check that the correct number are present
-    assert len(databases) == 4
+    assert len(databases) >= 4
