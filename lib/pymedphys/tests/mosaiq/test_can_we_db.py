0a06e53da6e2d8a101125b1fb8385f6890b94885
Lane, Derek <dg1an3@2nd-messenger.com>
2021-01-26 13:37:35 -0600

    setting up dummy test to query the db container

--- lib/pymedphys/tests/mosaiq/test_can_we_db.py
+++ lib/pymedphys/tests/mosaiq/test_can_we_db.py
@@ -1,8 +1,9 @@
 import pymssql
 
 
+@pytest.mark.msq_db
 def test_can_we_db():
-    conn = pymssql.connect(".", user="sa", password="sqlServerPassw0rd")
+    conn = pymssql.connect(".", user="sa", password="passWord123")
     cursor = conn.cursor()
     cursor.execute("select * from sys.databases")
 
