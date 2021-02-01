6c88c8fdf1600687091e866aaaebd63196e975f5
Lane, Derek <dg1an3@2nd-messenger.com>
2021-01-31 21:37:39 -0600

    populating a basic tx_field and site table to test get_patient_fields

--- lib/pymedphys/tests/mosaiq/test_mock_patient_table.py
+++ lib/pymedphys/tests/mosaiq/test_mock_patient_table.py
@@ -1,18 +1,24 @@
+from pymedphys._imports import numpy as np
 from pymedphys._imports import pandas as pd
 from pymedphys._imports import pymssql, pytest, sqlalchemy
 
-from pymedphys._mosaiq.helpers import get_patient_name
+from pymedphys._mosaiq.helpers import get_patient_fields, get_patient_name
 from pymedphys.mosaiq import connect, execute
 
+msq_server = "."
+test_db_name = "MosaiqTest77008"
 
-@pytest.mark.mosaiqdb
-def test_mock_patient_table():
-    msq_server = "."
-    test_db_name = "MosaiqTest77008"
+sa_user = "sa"
+sa_password = "sqlServerPassw0rd"
 
-    sa_user = "sa"
-    sa_password = "sqlServerPassw0rd"
+# set up a SQLAlchemy engine to be used for populating tables
+connection_str = f"mssql+pymssql://{sa_user}:{sa_password}@{msq_server}/{test_db_name}"
+engine = sqlalchemy.create_engine(connection_str, echo=False)
 
+
+@pytest.fixture
+def check_create_test_db():
+    """ will create the test database, if it does not already exist on the instance """
     # sa connection to create the test database
     with pymssql.connect(
         msq_server, user=sa_user, password=sa_password
@@ -31,28 +37,38 @@ def test_mock_patient_table():
                 """
             )
 
+
+@pytest.fixture
+def create_mock_patients(check_create_test_db):
+    """ creates a mock patient, with small Patient and Ident tables with relevent attributes"""
+
     # create dataframes for the Patient and Ident tables
-    patient_df = pd.DataFrame(
-        [("Larry", "Fine"), ("Moe", "Howard"), ("Curly", "Howard")],
-        columns=["First_Name", "Last_Name"],
+    patient_ident_df = pd.DataFrame(
+        [
+            ("Larry", "Fine", "MR8001"),
+            ("Moe", "Howard", "MR8002"),
+            ("Curly", "Howard", "MR8003"),
+        ],
+        columns=["First_Name", "Last_Name", "IDA"],
     )
-    patient_df.index = patient_df.index + 10001  # use the index+10001 as the Pat_ID1
+    patient_ident_df.index = (
+        patient_ident_df.index + 10001
+    )  # use the index+10001 as the Pat_ID1
 
-    ident_df = pd.DataFrame([("MR8001"), ("MR8002"), ("MR8003")], columns=["IDA"])
-    ident_df.index = (
-        ident_df.index + 10001
-    )  # use index+10001 in the same order as Pat_ID1
-
-    # now use SQLAlchemy to populate the two tables
-    conn_str = f"mssql+pymssql://{sa_user}:{sa_password}@{msq_server}/{test_db_name}"
-    engine = sqlalchemy.create_engine(conn_str, echo=False)
-
-    patient_df.to_sql(
+    # now use SQLAlchemy to populate the two tables from the single composite
+    patient_ident_df.drop(columns=["IDA"]).to_sql(
         "Patient", engine, if_exists="replace", index=True, index_label="Pat_Id1"
     )
-    ident_df.to_sql(
+    patient_ident_df.drop(columns=["First_Name", "Last_Name"]).to_sql(
         "Ident", engine, if_exists="replace", index=True, index_label="Pat_Id1"
     )
+
+    return patient_ident_df
+
+
+@pytest.mark.mosaiqdb
+def test_get_patient_name(create_mock_patients):
+    """ tests the get_patient_name helper function"""
 
     with connect(
         msq_server,
@@ -84,3 +100,71 @@ def test_mock_patient_table():
         moe_patient_name = get_patient_name(connection, "MR8002")
 
         assert moe_patient_name == "HOWARD, Moe"
+
+
+@pytest.mark.mosaiqdb
+def test_get_patient_fields(create_mock_patients):
+    """ creates basic tx field and site metadata for the mock patients """
+
+    # the create_mock_patients output is the patient_ident dataframe
+    patient_ident_df = create_mock_patients
+
+    # set up site to have same rows as patient_ident
+    site_df = patient_ident_df.drop(columns=["First_Name", "Last_Name", "IDA"])
+    site_df["Site_Name"] = "rx1"
+    site_df["Pat_ID1"] = site_df.index
+    site_df.index = np.arange(1, len(site_df) + 1)
+
+    # populate a list of tx_fields, 3 for each site
+    tx_fields = []
+    for sit_set_id, site in site_df.iterrows():
+        tx_fields += [
+            ("A", "FieldA", 1, "MU", 1, site["Pat_ID1"], sit_set_id),
+            ("B", "FieldB", 1, "MU", 1, site["Pat_ID1"], sit_set_id),
+            ("C", "FieldC", 1, "MU", 1, site["Pat_ID1"], sit_set_id),
+        ]
+
+    # now create the tx_field dataframe
+    txfield_df = pd.DataFrame(
+        tx_fields,
+        columns=[
+            "Field_Label",
+            "Field_Name",
+            "Version",
+            "Meterset",
+            "Type_Enum",
+            "Pat_ID1",
+            "SIT_SET_ID",
+        ],
+    )
+    txfield_df.index += 1
+
+    # now use SQLAlchemy to populate the two tables
+    site_df.to_sql(
+        "Site", engine, if_exists="replace", index=True, index_label="SIT_SET_ID"
+    )
+    txfield_df.to_sql(
+        "TxField", engine, if_exists="replace", index=True, index_label="FLD_ID"
+    )
+
+    with connect(
+        msq_server,
+        port=1433,
+        database=test_db_name,
+        username=sa_user,
+        password=sa_password,
+    ) as connection:
+
+        # test the get_patient_fields helper function
+        txfield_df = get_patient_fields(connection, "MR8002")
+        print(txfield_df)
+
+        # make sure the correct number of rows were returned
+        assert len(txfield_df) == 3
+
+        # for each treatment field
+        for fld_id, txfield in txfield_df.iterrows():
+            print(fld_id, txfield)
+
+            # check that the field label matches the field name
+            assert f"Field{txfield['field_label']}" == txfield["field_name"]
