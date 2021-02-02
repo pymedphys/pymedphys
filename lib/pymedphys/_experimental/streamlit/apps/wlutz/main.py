f436236e37568f686ac88acf624776875126ffcc
SimonBiggs <me@simonbiggs.net>
2021-02-02 17:04:25 +1100

    initial work creating a DICOM file from the image frames

--- lib/pymedphys/_experimental/streamlit/apps/wlutz/main.py
+++ lib/pymedphys/_experimental/streamlit/apps/wlutz/main.py
@@ -23,7 +23,11 @@ from pymedphys._imports import scipy
 from pymedphys._imports import streamlit as st
 from pymedphys._imports import streamlit_ace, tomlkit
 
+import pymedphys._dicom.create as _pp_dcm_create
+from pymedphys import _losslessjpeg as lljpeg
+
 from pymedphys._experimental.streamlit.utilities import icom as _icom
+from pymedphys._experimental.wlutz import iview as _pp_wlutz_iview
 
 from . import (
     _angles,
@@ -317,6 +321,52 @@ def _angle_filtering(database_table):
     database_table = pd.concat(collated_dataframes, axis=0)
 
     return database_table
+
+
+def _create_portal_image_dicom_dataset(
+    gantry_angle, collimator_angle, table_angle, image_path
+):
+    """Don't intend this DICOM file to be compliant to the spec.
+
+    Instead, for now, just enough that software such as PIPs will
+    happily accept the created file.
+    """
+
+    # Image plane pixel spacing.
+    # Exported DICOM files have an image plane pixel spacing of 0.405,
+    # with SID of 1600 and SAD of 1000. This corresponds to an iso
+    # centre pixel spacing of 0.2531 mm.
+    #
+    # Within the database, those same images have an isocentre pixel
+    # spacing of either 0.2510 mm or 0.2488 mm. I suspect potentially
+    # there might be some interesting datastore rounding going on here.
+    # I was under the impression that the isocentre pixel spacing was
+    # for these images was 0.25 mm.
+
+    pixel_array = lljpeg.imread(image_path)
+    pixels_per_mm = _pp_wlutz_iview.infer_pixels_per_mm_from_shape(pixel_array)
+
+    sid = 1600.0
+    sad = 1000.0
+
+    pixel_spacing = 1 / pixels_per_mm * sid / sad
+
+    ds = _pp_dcm_create.dicom_dataset_from_dict(
+        {
+            "ImageType": ["ORIGINAL", "PRIMARY", "PORTAL"],
+            "Rows": pixel_array.shape[0],
+            "Columns": pixel_array.shape[1],
+            "GantryAngle": gantry_angle,
+            "BeamLimitingDeviceAngle ": collimator_angle,
+            "PatientSupportAngle ": table_angle,
+            "PixelData": pixel_array,
+            "RadiationMachineSAD": sad,
+            "RTImageSID": sid,
+            "ImagePlanePixelSpacing": [pixel_spacing, pixel_spacing],
+        }
+    )
+
+    return ds
 
 
 def _presentation_of_results(wlutz_directory_by_date, advanced_mode):
