0159e5090531ca0b0ed099913210372845569932
Matthew Jennings <Centrus.007@gmail.com>
2021-02-04 19:18:17 +1030

    Update copyrights.

--- lib/pymedphys/_dicom/dose.py
+++ lib/pymedphys/_dicom/dose.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2016-2019 Matthew Jennings and Simon Biggs
+# Copyright (C) 2016-2021 Matthew Jennings and Simon Biggs
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
