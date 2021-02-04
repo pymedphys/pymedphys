f0aa324dc3087dd6e751528869d64ece8ab52403
Matthew Jennings <Centrus.007@gmail.com>
2021-02-04 14:41:09 +1030

    Move pretty_patient_name_to lib/_dicom/header

--- lib/pymedphys/_dicom/header.py
+++ lib/pymedphys/_dicom/header.py
@@ -147,3 +147,67 @@ def adjust_RED_by_structure_name_cli(args):
     new_dicom_dataset = adjust_RED_by_structure_name(dicom_dataset)
 
     pydicom.write_file(args.output_file, new_dicom_dataset)
+
+
+def pretty_patient_name(
+    ds: "pydicom.dataset.Dataset",
+    surname_first: bool = False,
+    capitalise_surname: bool = True,
+    include_honorific: bool = False,
+) -> str:
+    """Return a prettier version of a DICOM PatientName
+
+    Parameters
+    ----------
+    ds : pydicom.dataset.Dataset
+        The DICOM dataset that contains the PatientName to be made
+        pretty.
+
+    surname_first : bool
+        If True, the pretty patient name will look like
+        '[Hon]. LastName, FirstName [MiddleName]'.
+        If False, the pretty patient name will look like
+        '[Hon]. FirstName [MiddleName] LastName'.
+        Defaults to False.
+
+    capitalise_surname : bool
+        If True, the pretty patient name will have the surname
+        capitalised. If False, surname will be in title case. Defaults
+        to True.
+
+    include_honorific: bool
+        If True, the pretty patient name will include the patient's
+        honorific at the beginning, if it exists. Defaults to False.
+
+    Returns
+    -------
+    str
+        A pretty version of PatientName.
+    """
+
+    names_as_str = str(ds.PatientName)
+
+    names, honorific = (
+        names_as_str.split("^^") if "^^" in names_as_str else (names_as_str, "")
+    )
+
+    name_list = names.split("^")
+    name_list.append(name_list.pop(0))  # Move surname to end
+
+    name_list_in_pretty_case = [n.title() for n in name_list]
+    honorific = honorific.title()
+
+    if capitalise_surname:
+        name_list_in_pretty_case[-1] = name_list_in_pretty_case[-1].upper()
+
+    if surname_first:
+        pretty_name = (
+            f"{name_list_in_pretty_case[-1]}, {' '.join(name_list_in_pretty_case[:-1])}"
+        )
+    else:
+        pretty_name = f"{' '.join(name_list_in_pretty_case)}"
+
+    if include_honorific:
+        pretty_name = f"{honorific}. {pretty_name}"
+
+    return pretty_name
