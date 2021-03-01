d476e4cbc1f333963b6569ef35ac70307653a16e
Matthew Jennings <Centrus.007@gmail.com>
2021-03-01 14:04:05 +1030

    Refactoring. Add Literal type hints. Rename axes to coordinates where still needed. Remove rounding where unneeded. Make is_decubitus() stricter

--- prototyping/xarray/xdose.py
+++ prototyping/xarray/xdose.py
@@ -1,46 +1,36 @@
 from typing import Tuple
+from typing_extensions import Literal
 
 import numpy as np
 import pydicom
 import xarray as xr
 
-from pymedphys._dicom import constants, coords, dose
-
-XARRAY_DIMS_BY_COORD_SYSTEM_AND_ORIENT = {
-    "S": ["y", "z", "x"],
-    "P": ["y", "z", "x"],
-    "P dec": ["y", "x", "z"],
-    "D": ["z", "y", "x"],
-    "D dec": ["z", "x", "y"],
-}
+from pymedphys._dicom import dose
 
 
-def xdose_from_dataset(ds, coord_system="SUPPORT", decimals_to_round_coords=-1):
-
-    dose_values = dose.dose_from_dataset(ds)
-    xr_dims = xarray_dims_from_dataset(ds, coord_system=coord_system)
-    xr_coords = xarray_coords_from_dataset(
-        ds,
-        coord_system=coord_system,
-        round_to_decimals=decimals_to_round_coords,
-    )
+def xdose_from_dataset(
+    ds: "pydicom.dataset.Dataset", coord_system: Literal["D", "P", "S"] = "S"
+) -> "xr.DataArray":
 
     return xr.DataArray(
-        data=dose_values,
-        dims=xr_dims,
-        coords=xr_coords,
+        data=dose.dose_from_dataset(ds),
+        dims=xarray_dims_from_dataset(ds, coord_system=coord_system),
+        coords=xarray_coords_from_dataset(
+            ds,
+            coord_system=coord_system,
+        ),
         name="dose",
         attrs={"units": ds.DoseUnits.title(), "coord_system": coord_system},
     )
 
 
 def coords_from_dataset(
-    ds: "pydicom.dataset.Dataset", coord_system: str = "SUPPORT"
+    ds: "pydicom.dataset.Dataset", coord_system: Literal["D", "P", "S"] = "S"
 ) -> Tuple["np.ndarray", "np.ndarray", "np.ndarray"]:
     r"""Returns the x, y and z coordinates of a DICOM dataset's
     pixel array in the specified coordinate system.
 
-    For DICOM RT Dose datasets, these are the x, y, z axes of the
+    For DICOM RT Dose datasets, these are the x, y, z coordinates of the
     dose grid.
 
     Parameters
@@ -54,14 +44,14 @@ def coords_from_dataset(
         coordinates of the DICOM dataset. The accepted, case-insensitive
         values of `coord_system` are:
 
-        'DICOM' or 'd':
-            Return axes in the DICOM coordinate system.
+        'D':
+            Return coordinates in the DICOM coordinate system.
 
-        'patient', 'IEC patient' or 'p':
-            Return axes in the IEC patient coordinate system.
+        'P':
+            Return coordinates in the IEC patient coordinate system.
 
-        'support', 'IEC support' or 's':
-            Return axes in the IEC support coordinate system.
+        'S':
+            Return coordinates in the IEC support coordinate system.
 
     Returns
     -------
@@ -77,14 +67,14 @@ def coords_from_dataset(
     =========================== ==========================
     Orientation                 ds.ImageOrientationPatient
     =========================== ==========================
-    Feet First Decubitus Left   [0, 1, 0, 1, 0, 0]
-    Feet First Decubitus Right  [0, -1, 0, -1, 0, 0]
-    Feet First Prone            [1, 0, 0, 0, -1, 0]
-    Feet First Supine           [-1, 0, 0, 0, 1, 0]
-    Head First Decubitus Left   [0, -1, 0, 1, 0, 0]
-    Head First Decubitus Right  [0, 1, 0, -1, 0, 0]
-    Head First Prone            [-1, 0, 0, 0, -1, 0]
-    Head First Supine           [1, 0, 0, 0, 1, 0]
+    Feet First Decubitus Left   [ 0,  1,  0,  1,  0,  0]
+    Feet First Decubitus Right  [ 0, -1,  0, -1,  0,  0]
+    Feet First Prone            [ 1,  0,  0,  0, -1,  0]
+    Feet First Supine           [-1,  0,  0,  0,  1,  0]
+    Head First Decubitus Left   [ 0, -1,  0,  1,  0,  0]
+    Head First Decubitus Right  [ 0,  1,  0, -1,  0,  0]
+    Head First Prone            [-1,  0,  0,  0, -1,  0]
+    Head First Supine           [ 1,  0,  0,  0,  1,  0]
     =========================== ==========================
 
     References
@@ -95,12 +85,9 @@ def coords_from_dataset(
        https://arxiv.org/ftp/arxiv/papers/1406/1406.0014.pdf
     """
 
-    position = np.array(ds.ImagePositionPatient)
-    orientation = np.array(ds.ImageOrientationPatient)
-
     if not (
-        np.array_equal(np.abs(orientation), np.array([1, 0, 0, 0, 1, 0]))
-        or np.array_equal(np.abs(orientation), np.array([0, 1, 0, 1, 0, 0]))
+        np.allclose(np.abs(ds.ImageOrientationPatient), np.array([1, 0, 0, 0, 1, 0]))
+        or np.allclose(np.abs(ds.ImageOrientationPatient), np.array([0, 1, 0, 1, 0, 0]))
     ):
         raise ValueError(
             "Dose grid orientation is not supported. Dose "
@@ -118,34 +105,48 @@ def coords_from_dataset(
     row_range = np.arange(0, ds.Rows * dj, dj)
 
     if is_decubitus:
-        x_support = orientation[1] * position[1] + col_range  # type: ignore
-        z_support = -(orientation[3] * position[0] + row_range)  # type: ignore
+        x_support = (
+            ds.ImageOrientationPatient[1] * ds.ImagePositionPatient[1] + col_range
+        )
+        z_support = -(
+            ds.ImageOrientationPatient[3] * ds.ImagePositionPatient[0] + row_range
+        )
     else:
-        x_support = orientation[0] * position[0] + col_range  # type: ignore
-        z_support = -(orientation[4] * position[1] + row_range)  # type: ignore
+        x_support = (
+            ds.ImageOrientationPatient[0] * ds.ImagePositionPatient[0] + col_range
+        )
+        z_support = -(
+            ds.ImageOrientationPatient[4] * ds.ImagePositionPatient[1] + row_range
+        )
 
     if is_head_first:
-        y_support = position[2] + np.array(ds.GridFrameOffsetVector)
+        y_support = ds.ImagePositionPatient[2] + np.array(ds.GridFrameOffsetVector)
     else:
-        y_support = -position[2] + np.array(ds.GridFrameOffsetVector)  # type: ignore
+        y_support = -ds.ImagePositionPatient[2] + np.array(ds.GridFrameOffsetVector)
 
-    if coord_system.upper() in ("SUPPORT", "IEC SUPPORT", "S"):
+    if coord_system.upper() == "S":
         return (x_support, y_support, z_support)
 
-    elif coord_system.upper() in ("DICOM", "D", "PATIENT", "IEC PATIENT", "P"):
+    elif coord_system.upper() in ("D", "P"):
 
-        x_patient = orientation[0] * x_support + orientation[3] * z_support
-        z_patient = orientation[1] * x_support + orientation[4] * z_support
+        x_patient = (
+            ds.ImageOrientationPatient[0] * x_support
+            + ds.ImageOrientationPatient[3] * z_support
+        )
+        z_patient = (
+            ds.ImageOrientationPatient[1] * x_support
+            + ds.ImageOrientationPatient[4] * z_support
+        )
 
         if not is_head_first:
             y_patient = -y_support
         else:
             y_patient = y_support
 
-        if coord_system.upper() in ("PATIENT", "IEC PATIENT", "P"):
+        if coord_system.upper() == "P":
             return (x_patient, y_patient, z_patient)
 
-        elif coord_system.upper() in ("DICOM", "D"):
+        elif coord_system.upper() == "D":
 
             x_dicom = x_patient
             y_dicom = -z_patient
@@ -154,45 +155,42 @@ def coords_from_dataset(
             return (x_dicom, y_dicom, z_dicom)
 
 
-def xarray_dims_from_dataset(ds, coord_system):
+def xarray_dims_from_dataset(
+    ds: "pydicom.dataset.Dataset", coord_system: Literal["D", "P", "S"] = "S"
+):
+    # fmt: off
+    XARRAY_DIMS_BY_COORD_SYSTEM_AND_ORIENT = {
+        "S":     ["y", "z", "x"],
+        "P":     ["y", "z", "x"],
+        "P dec": ["y", "x", "z"],
+        "D":     ["z", "y", "x"],
+        "D dec": ["z", "x", "y"],
+    }
+    # fmt: on
 
-    if coord_system.upper() in ("SUPPORT", "IEC SUPPORT", "S"):
-        return XARRAY_DIMS_BY_COORD_SYSTEM_AND_ORIENT["S"]
+    if dataset_orientation_is_decubitus(ds):
+        dim_key = f"{coord_system} dec"
+    else:
+        dim_key = coord_system
 
-    elif coord_system.upper() in ("PATIENT", "IEC PATIENT", "P"):
-        if dataset_orientation_is_decubitus(ds):
-            return XARRAY_DIMS_BY_COORD_SYSTEM_AND_ORIENT["P dec"]
-        else:
-            return XARRAY_DIMS_BY_COORD_SYSTEM_AND_ORIENT["P"]
-
-    elif coord_system.upper() in ("DICOM", "D"):
-        if dataset_orientation_is_decubitus(ds):
-            return XARRAY_DIMS_BY_COORD_SYSTEM_AND_ORIENT["D dec"]
-        else:
-            return XARRAY_DIMS_BY_COORD_SYSTEM_AND_ORIENT["D"]
+    return XARRAY_DIMS_BY_COORD_SYSTEM_AND_ORIENT[dim_key]
 
 
 def xarray_coords_from_dataset(
-    ds, coord_system: str = "SUPPORT", round_to_decimals: int = -1
+    ds: "pydicom.dataset.Dataset",
+    coord_system: Literal["D", "P", "S"] = "S",
 ):
     x, y, z = coords_from_dataset(ds, coord_system=coord_system)
 
-    xcoords = {"x": x, "y": y, "z": z}
-
-    if round_to_decimals >= 0:
-        return {
-            dim: np.round(xc, decimals=round_to_decimals) for dim, xc in xcoords.items()
-        }
-    else:
-        return xcoords
+    return {"x": x, "y": y, "z": z}
 
 
-def dataset_orientation_is_head_first(ds):
+def dataset_orientation_is_head_first(ds: "pydicom.dataset.Dataset"):
     if dataset_orientation_is_decubitus(ds):
         return np.abs(np.sum(ds.ImageOrientationPatient)) != 2
     else:
         return np.abs(np.sum(ds.ImageOrientationPatient)) == 2
 
 
-def dataset_orientation_is_decubitus(ds):
-    return ds.ImageOrientationPatient[0] == 0
+def dataset_orientation_is_decubitus(ds: "pydicom.dataset.Dataset"):
+    return np.allclose(np.abs(ds.ImageOrientationPatient), np.array([0, 1, 0, 1, 0, 0]))
