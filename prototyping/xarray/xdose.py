6d3f9221cb17a20989c46bd5811f107ada1aea9a
Matthew Jennings <Centrus.007@gmail.com>
2021-03-02 11:31:08 +1030

    Move plot_xdose_tcs to xdose module. Add demo of IEC SUPPORT xdose plotting

--- prototyping/xarray/xdose.py
+++ prototyping/xarray/xdose.py
@@ -1,6 +1,7 @@
 from typing import Tuple
 from typing_extensions import Literal
 
+from matplotlib import pyplot as plt
 import numpy as np
 import pydicom
 import xarray as xr
@@ -221,3 +222,102 @@ def _validate_coord_system(coord_system):
                 "\n".join(VALID_COORD_SYSTEMS)
             )
         )
+
+
+def plot_xdose_tcs_at_point(xdose_to_plot, point, coord_system="S"):
+    LAT_2_LONG_RATIO = xdose_to_plot.x.size / xdose_to_plot.y.size
+    VERT_2_LONG_RATIO = xdose_to_plot.z.size / xdose_to_plot.y.size
+
+    xdose_to_plot = round_xdose_coords(xdose_to_plot)
+
+    fig, axes = plt.subplots(
+        nrows=2,
+        ncols=2,
+        figsize=(12, 8),
+        gridspec_kw={
+            "width_ratios": (LAT_2_LONG_RATIO, 1),
+            "height_ratios": (VERT_2_LONG_RATIO, 1),
+        },
+    )
+
+    fig.suptitle(xdose_to_plot.name, fontsize=24)
+
+    axes[1, 1].axis("off")
+    for ax in axes.ravel():
+        ax.set_aspect("equal")
+
+    xdose_to_plot.sel(y=point[1], method="nearest").plot(
+        ax=axes[0, 0], cmap="jet", vmin=0, vmax=xdose_to_plot.max()
+    )
+    axes[0, 0].axhline(y=point[2], color="silver")
+    axes[0, 0].axvline(x=point[0], color="silver")
+    axes[0, 0].set_title("Transverse")
+
+    xdose_to_plot.sel(x=point[0], method="nearest").T.plot(
+        ax=axes[0, 1], cmap="jet", vmin=0, vmax=xdose_to_plot.max()
+    )
+    axes[0, 1].axhline(y=point[2], color="silver")
+    axes[0, 1].axvline(x=point[1], color="silver")
+    axes[0, 1].set_title("Sagittal")
+
+    xdose_to_plot.sel(z=point[2], method="nearest").plot(
+        ax=axes[1, 0], cmap="jet", vmin=0, vmax=xdose_to_plot.max()
+    )
+    axes[1, 0].axhline(y=point[1], color="silver")
+    axes[1, 0].axvline(x=point[0], color="silver")
+    axes[1, 0].set_title("Coronal")
+
+    fig.tight_layout()
+
+
+def zoom(
+    xdose_to_zoom,
+    x_start=None,
+    x_end=None,
+    y_start=None,
+    y_end=None,
+    z_start=None,
+    z_end=None,
+):
+
+    if x_start is None:
+        x_start = xdose_to_zoom["x"][0]
+    if x_end is None:
+        x_end = xdose_to_zoom["x"][-1]
+    if y_start is None:
+        y_start = xdose_to_zoom["y"][0]
+    if y_end is None:
+        y_end = xdose_to_zoom["y"][-1]
+    if z_start is None:
+        z_start = xdose_to_zoom["z"][0]
+    if z_end is None:
+        z_end = xdose_to_zoom["z"][-1]
+
+    if xdose_to_zoom["x"][0] < xdose_to_zoom["x"][-1] and x_start > x_end:
+        raise ValueError(
+            "`x_start` must be less than `x_end` since the x coords are increasing in `xdose_to_zoom`"
+        )
+    elif xdose_to_zoom["x"][0] > xdose_to_zoom["x"][-1] and x_start < x_end:
+        raise ValueError(
+            "`x_start` must be greater than `x_end` since the x coords are decreasing in `xdose_to_zoom`"
+        )
+    if xdose_to_zoom["y"][0] < xdose_to_zoom["y"][-1] and y_start > y_end:
+        raise ValueError(
+            "`y_start` must be less than `y_end` since the y coords are increasing in `xdose_to_zoom`"
+        )
+    elif xdose_to_zoom["y"][0] > xdose_to_zoom["y"][-1] and x_start < x_end:
+        raise ValueError(
+            "`y_start` must be greater than `y_end` since the y coords are decreasing in `xdose_to_zoom`"
+        )
+    if xdose_to_zoom["z"][0] < xdose_to_zoom["z"][-1] and z_start > z_end:
+        raise ValueError(
+            "`z_start` must be less than `z_end` since the z coords are increasing in `xdose_to_zoom`"
+        )
+    elif xdose_to_zoom["z"][0] > xdose_to_zoom["z"][-1] and z_start < z_end:
+        raise ValueError(
+            "`z_start` must be greater than `z_end` since the z coords are decreasing in `xdose_to_zoom`"
+        )
+
+    return xdose_to_zoom.sel(
+        x=slice(x_start, x_end), y=slice(y_start, y_end), z=slice(z_start, z_end)
+    )
