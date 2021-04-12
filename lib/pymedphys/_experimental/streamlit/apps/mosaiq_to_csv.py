0daad55015d4fac3fb7090d94cb9fcb2f7334769
SimonBiggs <me@simonbiggs.net>
2021-04-12 17:42:37 +1000

    continuing to pull out tables

--- lib/pymedphys/_experimental/streamlit/apps/mosaiq_to_csv.py
+++ lib/pymedphys/_experimental/streamlit/apps/mosaiq_to_csv.py
@@ -15,10 +15,7 @@
 from pymedphys._imports import pandas as pd
 from pymedphys._imports import streamlit as st
 
-from altair.vegalite.v4.schema.core import Value
-
 import pymedphys
-from pymedphys._mosaiq import delivery, helpers
 from pymedphys._streamlit import categories
 from pymedphys._streamlit.utilities import config as st_config
 from pymedphys._streamlit.utilities import mosaiq as _mosaiq
@@ -42,6 +39,7 @@ ALLOWLIST_COLUMN_NAMES = [
     "Pat_ID1",
     "SIT_Set_ID",
     "FLD_ID",
+    "Staff_ID",
 ]
 
 
@@ -61,33 +59,66 @@ def main():
 
     ident = get_filtered_table(connection, "Ident", "IDA", patient_id)
     pat_id1 = str(ident["Pat_Id1"].iloc[0])
+    st.write("## `Ident` Table")
     st.write(ident)
 
     # Patient.Pat_ID1 = Ident.Pat_ID1 AND
 
     patient = get_filtered_table(connection, "Patient", "Pat_ID1", pat_id1)
+    st.write("## `Patient` Table")
     st.write(patient)
 
     tx_field = get_filtered_table(connection, "TxField", "Pat_ID1", pat_id1)
+    st.write("## `TxField` Table")
     st.write(tx_field)
 
     # TxField.SIT_Set_ID = Site.SIT_Set_ID
 
-    for _, row in tx_field.iterrows():
-        sit_set_id = str(row["SIT_Set_ID"])
-        fld_id = str(row["FLD_ID"])
+    sit_set_ids = set()
+    fld_ids = set()
 
-        st.write(f"Field ID: `{fld_id}`")
+    for _, tx_field_row in tx_field.iterrows():
+        sit_set_ids.add(tx_field_row["SIT_Set_ID"])
+        fld_ids.add(tx_field_row["FLD_ID"])
 
-        site = get_filtered_table(connection, "Site", "SIT_Set_ID", sit_set_id)
-        st.write(site)
+    site = pd.DataFrame()
+    for sit_set_id in sit_set_ids:
+        site = pd.concat(
+            [site, get_filtered_table(connection, "Site", "SIT_Set_ID", sit_set_id)]
+        )
 
+    st.write("## `Site` Table")
+    st.write(site)
+
+    track_treatment = pd.DataFrame()
+    for fld_id in fld_ids:
         # TrackTreatment.FLD_ID = TxField.FLD_ID
 
-        track_treatment = get_filtered_table(
-            connection, "TrackTreatment", "FLD_ID", fld_id
+        track_treatment = pd.concat(
+            [
+                track_treatment,
+                get_filtered_table(connection, "TrackTreatment", "FLD_ID", fld_id),
+            ]
         )
-        st.write(track_treatment)
+
+    st.write("## `TrackTreatment` Table")
+    st.write(track_treatment)
+    machine_staff_ids = track_treatment["Machine_ID_Staff_ID"].unique()
+
+    # Staff.Staff_ID = TrackTreatment.Machine_ID_Staff_ID
+
+    staff = pd.DataFrame()
+    for machine_staff_id in machine_staff_ids:
+
+        staff = pd.concat(
+            [
+                staff,
+                get_filtered_table(connection, "Staff", "Staff_ID", machine_staff_id),
+            ]
+        )
+
+    st.write("## `Staff` Table")
+    st.write(staff)
 
     # fields = helpers.get_patient_fields(connection, patient_id)
     # st.write(fields)
@@ -120,6 +151,7 @@ def get_all_column_names(connection, table):
     return columns
 
 
+@st.cache(ttl=86400, hash_funcs={pymedphys.mosaiq.Connection: id})
 def get_filtered_table(connection, table, column_name, column_value):
     if not table in ALLOWLIST_TABLE_NAMES:
         raise ValueError(f"{table} must be within the allowlist")
@@ -127,6 +159,7 @@ def get_filtered_table(connection, table, column_name, column_value):
     if not column_name in ALLOWLIST_COLUMN_NAMES:
         raise ValueError(f"{column_name} must be within the allowlist")
 
+    column_value = str(column_value)
     column_names = get_all_column_names(connection, table)
 
     sql_string = f"""
