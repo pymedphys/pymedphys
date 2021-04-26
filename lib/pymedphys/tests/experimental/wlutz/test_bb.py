24d63b152695a44185eb4d4789c13d24d7c8b17e
SimonBiggs <me@simonbiggs.net>
2021-04-26 14:21:14 +1000

    fix pylint issue

--- lib/pymedphys/tests/experimental/wlutz/test_bb.py
+++ lib/pymedphys/tests/experimental/wlutz/test_bb.py
@@ -14,7 +14,6 @@
 
 
 from pymedphys._imports import numpy as np
-from pymedphys._imports import pytest
 
 import pymedphys._mocks.wlutz as wlutz_mocks
 
@@ -43,7 +42,6 @@ def test_normal_bb():
     )
 
 
-# @pytest.mark.skip(reason="Limitation of current PyMedPhys WLutz algorithm")
 def test_small_bb():
     field_centre = [0, 0]
     field_side_lengths = [15, 35]
