2432086645178b1c0a0930c67ac1655bbed4b1f1
SimonBiggs <me@simonbiggs.net>
2021-04-27 13:56:06 +1000

    update streamlit

--- setup.py
+++ setup.py
@@ -115,8 +115,7 @@ packages = [
 
 package_data = {
     "": ["*"],
-    "pymedphys._experimental.serviceplans": ["templates/*"],
-    "pymedphys._experimental.streamlit.apps": ["data/*"],
+    "pymedphys._experimental": ["serviceplans/templates/*", "streamlit/apps/data/*"],
     "pymedphys.docs": [
         "_static/*",
         "app/*",
@@ -139,17 +138,16 @@ package_data = {
         "lib/tutes/*",
         "trees/*",
     ],
-    "pymedphys.tests.dicom": ["data/rtplan/*", "scratch/*"],
-    "pymedphys.tests.e2e": [
-        "cypress/*",
-        "cypress/fixtures/*",
-        "cypress/integration/streamlit/*",
-        "cypress/plugins/*",
-        "cypress/support/*",
-    ],
-    "pymedphys.tests.experimental.mephysto": [
-        "data/baselines/*",
-        "data/measurements/*",
+    "pymedphys.tests": [
+        "dicom/data/rtplan/*",
+        "dicom/scratch/*",
+        "e2e/cypress/*",
+        "e2e/cypress/fixtures/.gitignore",
+        "e2e/cypress/integration/streamlit/*",
+        "e2e/cypress/plugins/*",
+        "e2e/cypress/support/*",
+        "experimental/mephysto/data/baselines/*",
+        "experimental/mephysto/data/measurements/*",
     ],
 }
 
@@ -190,7 +188,7 @@ extras_require = {
         "pymedphys-pylibjpeg-libjpeg",
         "shapely>=1.7.0",
         "scikit-image>=0.18.1",
-        "streamlit==0.78.0",
+        "streamlit==0.80.0",
         "streamlit-ace==0.0.4",
         "streamlit-analytics==0.1.2",
         "pylinac==2.3.2",
@@ -274,7 +272,7 @@ extras_require = {
         "pymedphys-pylibjpeg-libjpeg",
         "shapely>=1.7.0",
         "scikit-image>=0.18.1",
-        "streamlit==0.78.0",
+        "streamlit==0.80.0",
         "streamlit-ace==0.0.4",
         "streamlit-analytics==0.1.2",
         "pylinac==2.3.2",
