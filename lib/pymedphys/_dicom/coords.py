4f32dbe8071a11c4ebca0a6dfcbd03cf081f0fb0
cedricbelanger <50745324+cedricbelanger@users.noreply.github.com>
2021-11-15 07:50:36 -0500

    Update spelling error
    Co-authored-by: Simon Biggs <simon.biggs@radiotherapy.ai>

--- lib/pymedphys/_dicom/coords.py
+++ lib/pymedphys/_dicom/coords.py
@@ -114,7 +114,7 @@ def xyz_axes_from_dataset(
 
     Extra notes
     -----------
-    The ordering to unpac the pixel spacing values from PixelSpacing have 
+    The ordering to unpack the pixel spacing values from PixelSpacing have 
     importance when dealing with non square pixels. For more informations 
     on how to unpack the PixelSpacing values in the right order, see : 
     http://dicom.nema.org/medical/dicom/current/output/chtml/part03/
