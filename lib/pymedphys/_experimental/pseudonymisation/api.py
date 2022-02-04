8302e0b41b14618e6b100d01ba1da5d3baaf2142
Stuart Swerdloff <sjswerdloff@gmail.com>
2022-02-04 17:06:42 +1300

    Revert "Merge pull request #1534 from pymedphys/unexpose-pseudonymise"
    This reverts commit 72f5e72316b7761527757b5a53fd7eeadd5dcdcb, reversing
    changes made to 9b16b9cbc293276422e7209c658160962405fba9.

--- lib/pymedphys/_experimental/pseudonymisation/api.py
+++
@@ -1,9 +0,0 @@
-# pylint: disable = unused-import
-
-
-from pymedphys._experimental.pseudonymisation.main import (
-    get_default_pseudonymisation_keywords,
-    is_valid_strategy_for_keywords,
-    pseudonymise,
-)
-from pymedphys._experimental.pseudonymisation.strategy import pseudonymisation_dispatch
