b0dab8aa0554dc223f951aff28727c60dd3dcfb9
ayalalazaro <ayalalazaro@protonmail.com>
2022-04-04 21:02:34 +0200

    qcheck pylint: disable = unused-import

--- lib/pymedphys/experimental/quickcheck.py
+++ lib/pymedphys/experimental/quickcheck.py
@@ -12,4 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# pylint: disable = unused-import
 from pymedphys._experimental.quickcheck.qcheck import QuickCheck
