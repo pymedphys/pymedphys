dda23255389db66096823c89d63f8a780a857225
ayalalazaro <ayalalazaro@protonmail.com>
2022-04-04 21:21:13 +0200

    qcheck more pylint: disable = unused-import, style fix

--- lib/pymedphys/tests/experimental/quickcheck/test_quickcheck.py
+++ lib/pymedphys/tests/experimental/quickcheck/test_quickcheck.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# pylint: disable = unused-import
 import socket
 from unittest import mock, TestCase
 from pymedphys.experimental.quickcheck import QuickCheck
@@ -26,19 +27,19 @@ def mock_socket_send(self):
     elif message.startswith(b'MEASGET;INDEX-MEAS='):
         index = message.split(b'=')[1].decode()
         raw_string = 'MEASGET;INDEX-MEAS={};MD=[ID=1636992223;Date=2021-11-15;Time=16:03:43];WORK=[' \
-                    'ID=1628265817;Name=VERSA BETA];TASK=[ID=188165042;TUnit=VERSA BETA;Prot=[' \
-                    'Name=HGUGM;Flat=2;Sym=1];Mod=Photons;En=6;Fs=200x200;SDD=1000;Ga=0;We=0;MU=100;My=1.0000E+00' \
-                    ';Info=QA DIARIO: 6 MV];MV=[' \
-                    'CAX=1.0572E+00;G10=1.0894E+00;L10=1.0869E+00;T10=1.0874E+00;R10=1.0846E+00;G20=1.0990E+00;L20=1' \
-                    '.0884E+00;T20=1.0965E+00;R20=1.0923E+00;E1=9.9593E-01;E2=1.0490E+00;E3=1.0794E+00;E4=1.1317E+00' \
-                    ';Temp=2.3590E+01;Press=9.3290E+02;CAXRate=5.4921E+00;ExpTime=1.1550E+01];AV=[CAX=[' \
-                    'Min=9.7500E+01;Max=1.0250E+02;Target=1.0000E+02;Norm=9.5371E+01;Value=1.0083E+02;Valid=1];FLAT=[' \
-                    'Min=9.7000E+01;Max=1.0300E+02;Target=1.0000E+02;Norm=9.5919E+01;Value=9.9709E+01;Valid=1];SYMGT' \
-                    '=[Min=9.7000E+01;Max=1.0300E+02;Target=1.0000E+02;Norm=9.9028E-01;Value=9.9253E+01;Valid=1' \
-                    '];SYMLR=[Min=9.7000E+01;Max=1.0300E+02;Target=1.0000E+02;Norm=9.9062E-01;Value=9.9423E+01;Valid' \
-                    '=1];BQF=[Min=9.5000E+01;Max=1.0500E+02;Target=1.0000E+02;Norm=1.6314E+01;Value=1.0059E+02;Valid' \
-                    '=1];We=[Min=0.0000E+00;Max=0.0000E+00;Target=0.0000E+00;Norm=1.0000E+00;Value=0.0000E+00;Valid=1' \
-                    ']];3229717283\r\n'.format(index)
+                     'ID=1628265817;Name=VERSA BETA];TASK=[ID=188165042;TUnit=VERSA BETA;Prot=[' \
+                     'Name=HGUGM;Flat=2;Sym=1];Mod=Photons;En=6;Fs=200x200;SDD=1000;Ga=0;We=0;MU=100;My=1.0000E+00' \
+                     ';Info=QA DIARIO: 6 MV];MV=[' \
+                     'CAX=1.0572E+00;G10=1.0894E+00;L10=1.0869E+00;T10=1.0874E+00;R10=1.0846E+00;G20=1.0990E+00;L20=1' \
+                     '.0884E+00;T20=1.0965E+00;R20=1.0923E+00;E1=9.9593E-01;E2=1.0490E+00;E3=1.0794E+00;E4=1.1317E+00' \
+                     ';Temp=2.3590E+01;Press=9.3290E+02;CAXRate=5.4921E+00;ExpTime=1.1550E+01];AV=[CAX=[' \
+                     'Min=9.7500E+01;Max=1.0250E+02;Target=1.0000E+02;Norm=9.5371E+01;Value=1.0083E+02;Valid=1];FLAT=' \
+                     '[Min=9.7000E+01;Max=1.0300E+02;Target=1.0000E+02;Norm=9.5919E+01;Value=9.9709E+01;Valid=1];' \
+                     'SYMGT=[Min=9.7000E+01;Max=1.0300E+02;Target=1.0000E+02;Norm=9.9028E-01;Value=9.9253E+01;Valid=1' \
+                     '];SYMLR=[Min=9.7000E+01;Max=1.0300E+02;Target=1.0000E+02;Norm=9.9062E-01;Value=9.9423E+01;Valid' \
+                     '=1];BQF=[Min=9.5000E+01;Max=1.0500E+02;Target=1.0000E+02;Norm=1.6314E+01;Value=1.0059E+02;Valid' \
+                     '=1];We=[Min=0.0000E+00;Max=0.0000E+00;Target=0.0000E+00;Norm=1.0000E+00;Value=0.0000E+00;' \
+                     'Valid=1]];3229717283\r\n'.format(index)
         self.raw_data = raw_string.encode()
 
 
