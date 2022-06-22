d0355f97082ff42ae0d8122f2ce094d835e8e014
Simon Biggs <simon.biggs@radiotherapy.ai>
2022-06-22 14:06:36 +1000

    remove grpc

--- lib/pymedphys/cli/experimental/__init__.py
+++ lib/pymedphys/cli/experimental/__init__.py
@@ -14,7 +14,6 @@
 
 
 from .dicom import dicom_cli
-from .grpc import grpc_cli
 from .pinnacle import pinnacle_cli
 from .quickcheck import quickcheck_cli
 
@@ -28,6 +27,5 @@ def experimental_cli(subparsers):
     dicom_cli(experimental_subparsers)
     pinnacle_cli(experimental_subparsers)
     quickcheck_cli(experimental_subparsers)
-    grpc_cli(experimental_subparsers)
 
     return experimental_parser
--- proto/compile.sh
+++
@@ -1,3 +0,0 @@
-poetry run python -m grpc_tools.protoc \
-  --python_betterproto_out=../lib/pymedphys/_experimental/_proto/ \
-  -I ../. ../proto/*.proto
