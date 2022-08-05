47cbdfd06e3e1a75761375e967f698db59ec6a81
Simon Biggs <simon.biggs@radiotherapy.ai>
2022-08-06 03:52:03 +1000

    re-introduce electron API by reverting commit 2466f6d1abc8eb0d4f6fa17e320373c95e261a94

--- lib/pymedphys/__init__.py
+++ lib/pymedphys/__init__.py
@@ -1,6 +1,6 @@
 """Module docstring."""
 
-from . import dicom, metersetmap, mosaiq, mudensity, trf
+from . import dicom, electronfactors, metersetmap, mosaiq, mudensity, trf
 from ._data import data_path, zenodo_data_paths, zip_data_paths
 from ._delivery import Delivery
 from ._gamma.implementation.shell import gamma_shell as gamma
