7363d850e3c77d15919948d31547a1dcee11fc0e
Simon Biggs <simon.biggs@radiotherapy.ai>
2022-08-06 03:52:35 +1000

    spelling

--- lib/pymedphys/electronfactors.py
+++ lib/pymedphys/electronfactors.py
@@ -1,5 +1,5 @@
 """A suite of functions that model electron insert/cutout factors by
-parameterising them as equivalent ellipses.
+parametrising them as equivalent ellipses.
 """
 
 # pylint: disable = unused-import
