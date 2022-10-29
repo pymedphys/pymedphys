6613827ca8300cd5196863985f01bce9df39a58e
Matthew Jennings <Centrus.007@gmail.com>
2022-10-29 19:23:18 +1000

    add mj copyright to gamma_shell

--- lib/pymedphys/_gamma/implementation/shell.py
+++ lib/pymedphys/_gamma/implementation/shell.py
@@ -1,3 +1,4 @@
+# Copyright (C) 2022 Matthew Jennings
 # Copyright (C) 2015-2018 Simon Biggs
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
