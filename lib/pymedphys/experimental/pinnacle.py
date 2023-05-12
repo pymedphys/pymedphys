470e18625cfc62153401cddcb66c7bd6703db11d
Phillip Chlap <phillip.chlap@unsw.edu.au>
2023-05-12 09:50:14 +0200

    set __name__ for deprecated calls

--- lib/pymedphys/experimental/pinnacle.py
+++ lib/pymedphys/experimental/pinnacle.py
@@ -4,18 +4,22 @@ from pymedphys.pinnacle import PinnacleImage as _PinnacleImage
 from pymedphys.pinnacle import PinnaclePlan as _PinnaclePlan
 from pymedphys.pinnacle import export_cli as _export_cli
 
+_PinnacleExport.__name__ = "pymedphys.experimental.pinnacle.PinnacleExport"
 PinnacleExport = _deprecated(
     reason="This has been replaced with `pymedphys.pinnacle.PinnacleExport`"
 )(_PinnacleExport)
 
+_export_cli.__name__ = "pymedphys.experimental.pinnacle.export_cli"
 export_cli = _deprecated(
     reason="This has been replaced with `pymedphys.pinnacle.export_cli`"
 )(_export_cli)
 
+_PinnacleImage.__name__ = "pymedphys.experimental.pinnacle.PinnacleImage"
 PinnacleImage = _deprecated(
     reason="This has been replaced with `pymedphys.pinnacle.PinnacleImage`"
 )(_PinnacleImage)
 
+_PinnaclePlan.__name__ = "pymedphys.experimental.pinnacle.PinnaclePlan"
 PinnaclePlan = _deprecated(
     reason="This has been replaced with `pymedphys.pinnacle.PinnaclePlan`"
 )(_PinnaclePlan)
