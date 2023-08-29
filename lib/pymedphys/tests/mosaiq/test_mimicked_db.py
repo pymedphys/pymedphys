1ed4b4f1b9bf2276ad5060f8d9bf6750cb72189a
Marc J.P. Chamberland <marc.chamberland@uvmhealth.org>
2023-08-29 15:09:10 -0400

    WIP

--- lib/pymedphys/tests/mosaiq/test_mimicked_db.py
+++ lib/pymedphys/tests/mosaiq/test_mimicked_db.py
@@ -185,4 +185,6 @@ def test_mosaiq_table_to_type_map_dict(connection: pymedphys.mosaiq.Connection):
             types_map["TxField"][key] = mimics.TYPE_CASTING[
                 value
             ]  # needed for TYPE_CASTING workaround in mimics.py
+        if value == "varbinary":
+            types_map["Txfield"][key] = "largebinary"
     assert mosaiq_table_type_map_dict["TxField"] == types_map["TxField"]
