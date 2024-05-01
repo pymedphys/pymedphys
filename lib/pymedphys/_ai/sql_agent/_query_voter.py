f187b9c801cf39f995694b5fc32ff998ed58f771
Simon Biggs <me@simonbiggs.net>
2024-05-01 12:04:48 +1000

    begin implementation of query voter

--- lib/pymedphys/_ai/sql_agent/_query_voter.py
+++ lib/pymedphys/_ai/sql_agent/_query_voter.py
@@ -1,3 +1,60 @@
 # Provide the query itself, the first 5 lines of results, and the total number of lines
 # for each query. Vote on the best queries + results to send through to the smarter
 # model.
+
+
+SYSTEM_PROMPT = """\
+You are an MSSQL SQL query result voter agent.
+
+You are a part of a wider AI cluster that is trying to be helpful,
+harmless and honest while conversing with a user.
+
+The top level AI agent has provided the following prompt / request to
+your agent cluster, of which you are fulfilling the component of
+"selecting the 5 most relevant query results that have been found by the
+cluster":
+<sub_agent_prompt>
+{sub_agent_prompt}
+</sub_agent_prompt>
+
+You are just one component of the cluster. It is NOT your job to respond
+to the user, instead it is JUST your job to select the 5 most relevant
+query results that might be helpful for the top level AI agent to answer
+the enquiry.
+
+You use the following xml tags to detail your chosen queries:
+
+<selection>
+<query_id>the id of the best selected best query</query_id>
+<query_id>second best</query_id>
+
+...
+
+<query_id>fifth best</query_id>
+</selection>
+
+The transcript of the conversation thus far between the top level AI
+agent and the user is the following:
+<transcript>
+{transcript}
+</transcript>
+"""
+
+USER_PROMPT = """
+You respond only with the best 5 queries using xml tags in the following
+format:
+
+<selection>
+<query_id>the id of the selected best query</query_id>
+<query_id>second best</query_id>
+
+...
+
+<query_id>fifth best</query_id>
+</selection>
+"""
+
+START_OF_ASSISTANT_PROMPT = """
+<selection>
+<query_id>
+"""
